a0e4b0ee3b7cfd6442c3a042e7674853265135d8
Daniel A. Nagy <nagy.da@gmail.com>
2015-09-03 18:26:20 +0200

    Solidity chequebook contract. WiP

---
+++ bzz/bzzcontract/chequebook.sol
@@ -0,0 +1,27 @@
+import "mortal";
+
+/// @title Chequebook for Ethereum micropayments
+/// @author Daniel A. Nagy <daniel@ethdev.com>
+contract chequebook is mortal {
+    // Cumulative paid amount in wei to each beneficiary
+    mapping (address => uint256) sent;
+    
+    /// @notice Cash cheque
+    /// 
+    /// @param beneficiary beneficiary address
+    /// @param amount cumulative amount in wei
+    /// @param sig_v signature parameter v
+    /// @param sig_r signature parameter r
+    /// @param sig_s signature parameter s
+    function cash(address beneficiary, uint256 amount,
+        uint8 sig_v, bytes32 sig_r, bytes32 sig_s) {
+        if(amount <= sent[beneficiary]) return;
+        bytes32 hash = sha3(beneficiary, amount);
+        // TODO: check signature
+        if (beneficiary.send(amount - sent[beneficiary])) {
+            sent[beneficiary] = amount;
+        } else {
+            // owner.sendToDebtorsPrison();
+        }
+    }
+}
