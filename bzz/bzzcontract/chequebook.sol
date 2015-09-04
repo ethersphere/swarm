ee3e85c6be402841e56452a65e19d585df7ff064
Daniel A. Nagy <nagy.da@gmail.com>
2015-09-04 12:51:12 +0200

    First workable version of chequebook. Not tested.

--- bzz/bzzcontract/chequebook.sol
+++ bzz/bzzcontract/chequebook.sol
@@ -5,6 +5,9 @@ import "mortal";
 contract chequebook is mortal {
     // Cumulative paid amount in wei to each beneficiary
     mapping (address => uint256) sent;
+
+    /// @notice Overdraft event
+    event Overdraft(address deadbeat);
     
     /// @notice Cash cheque
     /// 
@@ -17,11 +20,14 @@ contract chequebook is mortal {
         uint8 sig_v, bytes32 sig_r, bytes32 sig_s) {
         if(amount <= sent[beneficiary]) return;
         bytes32 hash = sha3(beneficiary, amount);
-        // TODO: check signature
+        if(owner != ecrecover(hash, sig_v, sig_r, sig_s)) return;
         if (beneficiary.send(amount - sent[beneficiary])) {
             sent[beneficiary] = amount;
         } else {
             // owner.sendToDebtorsPrison();
+            Overdraft(owner);
+            suicide(beneficiary);
         }
     }
 }
+
