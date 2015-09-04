3faaec6f64f97ba6c2d26c8ff2f21e6103da41f3
Daniel A. Nagy <nagy.da@gmail.com>
2015-09-04 15:35:42 +0200

    Properly documented chequebook with accessor for cumulative amounts.

--- bzz/bzzcontract/chequebook.sol
+++ bzz/bzzcontract/chequebook.sol
@@ -9,6 +9,14 @@ contract chequebook is mortal {
     /// @notice Overdraft event
     event Overdraft(address deadbeat);
     
+    /// @notice Accessor for sent map
+    ///
+    /// @param beneficiary beneficiary address
+    /// @return cumulative amount in wei sent to beneficiary
+    function getSent(address beneficiary) returns (uint256) {
+	    return sent[beneficiary];
+    }
+
     /// @notice Cash cheque
     /// 
     /// @param beneficiary beneficiary address
@@ -18,16 +26,23 @@ contract chequebook is mortal {
     /// @param sig_s signature parameter s
     function cash(address beneficiary, uint256 amount,
         uint8 sig_v, bytes32 sig_r, bytes32 sig_s) {
+        // Check if the cheque is old.
+        // Only cheques that are more recent than the last cashed one are considered.
         if(amount <= sent[beneficiary]) return;
+        // Check the digital signature of the cheque.
         bytes32 hash = sha3(beneficiary, amount);
         if(owner != ecrecover(hash, sig_v, sig_r, sig_s)) return;
+        // Attempt sending the difference between the cumulative amount on the cheque
+        // and the cumulative amount on the last cashed cheque to beneficiary.
         if (beneficiary.send(amount - sent[beneficiary])) {
+            // Upon success, update the cumulative amount.
             sent[beneficiary] = amount;
         } else {
+            // Upon failure, punish owner (for writing a bounced cheque)
+            // and compensate beneficiary.
             // owner.sendToDebtorsPrison();
             Overdraft(owner);
             suicide(beneficiary);
         }
     }
 }
-
