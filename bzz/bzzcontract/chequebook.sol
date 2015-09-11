caa57f88381ae0858405c8596e3ca100e09ddf93
Daniel A. Nagy <nagy.da@gmail.com>
2015-09-11 15:01:25 +0200

    Multi-chequebook support fixed.

--- bzz/bzzcontract/chequebook.sol
+++ bzz/bzzcontract/chequebook.sol
@@ -19,21 +19,19 @@ contract chequebook is mortal {
 
     /// @notice Cash cheque
     /// 
-    /// @param contract contract address identifying the chequebook
     /// @param beneficiary beneficiary address
     /// @param amount cumulative amount in wei
     /// @param sig_v signature parameter v
     /// @param sig_r signature parameter r
     /// @param sig_s signature parameter s
-    function cash(address contract, address beneficiary, uint256 amount,
+    /// The digital signature is calculated on the concatenated triplet of contract address, beneficiary address and cumulative amount
+    function cash(address beneficiary, uint256 amount,
         uint8 sig_v, bytes32 sig_r, bytes32 sig_s) {
-        // Check whether or not the cheque is for us
-        if(address(this) != contract) return;
         // Check if the cheque is old.
         // Only cheques that are more recent than the last cashed one are considered.
         if(amount <= sent[beneficiary]) return;
         // Check the digital signature of the cheque.
-        bytes32 hash = sha3(beneficiary, amount);
+        bytes32 hash = sha3(address(this), beneficiary, amount);
         if(owner != ecrecover(hash, sig_v, sig_r, sig_s)) return;
         // Attempt sending the difference between the cumulative amount on the cheque
         // and the cumulative amount on the last cashed cheque to beneficiary.
