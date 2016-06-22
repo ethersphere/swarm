0e025bc6bfbae1d73943ba349b2d807025a23abd
Nick Johnson <arachnid@notdot.net>
2016-06-22 11:51:32 +0100

    swarm/services/ens: Rework ENS functionality to use the current spec.

--- swarm/services/ens/contract/ens.sol
+++ swarm/services/ens/contract/ens.sol
@@ -1,19 +1,24 @@
-import "mortal";
-/// @title Swarm Distributed Preimage Archive
-/// @author Viktor Tron <viktor@ethdev.com>
-contract ENS is mortal
-{
+/**
+ * ENS resolver interface.
+ */
+contract Resolver {
+    bytes32 constant TYPE_STAR = "*";
+    
+    // Response codes.
+    uint16 constant RCODE_OK = 0;
+    uint16 constant RCODE_NXDOMAIN = 3;
 
-  mapping (bytes32 => bytes32) public Registry;
-  mapping (bytes32 => address) public Owners;
+    function findResolver(bytes12 nodeId, bytes32 label) constant
+        returns (uint16 rcode, uint32 ttl, bytes12 rnode, address raddress);
+    function resolve(bytes12 nodeId, bytes32 qtype, uint16 index) constant
+        returns (uint16 rcode, bytes16 rtype, uint32 ttl, uint16 len,
+                 bytes32 data);
+    function getExtended(bytes32 id) constant returns (bytes data);
 
-  function Set(bytes32 host, bytes32 content) {
-    if (Owners[host] == 0x0) {
-      Owners[host] = tx.origin;
-    }
-    if (Owners[host] == tx.origin) {
-      Registry[host] = content;
-    }
-  }
-
+    // Optional 'personal resolver' update functions.
+    function isPersonalResolver() constant returns (bool);
+    function setRR(bytes12 rootNodeId, string name, bytes16 rtype, uint32 ttl, uint16 len, bytes32 data);
+    function setPrivateRR(bytes12 rootNodeId, bytes32[] name, bytes16 rtype, uint32 ttl, uint16 len, bytes32 data);
+    function deleteRR(bytes12 rootNodeId, string name);
+    function deletePrivateRR(bytes12 rootNodeId, bytes32[] name);
 }
--- swarm/services/ens/ens.go
+++ swarm/services/ens/ens.go
@@ -1,14 +1,12 @@
-//go:generate abigen --sol contract/ens.sol	--pkg contract --out contract/ens.go
 package ens
 
 import (
 	"fmt"
-	"math/big"
 	"regexp"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/logger"
 	"github.com/ethereum/go-ethereum/logger/glog"
@@ -17,68 +15,65 @@ import (
 )
 
 var domainAndVersion = regexp.MustCompile("[@:;,]+")
+var qtypeChash = [32]byte{ 0x43, 0x48, 0x41, 0x53, 0x48}
 
 // swarm domain name registry and resolver
 // the ENS instance can be directly wrapped in rpc.Api
 type ENS struct {
-	*contract.ENSSession
+	session *contract.ResolverSession
+	contractBackend bind.ContractBackend
 }
 
 // NewENS creates a proxy instance wrapping the abigen interface to the ENS contract
 // using the transaction options passed as first argument, it sets up a session
 func NewENS(transactOpts *bind.TransactOpts, contractAddr common.Address, contractBackend bind.ContractBackend) *ENS {
-	ens, err := contract.NewENS(contractAddr, contractBackend)
+	ens, err := contract.NewResolver(contractAddr, contractBackend)
 	if err != nil {
 		glog.V(logger.Debug).Infof("error setting up name server on %v, skipping: %v", contractAddr.Hex(), err)
 	}
 	return &ENS{
-		&contract.ENSSession{
+		&contract.ResolverSession{
 			Contract:     ens,
 			TransactOpts: *transactOpts,
 		},
+		contractBackend,
 	}
-}
-
-// Register(name, hash )
-//involves sending a transaction (sent by sender specified as From of Transact)
-func (self *ENS) Register(name string, hash common.Hash) (*types.Transaction, error) {
-	namehash := crypto.Sha3Hash([]byte(name))
-	owner, err := self.Owners(namehash)
-	if err != nil {
-		return nil, fmt.Errorf("error registering '%s': %v", name, err)
-	}
-	if (owner != common.Address{} && owner != self.TransactOpts.From) {
-		return nil, fmt.Errorf("error registering '%s': already set as %", name)
-	}
-	glog.V(logger.Debug).Infof("[ENS]: host '%s' (hash: '%v') to be registered as '%v'", name, namehash.Hex(), hash.Hex())
-	return self.Set(namehash, hash)
-}
-
-func (self *ENS) WhoseIs(name string) (common.Address, error) {
-	namehash := crypto.Sha3Hash([]byte(name))
-	return self.Owners(namehash)
 }
 
 // resolve is a non-tranasctional call, returns hash as storage.Key
 func (self *ENS) Resolve(hostPort string) (storage.Key, error) {
 	host := hostPort
-	var version *big.Int
 	parts := domainAndVersion.Split(host, 3)
 	if len(parts) > 1 && parts[1] != "" {
 		host = parts[0]
-		version = common.Big(parts[1])
 	}
-	hash := crypto.Sha3Hash([]byte(host))
-	_ = version
-	// hash, err = self.registrar.Resolver(version).HashToHash(hostHash)
-	hash, err := self.Registry(hash)
+	return self.resolveName(host)
+}
+
+func (self *ENS) resolveName(host string) (storage.Key, error) {
+	labels := strings.Split(host, ".")
+	resolver := self
+	var nodeId [12]byte;
+
+	for i := len(labels) - 1; i >= 0; i-- {
+		hash := crypto.Sha3Hash([]byte(labels[i]))
+		ret, err := resolver.session.FindResolver(nodeId, hash)
+		if err != nil {
+			return nil, fmt.Errorf("error resolving label '%v' of '%v': %v", labels[i], host, err)
+		}
+		if ret.Rcode != 0 {
+			return nil, fmt.Errorf("error resolving label '%v' of '%v': got response code %v", labels[i], host, ret.Rcode)
+		}
+		nodeId = ret.Rnode;
+		resolver = NewENS(&resolver.session.TransactOpts, ret.Raddress, resolver.contractBackend)
+	}
+
+	ret, err := resolver.session.Resolve(nodeId, qtypeChash, 0)
 	if err != nil {
-		return nil, fmt.Errorf("error resolving '%v': %v", hash.Hex(), err)
+		return nil, fmt.Errorf("error looking up RR on '%v': %v", host, err)
 	}
-	if (hash == common.Hash{}) {
-		return nil, fmt.Errorf("unable to resolve '%v': not found", hash)
+	if ret.Rcode != 0 {
+		return nil, fmt.Errorf("error looking up RR on '%v': got response code %v", host, ret.Rcode)
 	}
-	contentHash := storage.Key(hash.Bytes())
-	glog.V(logger.Debug).Infof("[ENS] resolve host '%v' to contentHash: '%v'", hash, contentHash)
-	return contentHash, nil
+	return storage.Key(ret.Data[:]), nil
 }
