ddf7514fac7e96eabb223424fe943d9c5aa3f629
lash <dev@holbrook.no>
2017-12-18 20:39:21 +0100

    swarm/storage: Add test startup and teardown

--- swarm/storage/resource_test.go
+++ swarm/storage/resource_test.go
@@ -2,8 +2,10 @@ package storage
 
 import (
 	"bytes"
+	"crypto/ecdsa"
 	"crypto/rand"
 	"encoding/binary"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -21,6 +23,7 @@ import (
 
 var (
 	blockCount = uint64(4200)
+	cleanF     func()
 )
 
 func init() {
@@ -36,128 +39,87 @@ func (r *FakeRPC) BlockNumber() (string, error) {
 }
 
 func TestResourceValidContent(t *testing.T) {
-	// privkey for signing updates
-	privkey, err := crypto.GenerateKey()
+
+	rh, privkey, _, err := setupTest()
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 
-	// temp datadir
-	datadir, err := ioutil.TempDir("", "rh")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(datadir)
-
-	rh, err := NewResourceHandler(privkey, datadir, &testCloudStore{}, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	// create a new resource
 	validname, err := idna.ToASCII("føø.bar")
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 
+	// generate a hash for block 4200 version 1
 	key := rh.resourceHash(ens.EnsNode(validname), 4200, 1)
 	chunk := NewChunk(key, nil)
 
+	// generate some bogus data for the chunk and sign it
 	data := make([]byte, 8)
 	_, err = rand.Read(data)
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 	rh.hasher.Reset()
 	rh.hasher.Write(data)
 	datahash := rh.hasher.Sum(nil)
 	sig, err := crypto.Sign(datahash, privkey)
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 
+	// put sig and data in the chunk
 	chunk.SData = make([]byte, 8+SIGNATURE_LENGTH)
 	copy(chunk.SData[:SIGNATURE_LENGTH], sig)
 	copy(chunk.SData[SIGNATURE_LENGTH:], data)
 
+	// check that we can recover the owner account from the update chunk's signature
 	// TODO: change this to verifyContent on ENS integration
 	recoveredaddress, err := rh.getContentAccount(chunk.SData)
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 	originaladdress := crypto.PubkeyToAddress(privkey.PublicKey)
 
 	if recoveredaddress != originaladdress {
-		t.Fatalf("addresses dont match: %x != %x", originaladdress, recoveredaddress)
+		teardownTest(t, fmt.Sprintf("addresses dont match: %x != %x", originaladdress, recoveredaddress))
 	}
+	teardownTest(t, "")
 }
 
 func TestResourceHandler(t *testing.T) {
 
-	// privkey for signing updates
-	privkey, err := crypto.GenerateKey()
+	rh, privkey, datadir, err := setupTest()
 	if err != nil {
-		t.Fatal(err)
-	}
-
-	// temp datadir
-	datadir, err := ioutil.TempDir("", "rh")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(datadir)
-
-	// starting the whole stack just to get blocknumbers is too cumbersome
-	// so we fake the rpc server to get blocknumbers for testing
-	ipcpath := filepath.Join(datadir, "test.ipc")
-	ipcl, err := rpc.CreateIPCListener(ipcpath)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rpcserver := rpc.NewServer()
-	rpcserver.RegisterName("eth", &FakeRPC{
-		blockcount: &blockCount,
-	})
-	go func() {
-		rpcserver.ServeListener(ipcl)
-	}()
-	defer rpcserver.Stop()
-
-	// connect to fake rpc
-	rpcclient, err := rpc.Dial(ipcpath)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rh, err := NewResourceHandler(privkey, datadir, &testCloudStore{}, rpcclient)
-	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 
 	// create a new resource
 	resourcename := "føø.bar"
 	resourcevalidname, err := idna.ToASCII(resourcename)
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 	resourcefrequency := uint64(42)
 	_, err = rh.NewResource(resourcename, resourcefrequency)
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 
 	// check that the new resource is stored correctly
 	namehash := ens.EnsNode(resourcevalidname)
 	chunk, err := rh.ChunkStore.(*resourceChunkStore).localStore.(*LocalStore).memStore.Get(Key(namehash[:]))
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 	startblocknumber := binary.LittleEndian.Uint64(chunk.SData[8:16])
 	chunkfrequency := binary.LittleEndian.Uint64(chunk.SData[16:])
 	if startblocknumber != blockCount {
-		t.Fatalf("stored block number %d does not match provided block number %d", startblocknumber, blockCount)
+		teardownTest(t, fmt.Sprintf("stored block number %d does not match provided block number %d", startblocknumber, blockCount))
 	}
 	if chunkfrequency != resourcefrequency {
-		t.Fatalf("stored frequency %d does not match provided frequency %d", chunkfrequency, resourcefrequency)
+		teardownTest(t, fmt.Sprintf("stored frequency %d does not match provided frequency %d", chunkfrequency, resourcefrequency))
 	}
 
 	// update halfway to first period
@@ -165,28 +127,28 @@ func TestResourceHandler(t *testing.T) {
 	blockCount = startblocknumber + (resourcefrequency / 2)
 	key["blinky"], err = rh.Update(resourcename, []byte("blinky"))
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 
 	// update on first period
 	blockCount = startblocknumber + resourcefrequency
 	key["pinky"], err = rh.Update(resourcename, []byte("pinky"))
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 
 	// update on second period
 	blockCount = startblocknumber + (resourcefrequency * 2)
 	key["inky"], err = rh.Update(resourcename, []byte("inky"))
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 
 	// update just after second period
 	blockCount = startblocknumber + (resourcefrequency * 2) + 1
 	key["clyde"], err = rh.Update(resourcename, []byte("clyde"))
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 	time.Sleep(time.Second)
 	rh.Close()
@@ -195,41 +157,106 @@ func TestResourceHandler(t *testing.T) {
 	// it will match on second iteration startblocknumber + (resourcefrequency * 3)
 	blockCount = startblocknumber + (resourcefrequency * 4)
 
-	rh2, err := NewResourceHandler(privkey, datadir, &testCloudStore{}, rpcclient)
+	rh2, err := NewResourceHandler(privkey, datadir, &testCloudStore{}, rh.ethapi)
 	_, err = rh2.OpenResource(resourcename, true)
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 
 	// last update should be "clyde", version two, blockheight startblocknumber + (resourcefrequency * 3)
 	if !bytes.Equal(rh2.resources[resourcename].data, []byte("clyde")) {
-		t.Fatalf("resource data was %v, expected %v", rh2.resources[resourcename].data, []byte("clyde"))
+		teardownTest(t, fmt.Sprintf("resource data was %v, expected %v", rh2.resources[resourcename].data, []byte("clyde")))
 	}
 	if rh2.resources[resourcename].version != 2 {
-		t.Fatalf("resource version was %d, expected 2", rh2.resources[resourcename].version)
+		teardownTest(t, fmt.Sprintf("resource version was %d, expected 2", rh2.resources[resourcename].version))
 	}
 	if rh2.resources[resourcename].lastBlock != startblocknumber+(resourcefrequency*3) {
-		t.Fatalf("resource blockheight was %d, expected %d", rh2.resources[resourcename].lastBlock, startblocknumber+(resourcefrequency*3))
+		teardownTest(t, fmt.Sprintf("resource blockheight was %d, expected %d", rh2.resources[resourcename].lastBlock, startblocknumber+(resourcefrequency*3)))
 	}
 
 	rsrc, err := NewResource(resourcename, startblocknumber, resourcefrequency)
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 	err = rh2.SetResource(rsrc, true)
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 	resource, err := rh2.OpenResource(resourcename, false) // if key is specified, refresh is implicit
 	if err != nil {
-		t.Fatal(err)
+		teardownTest(t, err.Error())
 	}
 
 	// check data
 	if !bytes.Equal(resource.data, []byte("clyde")) {
-		t.Fatalf("resource data was %v, expected %v", rh2.resources[resourcename].data, []byte("clyde"))
+		teardownTest(t, fmt.Sprintf("resource data was %v, expected %v", rh2.resources[resourcename].data, []byte("clyde")))
+	}
+	teardownTest(t, "")
+
+}
+
+func setupTest() (rh *ResourceHandler, privkey *ecdsa.PrivateKey, datadir string, err error) {
+
+	var fsClean func()
+	var rpcClean func()
+	cleanF = func() {
+		if fsClean != nil {
+			fsClean()
+		}
+		if rpcClean != nil {
+			rpcClean()
+		}
 	}
 
+	// privkey for signing updates
+	privkey, err = crypto.GenerateKey()
+	if err != nil {
+		return
+	}
+
+	// temp datadir
+	datadir, err = ioutil.TempDir("", "rh")
+	if err != nil {
+		return
+	}
+	fsClean = func() {
+		os.RemoveAll(datadir)
+	}
+
+	// starting the whole stack just to get blocknumbers is too cumbersome
+	// so we fake the rpc server to get blocknumbers for testing
+	ipcpath := filepath.Join(datadir, "test.ipc")
+	ipcl, err := rpc.CreateIPCListener(ipcpath)
+	if err != nil {
+		return
+	}
+	rpcserver := rpc.NewServer()
+	rpcserver.RegisterName("eth", &FakeRPC{
+		blockcount: &blockCount,
+	})
+	go func() {
+		rpcserver.ServeListener(ipcl)
+	}()
+	rpcClean = func() {
+		rpcserver.Stop()
+	}
+
+	// connect to fake rpc
+	rpcclient, err := rpc.Dial(ipcpath)
+	if err != nil {
+		return
+	}
+
+	rh, err = NewResourceHandler(privkey, datadir, &testCloudStore{}, rpcclient)
+	return
+
+}
+
+func teardownTest(t *testing.T, errstr string) {
+	cleanF()
+	if errstr != "" {
+		t.Fatal(errstr)
+	}
 }
 
 type testCloudStore struct {
