22ac2c6c99d4dcd17decb1e3faf483b3be42349b
lash <dev@holbrook.no>
2018-04-27 14:28:28 +0200

    mobile: attempt string only param for setpeerpublic key pss wrap

--- mobile/pss.go
+++ mobile/pss.go
@@ -13,11 +13,14 @@ type Pss struct {
 	ps pss.Pss
 }
 
-func (ps *Pss) SetPeerPublicKey(pubKeyHex string, topic [4]byte, addr []byte) error {
+//func (ps *Pss) SetPeerPublicKey(pubKeyHex string, topic [4]byte, addr []byte) error {
+func (ps *Pss) SetPeerPublicKey(pubKeyHex string) error {
 	pubBytes, err := hexutil.Decode(pubKeyHex)
 	if err != nil {
 		return err
 	}
+	addr := []byte{}
 	pssaddr := pss.PssAddress(addr)
-	return ps.ps.SetPeerPublicKey(crypto.ToECDSAPub(pubBytes), topic, &pssaddr)
+	//return ps.ps.SetPeerPublicKey(crypto.ToECDSAPub(pubBytes), topic, &pssaddr)
+	return ps.ps.SetPeerPublicKey(crypto.ToECDSAPub(pubBytes), pss.Topic{}, &pssaddr)
 }
