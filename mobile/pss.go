bab6de529f2cced19ca7c575b935298a42621693
lash <dev@holbrook.no>
2018-04-27 15:01:12 +0200

    mobile: Make wrapper function with only byte params

--- mobile/pss.go
+++ mobile/pss.go
@@ -2,11 +2,8 @@ package geth
 
 import (
 	"errors"
-	//"crypto/ecdsa"
-	//	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/swarm/pss"
 )
 
@@ -14,51 +11,12 @@ type Pss struct {
 	ps pss.Pss
 }
 
-//func (ps *Pss) SetPeerPublicKey(pubKeyHex string, topic [4]byte, addr []byte) error {
-func (ps *Pss) SetPeerPublicKey(pubKeyHex string, topic string, addr string) error {
-	topicFixedBytes := [4]byte{}
-	topicBytes, err := hexutil.Decode(topic)
-	if err != nil {
-		return err
-	} else if len(topicBytes) != 4 {
-		return errors.New("topic must be four bytes")
+func (ps *Pss) SetPeerPublicKey(pubKey []byte, topic []byte, addr []byte) error {
+	if len(topic) != 4 {
+		return errors.New("mensch!! topic muss 4 bytes sein!")
 	}
-	copy(topicFixedBytes[:], topicBytes[:])
-
-	addrBytes, err := hexutil.Decode(addr)
-	if err != nil {
-		return err
-	}
-	return ps.setPeerPublicKey(pubKeyHex, topicFixedBytes, addrBytes)
-}
-
-func (ps *Pss) SetPeerPublicKeyTopicBytes(pubKeyHex string, topic [4]byte, addr string) error {
-	addrBytes, err := hexutil.Decode(addr)
-	if err != nil {
-		return err
-	}
-	return ps.setPeerPublicKey(pubKeyHex, topic, addrBytes)
-
-}
-
-func (ps *Pss) SetPeerPublicKeyAddrBytes(pubKeyHex string, topic string, addr []byte) error {
-	topicFixedBytes := [4]byte{}
-	topicBytes, err := hexutil.Decode(topic)
-	if err != nil {
-		return err
-	} else if len(topicBytes) != 4 {
-		return errors.New("topic must be four bytes")
-	}
-	copy(topicFixedBytes[:], topicBytes[:])
-	return ps.setPeerPublicKey(pubKeyHex, topicFixedBytes, addr)
-
-}
-
-func (ps *Pss) setPeerPublicKey(pubKeyHex string, topic [4]byte, addr []byte) error {
-	pubBytes, err := hexutil.Decode(pubKeyHex)
-	if err != nil {
-		return err
-	}
+	topicType := pss.Topic{}
+	copy(topicType[:], topic)
 	pssaddr := pss.PssAddress(addr)
-	return ps.ps.SetPeerPublicKey(crypto.ToECDSAPub(pubBytes), pss.Topic{}, &pssaddr)
+	return ps.ps.SetPeerPublicKey(crypto.ToECDSAPub(pubKey), pss.Topic{}, &pssaddr)
 }
