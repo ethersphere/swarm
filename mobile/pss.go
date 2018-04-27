bba9e9829766516d066420d15f0a84fa1945e0b6
lash <dev@holbrook.no>
2018-04-27 14:45:32 +0200

    mobile: Try different signature alternatives for pss wrap

--- mobile/pss.go
+++ mobile/pss.go
@@ -1,6 +1,7 @@
 package geth
 
 import (
+	"errors"
 	//"crypto/ecdsa"
 	//	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -14,13 +15,50 @@ type Pss struct {
 }
 
 //func (ps *Pss) SetPeerPublicKey(pubKeyHex string, topic [4]byte, addr []byte) error {
-func (ps *Pss) SetPeerPublicKey(pubKeyHex string) error {
+func (ps *Pss) SetPeerPublicKey(pubKeyHex string, topic string, addr string) error {
+	topicFixedBytes := [4]byte{}
+	topicBytes, err := hexutil.Decode(topic)
+	if err != nil {
+		return err
+	} else if len(topicBytes) != 4 {
+		return errors.New("topic must be four bytes")
+	}
+	copy(topicFixedBytes[:], topicBytes[:])
+
+	addrBytes, err := hexutil.Decode(addr)
+	if err != nil {
+		return err
+	}
+	return ps.setPeerPublicKey(pubKeyHex, topicFixedBytes, addrBytes)
+}
+
+func (ps *Pss) SetPeerPublicKeyTopicBytes(pubKeyHex string, topic [4]byte, addr string) error {
+	addrBytes, err := hexutil.Decode(addr)
+	if err != nil {
+		return err
+	}
+	return ps.setPeerPublicKey(pubKeyHex, topic, addrBytes)
+
+}
+
+func (ps *Pss) SetPeerPublicKeyAddrBytes(pubKeyHex string, topic string, addr []byte) error {
+	topicFixedBytes := [4]byte{}
+	topicBytes, err := hexutil.Decode(topic)
+	if err != nil {
+		return err
+	} else if len(topicBytes) != 4 {
+		return errors.New("topic must be four bytes")
+	}
+	copy(topicFixedBytes[:], topicBytes[:])
+	return ps.setPeerPublicKey(pubKeyHex, topicFixedBytes, addr)
+
+}
+
+func (ps *Pss) setPeerPublicKey(pubKeyHex string, topic [4]byte, addr []byte) error {
 	pubBytes, err := hexutil.Decode(pubKeyHex)
 	if err != nil {
 		return err
 	}
-	addr := []byte{}
 	pssaddr := pss.PssAddress(addr)
-	//return ps.ps.SetPeerPublicKey(crypto.ToECDSAPub(pubBytes), topic, &pssaddr)
 	return ps.ps.SetPeerPublicKey(crypto.ToECDSAPub(pubBytes), pss.Topic{}, &pssaddr)
 }
