909548f06df0148b664b4cd54c8279ec42a9493e
lash <dev@holbrook.no>
2018-04-27 15:40:19 +0200

    mobile: Add SendAsym method

--- mobile/pss.go
+++ mobile/pss.go
@@ -11,12 +11,21 @@ type Pss struct {
 	ps pss.Pss
 }
 
+func makeTopic(topic []byte) pss.Topic {
+	topicType := pss.Topic{}
+	copy(topicType[:], topic)
+	return topicType
+}
+
 func (ps *Pss) SetPeerPublicKey(pubKey []byte, topic []byte, addr []byte) error {
 	if len(topic) != 4 {
 		return errors.New("mensch!! topic muss 4 bytes sein!")
 	}
-	topicType := pss.Topic{}
-	copy(topicType[:], topic)
 	pssaddr := pss.PssAddress(addr)
-	return ps.ps.SetPeerPublicKey(crypto.ToECDSAPub(pubKey), pss.Topic{}, &pssaddr)
+	return ps.ps.SetPeerPublicKey(crypto.ToECDSAPub(pubKey), makeTopic(topic), &pssaddr)
+}
+
+func (ps *Pss) SendAsym(pubKeyId string, topic []byte, addr []byte) error {
+	pssaddr := pss.PssAddress(addr)
+	return ps.ps.SendAsym(pubKeyId, makeTopic(topic), pssaddr)
 }
