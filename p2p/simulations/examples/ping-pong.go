96fa2c50e6e87f68b4a10ef77d7132449ff4641a
Anton Evangelatov <anton.evangelatov@gmail.com>
2018-05-11 14:31:05 +0200

    p2p/sim/examples: opentracing experiment

--- p2p/simulations/examples/ping-pong.go
+++ p2p/simulations/examples/ping-pong.go
@@ -17,8 +17,11 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -32,6 +35,10 @@ import (
 	"github.com/ethereum/go-ethereum/p2p/simulations"
 	"github.com/ethereum/go-ethereum/p2p/simulations/adapters"
 	"github.com/ethereum/go-ethereum/rpc"
+	opentracing "github.com/opentracing/opentracing-go"
+	jaeger "github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	jaegerlog "github.com/uber/jaeger-client-go/log"
 )
 
 var adapterType = flag.String("adapter", "sim", `node adapter to use (one of "sim", "exec" or "docker")`)
@@ -98,6 +105,7 @@ func main() {
 type pingPongService struct {
 	id       discover.NodeID
 	log      log.Logger
+	closer   io.Closer
 	received int64
 }
 
@@ -123,11 +131,15 @@ func (p *pingPongService) APIs() []rpc.API {
 }
 
 func (p *pingPongService) Start(server *p2p.Server) error {
+	p.closer = initTracer()
+
 	p.log.Info("ping-pong service starting")
 	return nil
 }
 
 func (p *pingPongService) Stop() error {
+	defer p.closer.Close()
+
 	p.log.Info("ping-pong service stopping")
 	return nil
 }
@@ -145,6 +157,18 @@ const (
 	pongMsgCode
 )
 
+type PingMsgPayload struct {
+	MarshalledContext []byte
+	Payload           string
+	Version           uint64
+}
+
+type PongMsgPayload struct {
+	MarshalledContext []byte
+	Payload           string
+	Version           uint64
+}
+
 // Run implements the ping-pong protocol which sends ping messages to the peer
 // at 10s intervals, and responds to pings with pong messages.
 func (p *pingPongService) Run(peer *p2p.Peer, rw p2p.MsgReadWriter) error {
@@ -152,12 +176,38 @@ func (p *pingPongService) Run(peer *p2p.Peer, rw p2p.MsgReadWriter) error {
 
 	errC := make(chan error)
 	go func() {
-		for range time.Tick(10 * time.Second) {
-			log.Info("sending ping")
-			if err := p2p.Send(rw, pingMsgCode, "PING"); err != nil {
+		for range time.Tick(5 * time.Second) {
+
+			tracer := opentracing.GlobalTracer()
+
+			sp := tracer.StartSpan("ping-operation")
+
+			var b bytes.Buffer
+			writer := bufio.NewWriter(&b)
+
+			err := tracer.Inject(
+				sp.Context(),
+				opentracing.Binary,
+				writer)
+			if err != nil {
+				panic(err)
+			}
+
+			writer.Flush()
+
+			pmp := &PingMsgPayload{
+				b.Bytes(),
+				"PING",
+				7,
+			}
+
+			log.Info("sending ping with ctx", "ctx", fmt.Sprintf("%x", b.Bytes()))
+			if err := p2p.Send(rw, pingMsgCode, pmp); err != nil {
 				errC <- err
 				return
 			}
+
+			sp.Finish()
 		}
 	}()
 	go func() {
@@ -167,18 +217,98 @@ func (p *pingPongService) Run(peer *p2p.Peer, rw p2p.MsgReadWriter) error {
 				errC <- err
 				return
 			}
-			payload, err := ioutil.ReadAll(msg.Payload)
-			if err != nil {
-				errC <- err
-				return
-			}
-			log.Info("received message", "msg.code", msg.Code, "msg.payload", string(payload))
+
+			log.Info("received message", "msg.code", msg.Code)
 			atomic.AddInt64(&p.received, 1)
 			if msg.Code == pingMsgCode {
+				log.Info("received message ping")
+
+				pmp := &PingMsgPayload{}
+
+				if err := msg.Decode(pmp); err != nil {
+					errC <- err
+				}
+
+				log.Info("received ping with ctx", "ctx", fmt.Sprintf("%x", pmp.MarshalledContext))
+
+				tracer := opentracing.GlobalTracer()
+
+				ctx, err := tracer.Extract(
+					opentracing.Binary,
+					bytes.NewReader(pmp.MarshalledContext))
+				if err != nil {
+					panic(err)
+				}
+
+				sp := tracer.StartSpan(
+					"pong-operation",
+					opentracing.ChildOf(ctx))
+
+				var b bytes.Buffer
+				writer := bufio.NewWriter(&b)
+
+				err = tracer.Inject(
+					sp.Context(),
+					opentracing.Binary,
+					writer)
+				if err != nil {
+					panic(err)
+				}
+
+				writer.Flush()
+
+				pop := &PongMsgPayload{
+					b.Bytes(),
+					"PONG",
+					8,
+				}
+
 				log.Info("sending pong")
-				go p2p.Send(rw, pongMsgCode, "PONG")
+				go p2p.Send(rw, pongMsgCode, pop)
+				sp.Finish()
+			}
+
+			if msg.Code == pongMsgCode {
+				log.Info("received message pong")
 			}
 		}
 	}()
 	return <-errC
+}
+
+func initTracer() (closer io.Closer) {
+	fmt.Println("==== init Tracer ====")
+	// Sample configuration for testing. Use constant sampling to sample every trace
+	// and enable LogSpan to log every span via configured Logger.
+	cfg := jaegercfg.Configuration{
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			LogSpans:            true,
+			BufferFlushInterval: 1 * time.Second,
+			LocalAgentHostPort:  "0.0.0.0:6831",
+		},
+	}
+
+	// Example logger and metrics factory. Use github.com/uber/jaeger-client-go/log
+	// and github.com/uber/jaeger-lib/metrics respectively to bind to real logging and metrics
+	// frameworks.
+	jLogger := jaegerlog.StdLogger
+	//jMetricsFactory := metrics.NullFactory
+
+	// Initialize tracer with a logger and a metrics factory
+	closer, err := cfg.InitGlobalTracer(
+		"ping-pong",
+		jaegercfg.Logger(jLogger),
+		//jaegercfg.Metrics(jMetricsFactory),
+		//jaegercfg.Observer(rpcmetrics.NewObserver(jMetricsFactory, rpcmetrics.DefaultNameNormalizer)),
+	)
+	if err != nil {
+		panic(fmt.Sprintf("Could not initialize jaeger tracer: %s", err.Error()))
+		return nil
+	}
+
+	return closer
 }
