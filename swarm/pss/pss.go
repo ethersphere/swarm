1c48b42ddb1e5d6b941630c69e18f86a2fe06662
lash <dev@holbrook.no>
2018-05-21 21:03:49 +0200

    swarm/pss: Add generic notify package to pss

--- swarm/pss/pss.go
+++ swarm/pss/pss.go
@@ -454,8 +454,8 @@ func (p *Pss) SetPeerPublicKey(pubkey *ecdsa.PublicKey, topic Topic, address *Ps
 }
 
 // Automatically generate a new symkey for a topic and address hint
-func (p *Pss) generateSymmetricKey(topic Topic, address *PssAddress, addToCache bool) (string, error) {
-	keyid, err := p.w.GenerateSymKey()
+func (p *Pss) GenerateSymmetricKey(topic Topic, address *PssAddress, addToCache bool) (string, error) {
+	keyid, err := self.w.GenerateSymKey()
 	if err != nil {
 		return "", err
 	}
