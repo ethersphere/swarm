078a14f992b107e23e8c66d90a7cf5654879c822
lash <dev@holbrook.no>
2018-05-22 12:33:30 +0200

    swarm/pss: Add godoc for notify

--- swarm/pss/notify/notify.go
+++ swarm/pss/notify/notify.go
@@ -16,9 +16,16 @@ import (
 )
 
 const (
+	// sent from requester to updater to request start of notifications
 	MsgCodeStart = iota
+
+	// sent from updater to requester, contains a notification plus a new symkey to replace the old
 	MsgCodeNotifyWithKey
+
+	// sent from updater to requester, contains a notification
 	MsgCodeNotify
+
+	// sent from requester to updater to request stop of notifications (currently unused)
 	MsgCodeStop
 	MsgCodeMax
 )
@@ -30,11 +37,13 @@ const (
 )
 
 var (
+	// control topic is used before symmetric key issuance completes
 	controlTopic = pss.Topic{0x00, 0x00, 0x00, 0x01}
 )
 
 // when code is MsgCodeStart, Payload is address
-// when code is MsgCodeNotify, Payload is symkey. If len = 0, keep old key
+// when code is MsgCodeNotifyWithKey, Payload is notification | symkey
+// when code is MsgCodeNotify, Payload is notification
 // when code is MsgCodeStop, Payload is address
 type Msg struct {
 	Name    string
@@ -42,6 +51,7 @@ type Msg struct {
 	Code    byte
 }
 
+// Serialize create a message byte string ready to send through pss
 func (self *Msg) Serialize() []byte {
 	b := bytes.NewBuffer(nil)
 	b.Write([]byte{self.Code})
@@ -55,6 +65,7 @@ func (self *Msg) Serialize() []byte {
 	return b.Bytes()
 }
 
+// reverses Serialize
 func deserializeMsg(msgbytes []byte) (*Msg, error) {
 	msg := &Msg{
 		Code: msgbytes[0],
@@ -70,27 +81,32 @@ func deserializeMsg(msgbytes []byte) (*Msg, error) {
 }
 
 // a notifier has one sendmux entry for each address space it sends messages to
-type sendMux struct {
+type sendBin struct {
 	address  pss.PssAddress
 	symKeyId string
 	count    int
 }
 
+// represents a single notification service
+// only subscription address bins that match the address of a notification client have entries. The threshold sets the amount of bytes each address bin uses.
+// every notification has a topic used for pss transfer of symmetrically encrypted notifications
+// contentFunc is the callback to get initial update data from the notifications service provider
 type notifier struct {
-	muxes       []*sendMux
+	bins        []*sendBin
 	topic       pss.Topic
 	threshold   int
 	contentFunc func(string) ([]byte, error)
-	updateFunc  func(string, []byte)
 	mu          sync.Mutex
 }
 
+// Controller is the interface to control, add and remove notification services
 type Controller struct {
 	pss       *pss.Pss
 	notifiers map[string]*notifier
 	handlers  map[string]func(string, []byte) error
 }
 
+// NewController creates a new Controller object
 func NewController(ps *pss.Pss) *Controller {
 	ctrl := &Controller{
 		pss:       ps,
@@ -101,11 +117,17 @@ func NewController(ps *pss.Pss) *Controller {
 	return ctrl
 }
 
+// IsActive is used to check if a notification service exists for a specified id string
+// Returns true if exists, false if not
 func (self *Controller) IsActive(name string) bool {
 	_, ok := self.notifiers[name]
 	return ok
 }
 
+// Request is used by a client to request notifications from a notification service provider
+// It will create a MsgCodeStart message and send asymmetrically to the provider using its public key and routing address
+// The handler function is a callback that will be called when notifications are recieved
+// Fails if the request pss cannot be sent
 func (self *Controller) Request(name string, pubkey *ecdsa.PublicKey, address pss.PssAddress, handler func(string, []byte) error) error {
 	msg := &Msg{
 		Name:    name,
@@ -118,9 +140,12 @@ func (self *Controller) Request(name string, pubkey *ecdsa.PublicKey, address ps
 	return self.pss.SendAsym(pubkeyid, controlTopic, msg.Serialize())
 }
 
+// NewNotifier is used by a notification service provider to create a new notification service
+// It takes a name as identifier for the resource, a threshold indicating the granularity of the subscription address bin, and a callback for getting the latest update
+// Fails if a notifier already is registered on the name
 func (self *Controller) NewNotifier(name string, threshold int, contentFunc func(string) ([]byte, error)) error {
 	if self.IsActive(name) {
-		return fmt.Errorf("%s already exists in controller", name)
+		return fmt.Errorf("Notification service %s already exists in controller", name)
 	}
 	self.notifiers[name] = &notifier{
 		topic:       pss.BytesToTopic([]byte(name)),
@@ -130,13 +155,20 @@ func (self *Controller) NewNotifier(name string, threshold int, contentFunc func
 	return nil
 }
 
+// Notify is called by a notification service provider to issue a new notification
+// It takes the name of the notification service the data to be sent.
+// It fails if a notifier with this name does not exist.
+// Note that it does NOT fail on failure to send a message
 func (self *Controller) Notify(name string, data []byte) error {
+	if !self.IsActive(name) {
+		return fmt.Errorf("Notification service %s doesn't exist", name)
+	}
 	msg := &Msg{
 		Name:    name,
 		Payload: data,
 		Code:    MsgCodeNotify,
 	}
-	for _, m := range self.notifiers[name].muxes {
+	for _, m := range self.notifiers[name].bins {
 		log.Debug("sending pss notify", "name", name, "addr", fmt.Sprintf("%x", m.address), "topic", fmt.Sprintf("%x", self.notifiers[name].topic), "data", data)
 		err := self.pss.SendSym(m.symKeyId, self.notifiers[name].topic, msg.Serialize())
 		if err != nil {
@@ -146,12 +178,13 @@ func (self *Controller) Notify(name string, data []byte) error {
 	return nil
 }
 
+// adds an client address to the corresponding address bin in the notifier service
 func (self *Controller) addToNotifier(name string, address pss.PssAddress) (string, error) {
 	notifier, ok := self.notifiers[name]
 	if !ok {
 		return "", fmt.Errorf("Unknown notifier %s", name)
 	}
-	for _, m := range notifier.muxes {
+	for _, m := range notifier.bins {
 		if bytes.Equal(address, m.address) {
 			m.count++
 			return m.symKeyId, nil
@@ -161,7 +194,7 @@ func (self *Controller) addToNotifier(name string, address pss.PssAddress) (stri
 	if err != nil {
 		return "", fmt.Errorf("Generate symkey fail: %v", err)
 	}
-	notifier.muxes = append(notifier.muxes, &sendMux{
+	notifier.bins = append(notifier.bins, &sendBin{
 		address:  address,
 		symKeyId: symKeyId,
 		count:    1,
@@ -169,6 +202,8 @@ func (self *Controller) addToNotifier(name string, address pss.PssAddress) (stri
 	return symKeyId, nil
 }
 
+// Handler is the pss topic handler to be used to process notification service messages
+// It should be registered in the pss of both to any notification service provides and clients using the service
 func (self *Controller) Handler(smsg []byte, p *p2p.Peer, asymmetric bool, keyid string) error {
 	log.Debug("notify controller handler", "keyid", keyid)
 
