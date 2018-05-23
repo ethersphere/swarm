baaffdf8472506869e60d8ac1b3b50415edc9257
stolen <sheled@gmail.com>
2018-05-23 03:03:18 +0200

    root dir bug

--- swarm/fuse/fuse_dir.go
+++ swarm/fuse/fuse_dir.go
@@ -151,9 +151,9 @@ func (sd *SwarmDir) Remove(ctx context.Context, req *fuse.RemoveRequest) error {
 
 func (sd *SwarmDir) Mkdir(ctx context.Context, req *fuse.MkdirRequest) (fs.Node, error) {
 	log.Debug(fmt.Sprintf("swarmfs Mkdir: path: %s, req.Name: %s", sd.path, req.Name))
-	joinedPath := filepath.Join(sd.path, req.Name)
-	newDir := NewSwarmDir(joinedPath, sd.mountInfo)
-
+	// joinedPath := filepath.Join(sd.path, req.Name)
+	// newDir := NewSwarmDir(joinedPath, sd.mountInfo)
+	newDir := NewSwarmDir(req.Name, sd.mountInfo)
 	sd.lock.Lock()
 	defer sd.lock.Unlock()
 	sd.directories = append(sd.directories, newDir)
