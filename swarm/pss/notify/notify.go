f6a88a08f8bbc7978753f85b9ce98ba0d18c1f95
lash <dev@holbrook.no>
2018-05-24 11:47:48 +0200

    swarm/pss: Change self to c Controller receiver

--- swarm/pss/notify/notify.go
+++ swarm/pss/notify/notify.go
@@ -57,8 +57,8 @@ func NewMsg(code byte, name string, payload []byte) *Msg {
 	}
 }
 
-func (self *Msg) GetName() string {
-	return string(self.Name)
+func (c *Msg) GetName() string {
+	return string(c.Name)
 }
 
 // a notifier has one sendmux entry for each address space it sends messages to
@@ -100,14 +100,14 @@ func NewController(ps *pss.Pss) *Controller {
 
 // IsActive is used to check if a notification service exists for a specified id string
 // Returns true if exists, false if not
-func (self *Controller) IsActive(name string) bool {
-	self.mu.Lock()
-	defer self.mu.Unlock()
-	return self.isActive(name)
+func (c *Controller) IsActive(name string) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.isActive(name)
 }
 
-func (self *Controller) isActive(name string) bool {
-	_, ok := self.notifiers[name]
+func (c *Controller) isActive(name string) bool {
+	_, ok := c.notifiers[name]
 	return ok
 }
 
@@ -115,30 +115,30 @@ func (self *Controller) isActive(name string) bool {
 // It will create a MsgCodeStart message and send asymmetrically to the provider using its public key and routing address
 // The handler function is a callback that will be called when notifications are recieved
 // Fails if the request pss cannot be sent or if the update message could not be serialized
-func (self *Controller) Request(name string, pubkey *ecdsa.PublicKey, address pss.PssAddress, handler func(string, []byte) error) error {
-	self.mu.Lock()
-	defer self.mu.Unlock()
-	msg := NewMsg(MsgCodeStart, name, self.pss.BaseAddr())
-	self.handlers[name] = handler
-	self.pss.SetPeerPublicKey(pubkey, controlTopic, &address)
+func (c *Controller) Request(name string, pubkey *ecdsa.PublicKey, address pss.PssAddress, handler func(string, []byte) error) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	msg := NewMsg(MsgCodeStart, name, c.pss.BaseAddr())
+	c.handlers[name] = handler
+	c.pss.SetPeerPublicKey(pubkey, controlTopic, &address)
 	pubkeyid := common.ToHex(crypto.FromECDSAPub(pubkey))
 	smsg, err := rlp.EncodeToBytes(msg)
 	if err != nil {
 		return fmt.Errorf("message could not be serialized: %v", err)
 	}
-	return self.pss.SendAsym(pubkeyid, controlTopic, smsg)
+	return c.pss.SendAsym(pubkeyid, controlTopic, smsg)
 }
 
 // NewNotifier is used by a notification service provider to create a new notification service
 // It takes a name as identifier for the resource, a threshold indicating the granularity of the subscription address bin, and a callback for getting the latest update
 // Fails if a notifier already is registered on the name
-func (self *Controller) NewNotifier(name string, threshold int, contentFunc func(string) ([]byte, error)) error {
-	self.mu.Lock()
-	defer self.mu.Unlock()
-	if self.isActive(name) {
+func (c *Controller) NewNotifier(name string, threshold int, contentFunc func(string) ([]byte, error)) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.isActive(name) {
 		return fmt.Errorf("Notification service %s already exists in controller", name)
 	}
-	self.notifiers[name] = &notifier{
+	c.notifiers[name] = &notifier{
 		topic:       pss.BytesToTopic([]byte(name)),
 		threshold:   threshold,
 		contentFunc: contentFunc,
@@ -150,20 +150,20 @@ func (self *Controller) NewNotifier(name string, threshold int, contentFunc func
 // It takes the name of the notification service the data to be sent.
 // It fails if a notifier with this name does not exist or if data could not be serialized
 // Note that it does NOT fail on failure to send a message
-func (self *Controller) Notify(name string, data []byte) error {
-	self.mu.Lock()
-	defer self.mu.Unlock()
-	if !self.isActive(name) {
+func (c *Controller) Notify(name string, data []byte) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if !c.isActive(name) {
 		return fmt.Errorf("Notification service %s doesn't exist", name)
 	}
 	msg := NewMsg(MsgCodeNotify, name, data)
-	for _, m := range self.notifiers[name].bins {
-		log.Debug("sending pss notify", "name", name, "addr", fmt.Sprintf("%x", m.address), "topic", fmt.Sprintf("%x", self.notifiers[name].topic), "data", data)
+	for _, m := range c.notifiers[name].bins {
+		log.Debug("sending pss notify", "name", name, "addr", fmt.Sprintf("%x", m.address), "topic", fmt.Sprintf("%x", c.notifiers[name].topic), "data", data)
 		smsg, err := rlp.EncodeToBytes(msg)
 		if err != nil {
 			return fmt.Errorf("Failed to serialize message: %v", err)
 		}
-		err = self.pss.SendSym(m.symKeyId, self.notifiers[name].topic, smsg)
+		err = c.pss.SendSym(m.symKeyId, c.notifiers[name].topic, smsg)
 		if err != nil {
 			log.Warn("Failed to send notify to addr %x: %v", m.address, err)
 		}
@@ -173,8 +173,8 @@ func (self *Controller) Notify(name string, data []byte) error {
 
 // adds an client address to the corresponding address bin in the notifier service
 // this method is not concurrency safe
-func (self *Controller) addToNotifier(name string, address pss.PssAddress) (string, error) {
-	notifier, ok := self.notifiers[name]
+func (c *Controller) addToNotifier(name string, address pss.PssAddress) (string, error) {
+	notifier, ok := c.notifiers[name]
 	if !ok {
 		return "", fmt.Errorf("Unknown notifier %s", name)
 	}
@@ -184,7 +184,7 @@ func (self *Controller) addToNotifier(name string, address pss.PssAddress) (stri
 			return m.symKeyId, nil
 		}
 	}
-	symKeyId, err := self.pss.GenerateSymmetricKey(notifier.topic, &address, false)
+	symKeyId, err := c.pss.GenerateSymmetricKey(notifier.topic, &address, false)
 	if err != nil {
 		return "", fmt.Errorf("Generate symkey fail: %v", err)
 	}
@@ -198,9 +198,9 @@ func (self *Controller) addToNotifier(name string, address pss.PssAddress) (stri
 
 // Handler is the pss topic handler to be used to process notification service messages
 // It should be registered in the pss of both to any notification service provides and clients using the service
-func (self *Controller) Handler(smsg []byte, p *p2p.Peer, asymmetric bool, keyid string) error {
-	self.mu.Lock()
-	defer self.mu.Unlock()
+func (c *Controller) Handler(smsg []byte, p *p2p.Peer, asymmetric bool, keyid string) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	log.Debug("notify controller handler", "keyid", keyid)
 
 	// see if the message is valid
@@ -215,35 +215,35 @@ func (self *Controller) Handler(smsg []byte, p *p2p.Peer, asymmetric bool, keyid
 		pubkey := crypto.ToECDSAPub(common.FromHex(keyid))
 
 		// if name is not registered for notifications we will not react
-		if _, ok := self.notifiers[msg.GetName()]; !ok {
+		if _, ok := c.notifiers[msg.GetName()]; !ok {
 			return fmt.Errorf("Subscribe attempted on unknown resource %s", msg.GetName())
 		}
 
 		// parse the address from the message and truncate if longer than our mux threshold
 		address := msg.Payload
-		if len(msg.Payload) > self.notifiers[msg.GetName()].threshold {
-			address = address[:self.notifiers[msg.GetName()].threshold]
+		if len(msg.Payload) > c.notifiers[msg.GetName()].threshold {
+			address = address[:c.notifiers[msg.GetName()].threshold]
 		}
 
 		// add the address to the notification list
-		symKeyId, err := self.addToNotifier(msg.GetName(), address)
+		symKeyId, err := c.addToNotifier(msg.GetName(), address)
 		if err != nil {
 			return fmt.Errorf("add address to notifier fail: %v", err)
 		}
-		symkey, err := self.pss.GetSymmetricKey(symKeyId)
+		symkey, err := c.pss.GetSymmetricKey(symKeyId)
 		if err != nil {
 			return fmt.Errorf("retrieve symkey fail: %v", err)
 		}
 
 		// add to address book for send initial notify
 		pssaddr := pss.PssAddress(address)
-		err = self.pss.SetPeerPublicKey(pubkey, controlTopic, &pssaddr)
+		err = c.pss.SetPeerPublicKey(pubkey, controlTopic, &pssaddr)
 		if err != nil {
 			return fmt.Errorf("add pss peer for reply fail: %v", err)
 		}
 
 		// send initial notify, will contain symkey to use for consecutive messages
-		notify, err := self.notifiers[msg.GetName()].contentFunc(msg.GetName())
+		notify, err := c.notifiers[msg.GetName()].contentFunc(msg.GetName())
 		if err != nil {
 			return fmt.Errorf("retrieve current update from source fail: %v", err)
 		}
@@ -254,7 +254,7 @@ func (self *Controller) Handler(smsg []byte, p *p2p.Peer, asymmetric bool, keyid
 		if err != nil {
 			return fmt.Errorf("reply message could not be serialized: %v", err)
 		}
-		err = self.pss.SendAsym(keyid, controlTopic, sReplyMsg)
+		err = c.pss.SendAsym(keyid, controlTopic, sReplyMsg)
 		if err != nil {
 			return fmt.Errorf("send start reply fail: %v", err)
 		}
@@ -263,11 +263,11 @@ func (self *Controller) Handler(smsg []byte, p *p2p.Peer, asymmetric bool, keyid
 		topic := pss.BytesToTopic(msg.Name)
 		// \TODO keep track of and add actual address
 		updaterAddr := pss.PssAddress([]byte{})
-		self.pss.SetSymmetricKey(symkey, topic, &updaterAddr, true)
-		self.pss.Register(&topic, self.Handler)
-		return self.handlers[msg.GetName()](msg.GetName(), msg.Payload)
+		c.pss.SetSymmetricKey(symkey, topic, &updaterAddr, true)
+		c.pss.Register(&topic, c.Handler)
+		return c.handlers[msg.GetName()](msg.GetName(), msg.Payload)
 	case MsgCodeNotify:
-		return self.handlers[msg.GetName()](msg.GetName(), msg.Payload)
+		return c.handlers[msg.GetName()](msg.GetName(), msg.Payload)
 	default:
 		return fmt.Errorf("Invalid message code: %d", msg.Code)
 	}
