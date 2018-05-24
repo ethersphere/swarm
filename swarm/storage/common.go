49693914ff7cdd0b234d9072a4c6c73ef55fa57d
lash <dev@holbrook.no>
2018-05-24 11:17:40 +0200

    swarm/storage: godoc

--- swarm/storage/common.go
+++ swarm/storage/common.go
@@ -2,16 +2,22 @@ package storage
 
 import (
 	"sync"
+
+	"github.com/ethereum/go-ethereum/log"
 )
 
-// put to localstore and wait for stored channel
-// does not check delivery error state
+// PutChunks adds chunks  to localstore
+// It waits for receive on the stored channel
+// It logs but does not fail on delivery error
 func PutChunks(store *LocalStore, chunks ...*Chunk) {
 	wg := sync.WaitGroup{}
 	wg.Add(len(chunks))
 	go func() {
 		for _, c := range chunks {
 			<-c.dbStoredC
+			if err := c.GetErrored(); err != nil {
+				log.Error("chunk store fail", "err", err, "key", c.Key)
+			}
 			wg.Done()
 		}
 	}()
