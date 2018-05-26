1a44f0ac91a9fafaa0b289bc19f52a42499b83f7
Elad <theman@elad.im>
2018-05-26 02:01:08 +0200

    bmt, pot: move into swarm package (#603)
    * bmt, pot: moved into swarm package

--- swarm/storage/types.go
+++ swarm/storage/types.go
@@ -26,10 +26,10 @@ import (
 	"io"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/bmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto/sha3"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/swarm/bmt"
 )
 
 const MaxPO = 16
