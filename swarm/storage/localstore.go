2d73a2e05e7615888e2d43aaa6602b65282c84c0
stolen <sheled@gmail.com>
2018-05-29 15:24:56 +0200

    removed the channel close

--- swarm/storage/localstore.go
+++ swarm/storage/localstore.go
@@ -130,10 +130,9 @@ func (self *LocalStore) Put(chunk *Chunk) {
 
 	self.memStore.Put(chunk)
 
-	if memChunk != nil && memChunk.ReqC != nil {
-		close(memChunk.ReqC)
-		memChunk.ReqC = nil
-	}
+	// if memChunk != nil && memChunk.ReqC != nil {
+	// 	close(memChunk.ReqC)
+	// }
 
 	self.DbStore.Put(chunk)
 
