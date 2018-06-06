ace1aed7ba5322123e6cfe7ba49668045907633e
lash <dev@holbrook.no>
2018-06-06 17:39:21 +0200

    swarm/storage/mru: Change index from name to metadata chunk addr

--- swarm/storage/mru/resource.go
+++ swarm/storage/mru/resource.go
@@ -116,8 +116,10 @@ type Signature [signatureLength]byte
 // If Limit is set to true then Max defines the amount of hops that can be performed
 // \TODO this is redundant, just use uint32 with 0 for unlimited hops
 type LookupParams struct {
-	Limit bool
-	Max   uint32
+	Period  uint32
+	Version uint32
+	Root    storage.Address
+	Limit   uint32
 }
 
 // encapsulates an specific resource update. When synced it contains the most recent
@@ -194,24 +196,19 @@ type Handler struct {
 	hashPool        sync.Pool
 	resourceLock    sync.RWMutex
 	storeTimeout    time.Duration
-	queryMaxPeriods *LookupParams
+	queryMaxPeriods uint32
 }
 
 // HandlerParams pass parameters to the Handler constructor NewHandler
 // Signer and HeaderGetter are mandatory parameters
 type HandlerParams struct {
-	QueryMaxPeriods *LookupParams
+	QueryMaxPeriods uint32
 	Signer          Signer
 	HeaderGetter    headerGetter
 }
 
 // NewHandler creates a new Mutable Resource API
 func NewHandler(params *HandlerParams) (*Handler, error) {
-	if params.QueryMaxPeriods == nil {
-		params.QueryMaxPeriods = &LookupParams{
-			Limit: false,
-		}
-	}
 	if params.Signer == nil {
 		return nil, NewError(ErrInit, "Signer cannot be nil")
 	}
@@ -369,7 +366,7 @@ func (h *Handler) NewWithAddress(ctx context.Context, name string, frequency uin
 		updated:    time.Now(),
 	}
 	copy(rsrc.owner[:], ownerAddr[:])
-	h.set(nameHash.Hex(), rsrc)
+	h.set(chunk.Addr.Hex(), rsrc)
 
 	return chunk.Addr, rsrc, nil
 }
@@ -412,18 +409,18 @@ func (h *Handler) newMetaChunk(name string, startBlock uint64, frequency uint64,
 // metadata chunk.
 // It is the callers responsibility to make sure that this chunk exists (if the resource
 // update root data was retrieved externally, it typically doesn't)
-func (h *Handler) LookupVersionByName(ctx context.Context, name string, period uint32, version uint32, refresh bool, maxLookup *LookupParams) (*resource, error) {
-	return h.LookupVersion(ctx, ens.EnsNode(name), period, version, refresh, maxLookup)
-}
+//func (h *Handler) LookupVersionByName(ctx context.Context, name string, period uint32, version uint32, refresh bool, maxLookup *LookupParams) (*resource, error) {
+//	return h.LookupVersion(ctx, ens.EnsNode(name), period, version, refresh, maxLookup)
+//}
 
 // LookupVersion performs the same action as LookupVersionByName, but takes the name hash instead of the cleartext name as identifier
-func (h *Handler) LookupVersion(ctx context.Context, nameHash common.Hash, period uint32, version uint32, refresh bool, maxLookup *LookupParams) (*resource, error) {
-	rsrc := h.get(nameHash.Hex())
-	if rsrc == nil {
-		return nil, NewError(ErrNothingToReturn, "resource not loaded")
-	}
-	return h.lookup(rsrc, period, version, refresh, maxLookup)
-}
+//func (h *Handler) LookupVersion(ctx context.Context, params *LookupParams) (*resource, Error) { //nameHash common.Hash, period uint32, version uint32, refresh bool, maxLookup *LookupParams) (*resource, error) {
+//	rsrc := h.get(params.Root)
+//	if rsrc == nil {
+//		return nil, NewError(ErrNothingToReturn, "resource not loaded")
+//	}
+//	return h.lookup(rsrc, params.Period, params.Version, param.Limit)
+//}
 
 // LookupHistoricalByName etrieves the latest version of the resource update identified by `name`
 // at the specified block height
@@ -431,18 +428,19 @@ func (h *Handler) LookupVersion(ctx context.Context, nameHash common.Hash, perio
 // successfully retrieved version is copied to the corresponding resources map entry
 // and returned.
 // See also (*Handler).LookupVersionByName
-func (h *Handler) LookupHistoricalByName(ctx context.Context, name string, period uint32, refresh bool, maxLookup *LookupParams) (*resource, error) {
-	return h.LookupHistorical(ctx, ens.EnsNode(name), period, refresh, maxLookup)
-}
+//func (h *Handler) LookupHistoricalByName(ctx context.Context, name string, period uint32, refresh bool, maxLookup *LookupParams) (*resource, error) {
+//	return h.LookupHistorical(ctx, ens.EnsNode(name), period, refresh, maxLookup)
+//}
 
 // LookupHistorical performs the same action as LookupHistoricalByName, but takes the name hash instead of the cleartext name as identifier
-func (h *Handler) LookupHistorical(ctx context.Context, nameHash common.Hash, period uint32, refresh bool, maxLookup *LookupParams) (*resource, error) {
-	rsrc := h.get(nameHash.Hex())
-	if rsrc == nil {
-		return nil, NewError(ErrNothingToReturn, "resource not loaded")
-	}
-	return h.lookup(rsrc, period, 0, refresh, maxLookup)
-}
+//func (h *Handler) LookupHistorical(ctx context.Context, nameHash common.Hash, period uint32, refresh bool, maxLookup *LookupParams) (*resource, error) {
+//func (h *Handler) LookupHistorical(ctx context.Context, params *LookupParams) (*resource, error) { //name string, period uint32, refresh bool, maxLookup *LookupParams) (*resource, error) {
+//	rsrc := h.get(params.Root)
+//	if rsrc == nil {
+//		return nil, NewError(ErrNothingToReturn, "resource not loaded")
+//	}
+//	return h.lookup(rsrc, rsrc.Period, 0, param.Limit)
+//}
 
 // LookupLatestByName retrieves the latest version of the resource update identified by `name`
 // at the next update block height
@@ -450,40 +448,45 @@ func (h *Handler) LookupHistorical(ctx context.Context, nameHash common.Hash, pe
 // tries the corresponding keys of each previous period until one is found
 // (or startBlock is reached, in which case there are no updates).
 // See also (*Handler).LookupHistoricalByName
-func (h *Handler) LookupLatestByName(ctx context.Context, name string, refresh bool, maxLookup *LookupParams) (*resource, error) {
-	return h.LookupLatest(ctx, ens.EnsNode(name), refresh, maxLookup)
-}
+//func (h *Handler) LookupLatestByName(ctx context.Context, name string, refresh bool, maxLookup *LookupParams) (*resource, error) {
+//	return h.LookupLatest(ctx, ens.EnsNode(name), refresh, maxLookup)
+//}
 
 // LookupLatest performs the same action as LookupLatestByName, but takes the name hash instead of the cleartext name as identifier
-func (h *Handler) LookupLatest(ctx context.Context, nameHash common.Hash, refresh bool, maxLookup *LookupParams) (*resource, error) {
+//func (h *Handler) LookupLatest(ctx context.Context, nameHash common.Hash, refresh bool, maxLookup *LookupParams) (*resource, error) {
+func (h *Handler) Lookup(ctx context.Context, params *LookupParams) (*resource, error) { //nameHash common.Hash, refresh bool, maxLookup *LookupParams) (*resource, error) {
 
-	// get our blockheight at this time and the next block of the update period
-	rsrc := h.get(nameHash.Hex())
+	rsrc := h.get(params.Root.Hex())
 	if rsrc == nil {
 		return nil, NewError(ErrNothingToReturn, "resource not loaded")
 	}
-	currentblock, err := h.getBlock(ctx, rsrc.name)
-	if err != nil {
-		return nil, err
+	if params.Period == 0 {
+		// get our blockheight at this time and the next block of the update period
+		currentblock, err := h.getBlock(ctx, rsrc.name)
+		if err != nil {
+			return nil, err
+		}
+		var period uint32
+		period, err = getNextPeriod(rsrc.startBlock, currentblock, rsrc.frequency)
+		if err != nil {
+			return nil, err
+		}
+		params.Period = period
 	}
-	nextperiod, err := getNextPeriod(rsrc.startBlock, currentblock, rsrc.frequency)
-	if err != nil {
-		return nil, err
-	}
-	return h.lookup(rsrc, nextperiod, 0, refresh, maxLookup)
+	return h.lookup(rsrc, params.Period, params.Version, params.Limit)
 }
 
 // LookupPreviousByName returns the resource before the one currently loaded in the resource index
 // This is useful where resource updates are used incrementally in contrast to
 // merely replacing content.
 // Requires a synced resource object
-func (h *Handler) LookupPreviousByName(ctx context.Context, name string, maxLookup *LookupParams) (*resource, error) {
-	return h.LookupPrevious(ctx, ens.EnsNode(name), maxLookup)
-}
+//func (h *Handler) LookupPreviousByName(ctx context.Context, name string, maxLookup *LookupParams) (*resource, error) {
+//	return h.LookupPrevious(ctx, ens.EnsNode(name), maxLookup)
+//}
 
 // LookupPrevious performs the same action as LookupPreviousByName, but takes the name hash instead of the cleartext name as identifier
-func (h *Handler) LookupPrevious(ctx context.Context, nameHash common.Hash, maxLookup *LookupParams) (*resource, error) {
-	rsrc := h.get(nameHash.Hex())
+func (h *Handler) LookupPrevious(ctx context.Context, params *LookupParams) (*resource, error) { //nameHash common.Hash, maxLookup *LookupParams) (*resource, error) {
+	rsrc := h.get(params.Root.Hex())
 	if rsrc == nil {
 		return nil, NewError(ErrNothingToReturn, "resource not loaded")
 	}
@@ -500,11 +503,11 @@ func (h *Handler) LookupPrevious(ctx context.Context, nameHash common.Hash, maxL
 		rsrc.version = 0
 		rsrc.lastPeriod--
 	}
-	return h.lookup(rsrc, rsrc.lastPeriod, rsrc.version, false, maxLookup)
+	return h.lookup(rsrc, rsrc.lastPeriod, rsrc.version, params.Limit)
 }
 
 // base code for public lookup methods
-func (h *Handler) lookup(rsrc *resource, period uint32, version uint32, refresh bool, maxLookup *LookupParams) (*resource, error) {
+func (h *Handler) lookup(rsrc *resource, period uint32, version uint32, limit uint32) (*resource, error) {
 
 	// we can't look for anything without a store
 	if h.chunkStore == nil {
@@ -526,13 +529,13 @@ func (h *Handler) lookup(rsrc *resource, period uint32, version uint32, refresh 
 	}
 
 	var hops uint32
-	if maxLookup == nil {
-		maxLookup = h.queryMaxPeriods
+	if limit == 0 {
+		limit = h.queryMaxPeriods
 	}
-	log.Trace("resource lookup", "period", period, "version", version, "limit", maxLookup.Limit, "max", maxLookup.Max)
+	log.Trace("resource lookup", "period", period, "version", version, "limit", limit)
 	for period > 0 {
-		if maxLookup.Limit && hops > maxLookup.Max {
-			return nil, NewError(ErrPeriodDepth, fmt.Sprintf("Lookup exceeded max period hops (%d)", maxLookup.Max))
+		if limit != 0 && hops > limit {
+			return nil, NewError(ErrPeriodDepth, fmt.Sprintf("Lookup exceeded max period hops (%d)", limit))
 		}
 		key := h.resourceHash(period, version, rsrc.nameHash, rsrc.owner)
 		chunk, err := h.chunkStore.GetWithTimeout(context.TODO(), key, defaultRetrieveTimeout)
@@ -578,7 +581,7 @@ func (h *Handler) Load(ctx context.Context, addr storage.Address) (*resource, er
 	rsrc := &resource{}
 	rsrc.UnmarshalBinary(chunk.SData[:])
 	rsrc.nameHash = ens.EnsNode(rsrc.name)
-	h.set(rsrc.nameHash.Hex(), rsrc)
+	h.set(addr.Hex(), rsrc)
 	log.Trace("resource index load", "rootkey", addr, "name", rsrc.name, "namehash", rsrc.nameHash, "startblock", rsrc.startBlock, "frequency", rsrc.frequency)
 	return rsrc, nil
 }
@@ -613,7 +616,7 @@ func (h *Handler) updateIndex(rsrc *resource, chunk *storage.Chunk) (*resource, 
 	rsrc.Reader = bytes.NewReader(rsrc.data)
 	copy(rsrc.data, data)
 	log.Debug(" synced", "name", rsrc.name, "key", chunk.Addr, "period", rsrc.lastPeriod, "version", rsrc.version)
-	h.set(rsrc.nameHash.Hex(), rsrc)
+	h.set(chunk.Addr.Hex(), rsrc)
 	return rsrc, nil
 }
 
@@ -729,24 +732,24 @@ func (h *Handler) parseUpdate(chunkdata []byte) (*Signature, uint32, uint32, str
 // It is the caller's responsibility to make sure that this data is not stale.
 //
 // A resource update cannot span chunks, and thus has max length 4096
-func (h *Handler) UpdateMultihash(ctx context.Context, name string, data []byte) (storage.Address, error) {
+func (h *Handler) UpdateMultihash(ctx context.Context, addr storage.Address, data []byte) (storage.Address, error) {
 	// \TODO perhaps this check should be in newUpdateChunk()
 	if _, _, err := multihash.GetMultihashLength(data); err != nil {
 		return nil, NewError(ErrNothingToReturn, err.Error())
 	}
-	return h.update(ctx, name, data, true)
+	return h.update(ctx, addr, data, true)
 }
 
 // Update adds an actual data update
 // Uses the Mutable Resource metadata currently loaded in the resources map entry.
 // It is the caller's responsibility to make sure that this data is not stale.
 // Note that a Mutable Resource update cannot span chunks, and thus has a MAX NET LENGTH 4096, INCLUDING update header data and signature. An error will be returned if the total length of the chunk payload will exceed this limit.
-func (h *Handler) Update(ctx context.Context, name string, data []byte) (storage.Address, error) {
-	return h.update(ctx, name, data, false)
+func (h *Handler) Update(ctx context.Context, addr storage.Address, data []byte) (storage.Address, error) {
+	return h.update(ctx, addr, data, false)
 }
 
 // create and commit an update
-func (h *Handler) update(ctx context.Context, name string, data []byte, multihash bool) (storage.Address, error) {
+func (h *Handler) update(ctx context.Context, addr storage.Address, data []byte, multihash bool) (storage.Address, error) {
 
 	// zero-length updates are bogus
 	if len(data) == 0 {
@@ -759,24 +762,26 @@ func (h *Handler) update(ctx context.Context, name string, data []byte, multihas
 	}
 
 	// get the cached information
-	nameHash := ens.EnsNode(name)
-	nameHashHex := nameHash.Hex()
-	rsrc := h.get(nameHashHex)
+	//nameHash := ens.EnsNode(name)
+	//nameHashHex := nameHash.Hex()
+	//rsrc := h.get(nameHashHex)
+	addrHex := addr.Hex()
+	rsrc := h.get(addrHex)
 	if rsrc == nil {
-		return nil, NewError(ErrNotFound, fmt.Sprintf(" object '%s' not in index", name))
+		return nil, NewError(ErrNotFound, fmt.Sprintf(" object '%s' not in index", rsrc.name))
 	} else if !rsrc.isSynced() {
 		return nil, NewError(ErrNotSynced, " object not in sync")
 	}
 
 	// an update can be only one chunk long; data length less header and signature data
 	// 12 = length of header and data length fields (2xuint16) plus period and frequency value fields (2xuint32)
-	datalimit := h.chunkSize() - int64(signatureLength-len(name)-12)
+	datalimit := h.chunkSize() - int64(signatureLength-len(rsrc.name)-12)
 	if int64(len(data)) > datalimit {
 		return nil, NewError(ErrDataOverflow, fmt.Sprintf("Data overflow: %d / %d bytes", len(data), datalimit))
 	}
 
 	// get our blockheight at this time and the next block of the update period
-	currentblock, err := h.getBlock(ctx, name)
+	currentblock, err := h.getBlock(ctx, rsrc.name)
 	if err != nil {
 		return nil, NewError(ErrIO, fmt.Sprintf("Could not get block height: %v", err))
 	}
@@ -788,7 +793,7 @@ func (h *Handler) update(ctx context.Context, name string, data []byte, multihas
 	// if we already have an update for this block then increment version
 	// resource object MUST be in sync for version to be correct, but we checked this earlier in the method already
 	var version uint32
-	if h.hasUpdate(nameHashHex, nextperiod) {
+	if h.hasUpdate(addr, nextperiod) {
 		version = rsrc.version
 	}
 	version++
@@ -820,11 +825,11 @@ func (h *Handler) update(ctx context.Context, name string, data []byte, multihas
 	if !multihash {
 		datalength = len(data)
 	}
-	chunk := newUpdateChunk(key, signature, nextperiod, version, name, data, datalength)
+	chunk := newUpdateChunk(key, signature, nextperiod, version, rsrc.name, data, datalength)
 
 	// send the chunk
 	h.chunkStore.Put(ctx, chunk)
-	log.Trace("resource update", "name", name, "key", key, "currentblock", currentblock, "lastperiod", nextperiod, "version", version, "data", chunk.SData, "multihash", multihash)
+	log.Trace("resource update", "name", rsrc.name, "key", key, "currentblock", currentblock, "lastperiod", nextperiod, "version", version, "data", chunk.SData, "multihash", multihash)
 
 	// update our resources map entry and return the new key
 	rsrc.lastPeriod = nextperiod
@@ -879,8 +884,8 @@ func (h *Handler) resourceHash(period uint32, version uint32, nameHash common.Ha
 }
 
 // Checks if we already have an update on this resource, according to the value in the current state of the resource index
-func (h *Handler) hasUpdate(nameHash string, period uint32) bool {
-	return h.resources[nameHash].lastPeriod == period
+func (h *Handler) hasUpdate(addr storage.Address, period uint32) bool {
+	return h.resources[addr.Hex()].lastPeriod == period
 }
 
 func getAddressFromDataSig(datahash common.Hash, signature Signature) (common.Address, error) {
