f10cb2c6061ae9bc371738dfaf4bfafc238d1f42
Anton Evangelatov <anton.evangelatov@gmail.com>
2018-06-06 15:47:15 +0300

    swarm/log: log more useful logline

--- swarm/log/log.go
+++ swarm/log/log.go
@@ -8,35 +8,35 @@ import (
 // Warn is a convenient alias for log.Warn with stats
 func Warn(msg string, ctx ...interface{}) {
 	metrics.GetOrRegisterCounter("warn", nil).Inc(1)
-	l.Warn(msg, ctx...)
+	l.Output(msg, l.LvlWarn, 3, ctx...)
 }
 
 // Error is a convenient alias for log.Error with stats
 func Error(msg string, ctx ...interface{}) {
 	metrics.GetOrRegisterCounter("error", nil).Inc(1)
-	l.Error(msg, ctx...)
+	l.Output(msg, l.LvlError, 3, ctx...)
 }
 
 // Crit is a convenient alias for log.Crit with stats
 func Crit(msg string, ctx ...interface{}) {
 	metrics.GetOrRegisterCounter("crit", nil).Inc(1)
-	l.Crit(msg, ctx...)
+	l.Output(msg, l.LvlCrit, 3, ctx...)
 }
 
 // Info is a convenient alias for log.Info with stats
 func Info(msg string, ctx ...interface{}) {
 	metrics.GetOrRegisterCounter("info", nil).Inc(1)
-	l.Info(msg, ctx...)
+	l.Output(msg, l.LvlInfo, 3, ctx...)
 }
 
 // Debug is a convenient alias for log.Debug with stats
 func Debug(msg string, ctx ...interface{}) {
 	metrics.GetOrRegisterCounter("debug", nil).Inc(1)
-	l.Debug(msg, ctx...)
+	l.Output(msg, l.LvlDebug, 3, ctx...)
 }
 
 // Trace is a convenient alias for log.Trace with stats
 func Trace(msg string, ctx ...interface{}) {
 	metrics.GetOrRegisterCounter("trace", nil).Inc(1)
-	l.Trace(msg, ctx...)
+	l.Output(msg, l.LvlTrace, 3, ctx...)
 }
