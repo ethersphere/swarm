a2931245178a85e55e79fd10efb23a460daa4edc
Javier Peletier <jm@epiclabs.io>
2018-06-13 17:12:34 +0200

    swarm/storage/mru: Added tests  for UpdateRequest

--- swarm/storage/mru/resourcemetadata_test.go
+++ swarm/storage/mru/resourcemetadata_test.go
@@ -1,10 +1,24 @@
+// Copyright 2018 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 package mru
 
 import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func compareByteSliceToExpectedHex(t *testing.T, variableName string, actualValue []byte, expectedHex string) {
@@ -14,8 +28,7 @@ func compareByteSliceToExpectedHex(t *testing.T, variableName string, actualValu
 }
 
 func TestMarshallingAndUnmarshalling(t *testing.T) {
-	privkey, _ := crypto.HexToECDSA("facadefacadefacadefacadefacadefacadefacadefacadefacadefacadefaca")
-	ownerAddr := crypto.PubkeyToAddress(privkey.PublicKey)
+	ownerAddr := newCharlieSigner().Address()
 	metadata := resourceMetadata{
 		name:      "world news report, every hour, on the hour",
 		startTime: 1528880400,
