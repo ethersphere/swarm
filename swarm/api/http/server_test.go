c775529da534910ac11c07cc2d69721c0ea0ce01
Javier Peletier <jm@epiclabs.io>
2018-06-21 02:45:06 +0200

    swarm/storage/mru: Moved Signature to resource_sign. Check Sign errors in server tests

--- swarm/api/http/server_test.go
+++ swarm/api/http/server_test.go
@@ -138,7 +138,9 @@ func TestBzzResourceMultihash(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	updateRequest.Sign(signer)
+	if err := updateRequest.Sign(signer); err != nil {
+		t.Fatal(err)
+	}
 	log.Info("added data", "manifest", string(b), "data", common.ToHex(mh))
 
 	body, err := mru.EncodeUpdateRequest(updateRequest)
@@ -211,8 +213,9 @@ func TestBzzResource(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	updateRequest.Sign(signer)
-
+	if err := updateRequest.Sign(signer); err != nil {
+		t.Fatal(err)
+	}
 	body, err := mru.EncodeUpdateRequest(updateRequest)
 	if err != nil {
 		t.Fatal(err)
