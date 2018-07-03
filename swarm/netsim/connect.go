241803c0db53a7d330df3ca4675145f005103809
Janos Guljas <janos@resenje.org>
2018-07-03 17:33:38 +0200

    swarm/netsim: fix comments

--- swarm/netsim/connect.go
+++ swarm/netsim/connect.go
@@ -115,7 +115,7 @@ func (s *Simulation) ConnectNodesRing(ids []discover.NodeID) (err error) {
 	return s.connect(ids[l-1], ids[0])
 }
 
-// ConnectNodesStar connects all nodes in a start topology
+// ConnectNodesStar connects all nodes in a star topology
 // with the center at provided NodeID.
 // If ids argument is nil, all nodes that are up will be connected.
 func (s *Simulation) ConnectNodesStar(id discover.NodeID, ids []discover.NodeID) (err error) {
@@ -135,7 +135,7 @@ func (s *Simulation) ConnectNodesStar(id discover.NodeID, ids []discover.NodeID)
 	return nil
 }
 
-// ConnectNodesStar connects all nodes in a start topology
+// ConnectNodesStar connects all nodes in a star topology
 // with the center at already set pivot node.
 // If ids argument is nil, all nodes that are up will be connected.
 func (s *Simulation) ConnectNodesStarPivot(ids []discover.NodeID) (err error) {
