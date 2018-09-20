75ff8179e8b5c159d825731538655ef309c8c3f6
lash <dev@holbrook.no>
2018-09-20 13:22:02 +0200

    swarm/storage: Correct referencehasher

--- swarm/storage/filehasher_r.go
+++ swarm/storage/filehasher_r.go
@@ -32,20 +32,20 @@ func NewReferenceFileHasher(hasher *bmt.Hasher, branches int) *ReferenceFileHash
 	}
 }
 
-func (f *ReferenceFileHasher) getLevelsFromLength(l int) int {
+func getLevelsFromLength(l int, segmentSize int, branches int) int {
 	if l == 0 {
 		return 0
-	} else if l <= 4096 {
+	} else if l <= segmentSize*branches {
 		return 2
 	}
-	c := (l - 1) / (f.segmentSize)
+	c := (l - 1) / (segmentSize)
 
-	return int(math.Log(float64(c))/math.Log(float64(f.branches)) + 2)
+	return int(math.Log(float64(c))/math.Log(float64(branches)) + 2)
 }
 
 func (f *ReferenceFileHasher) Hash(r io.Reader, l int) common.Hash {
 	f.totalBytes = l
-	levelCount := f.getLevelsFromLength(l)
+	levelCount := getLevelsFromLength(l, f.segmentSize, f.branches)
 	log.Debug("level count", "l", levelCount, "b", f.branches, "c", l, "s", f.segmentSize)
 	bufLen := f.segmentSize
 	for i := 1; i < levelCount; i++ {
@@ -78,38 +78,59 @@ func (f *ReferenceFileHasher) Hash(r io.Reader, l int) common.Hash {
 	return common.BytesToHash(f.buffer[f.cursors[levelCount-1] : f.cursors[levelCount-1]+f.segmentSize])
 }
 
+// TODO: check length 0
 func (f *ReferenceFileHasher) write(b []byte, level int, end bool) bool {
+	log.Trace("write", "l", level, "len", len(b), "b", b, "end", end, "wbc", f.writeByteCount)
+
+	// copy data from buffer to current position of corresponding level in buffer
+	copy(f.buffer[f.cursors[level]*f.segmentSize:], b)
 	for i, l := range f.cursors {
 		log.Debug("cursor", "#", i, "pos", l)
 	}
-	log.Trace("write", "l", level, "len", len(b), "b", b, "end", end, "wbc", f.writeByteCount)
-	copy(f.buffer[f.cursors[level]*f.segmentSize:], b)
+
+	// if we are at the tree root the result will be in the first segmentSize bytes of the buffer. Return
 	if level == len(f.cursors)-1 {
 		return true
 	}
+
+	if end && level > 0 && f.cursors[level] == f.cursors[level+1] {
+		res := f.write(b, level+1, end)
+		return res
+	}
+	// increment the position of this level in buffer
 	f.cursors[level]++
 
+	// perform recursive writes down the tree if end of output or on batch boundary
 	var res bool
 	if f.cursors[level]-f.cursors[level+1] == f.branches || end {
+		if f.cursors[level] == f.cursors[level+1] && f.cursors[level] > 0 {
+			log.Debug("short return in write")
+			return true
+		}
+
+		// calculate what the potential span under this chunk will be
 		span := f.chunkSize
 		for i := 0; i < level; i++ {
 			span *= f.branches
 		}
+
+		// if we have a dangling chunk, simply pass it up
+		// calculate the data in this chunk (the data to be hashed)
 		var dataUnderSpan int
 		if end {
-			dataUnderSpan = f.totalBytes % span
+			dataUnderSpan = (f.totalBytes-1)%span + 1
 		} else {
 			dataUnderSpan = span
 		}
-		if end && dataUnderSpan == 0 {
-			dataUnderSpan = f.chunkSize
-		}
+
+		// calculate the actual data under this span
 		var hashDataSize int
 		if level == 0 {
 			hashDataSize = dataUnderSpan
 		} else {
 			hashDataSize = ((dataUnderSpan-1)/(span/f.branches) + 1) * f.segmentSize
 		}
+
 		meta := make([]byte, 8)
 		binary.LittleEndian.PutUint64(meta, uint64(dataUnderSpan))
 		f.hasher.ResetWithLength(meta)
