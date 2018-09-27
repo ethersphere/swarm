d655184101664c63206abab0433a34d8fe6cf301
lash <dev@holbrook.no>
2018-09-27 13:14:20 +0200

    swarm/storage: Add alt filehasher impl, ok up to chunk boundary

---
+++ swarm/storage/filehasher_alt.go
@@ -0,0 +1,231 @@
+package storage
+
+import (
+	"encoding/binary"
+	"sync"
+
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/swarm/bmt"
+)
+
+const (
+	altFileHasherMaxLevels = 9 // 22 zetabytes should be enough for anyone
+)
+
+type AltFileHasher struct {
+	branches    int
+	segmentSize int
+	hashers     [altFileHasherMaxLevels]bmt.SectionWriter
+	buffers     [altFileHasherMaxLevels - 1][]byte
+	levelCount  int
+	chunkSize   int
+	finished    bool
+	totalBytes  int
+	targetCount [altFileHasherMaxLevels - 1]int
+	writeCount  [altFileHasherMaxLevels]int
+	doneC       [altFileHasherMaxLevels]chan struct{}
+	wg          sync.WaitGroup                         // used when level done
+	lwg         [altFileHasherMaxLevels]sync.WaitGroup // used when busy hashing
+	lock        sync.Mutex
+}
+
+func NewAltFileHasher(hasherFunc func() bmt.SectionWriter, segmentSize int, branches int) *AltFileHasher {
+	f := &AltFileHasher{
+		branches:    branches,
+		segmentSize: segmentSize,
+		chunkSize:   branches * segmentSize,
+	}
+	for i := 0; i < altFileHasherMaxLevels-1; i++ {
+		f.buffers[i] = make([]byte, f.chunkSize)
+		f.hashers[i] = hasherFunc()
+		f.doneC[i] = make(chan struct{}, 1)
+	}
+	f.Reset()
+	return f
+}
+
+func (f *AltFileHasher) Reset() {
+	f.totalBytes = 0
+	f.levelCount = 0
+	f.wg.Add(altFileHasherMaxLevels)
+	for i := 0; i < altFileHasherMaxLevels; i++ {
+		if i > 0 {
+			f.targetCount[i-1] = 0
+		}
+		f.writeCount[i] = 0
+	}
+}
+
+func (f *AltFileHasher) isWriteFinished() bool {
+	var finished bool
+	f.lock.Lock()
+	finished = f.finished
+	f.lock.Unlock()
+	return finished
+}
+
+func (f *AltFileHasher) Finish(b []byte) []byte {
+	if b != nil {
+		f.totalBytes += len(b)
+	}
+	f.finished = true
+
+	// find our level height and release the unused levels
+	f.levelCount = getLevelsFromLength(f.totalBytes, f.segmentSize, f.branches)
+
+	log.Debug("finish set", "levelcount", f.levelCount)
+	for i := altFileHasherMaxLevels; i > f.levelCount; i-- {
+		log.Debug("purging unused level wg", "l", i)
+		f.wg.Done()
+	}
+
+	// calculate the amount of writes expected on each level
+	target := (f.totalBytes-1)/f.segmentSize + 1
+	for i := 1; i < f.levelCount; i++ {
+		target = (target-1)/f.branches + 1
+		f.targetCount[i] = target
+		log.Debug("setting targetcount", "l", i, "t", target)
+	}
+
+	// write and return result when we get it back
+	f.write(b, f.writeCount[0], 0)
+	f.wg.Wait()
+	return f.buffers[f.levelCount-1][:f.segmentSize]
+}
+
+func (f *AltFileHasher) Write(b []byte) {
+	f.totalBytes += len(b)
+	f.write(b, f.writeCount[0], 0)
+}
+
+func (f *AltFileHasher) getPotentialSpan(level int) int {
+	span := f.chunkSize
+	for i := 0; i < level; i++ {
+		span *= f.branches
+	}
+	return span
+}
+
+// TODO: check if length 0
+// TODO: log error if not end and len(b) < segmentsize
+// performs recursive hashing on complete batches or data end
+func (f *AltFileHasher) write(b []byte, offset int, level int) {
+
+	if b == nil {
+		log.Debug("write", "level", level, "offset", offset, "length", "nil")
+	} else {
+		l := 32
+		if len(b) < l {
+			l = len(b)
+		}
+		log.Debug("write", "level", level, "offset", offset, "length", len(b), "data", b[:l])
+	}
+
+	// top level then return
+	if level == f.levelCount-1 {
+		copy(f.buffers[level], b)
+		f.lock.Lock()
+		f.wg.Done()
+		f.lock.Unlock()
+		log.Debug("top done", "level", level)
+		return
+	}
+
+	// thread safe writecount
+	// b will never be nil except bottom level, which will have already been hashed if on chunk boundary
+	f.lock.Lock()
+	wc := f.writeCount[level]
+	f.lock.Unlock()
+
+	// only write if we have data
+	// data might be nil when upon write finish
+	if b != nil {
+		f.hashers[level].Write(offset%f.branches, b)
+		f.lock.Lock()
+		f.writeCount[level]++
+		wc = f.writeCount[level]
+		f.lock.Unlock()
+	} else if wc%f.branches == 0 {
+		f.lock.Lock()
+		f.wg.Done()
+		f.lock.Unlock()
+		f.doneC[level] <- struct{}{}
+		return
+	}
+
+	// execute the hasher if:
+	// - we are on a chunk edge
+	// - we are on the data level and writes are set to finished
+	// - we are above data level, writes are finished, and expected level write count is reached
+	executeHasher := false
+	if wc%f.branches == 0 {
+		log.Debug("executehasher", "reason", "edge", "level", level, "offset", offset)
+		executeHasher = true
+	} else if f.finished && level == 0 {
+		log.Debug("executehasher", "reason", "data done", "level", level, "offset", offset)
+		executeHasher = true
+	} else if f.finished && f.targetCount[level] > 0 && f.targetCount[level] == wc {
+		<-f.doneC[level-1]
+		log.Debug("executehasher", "reason", "target done", "level", level, "offset", offset)
+		executeHasher = true
+	}
+
+	if executeHasher {
+
+		f.lock.Lock()
+		f.lwg[level].Add(1)
+		f.lock.Unlock()
+
+		// calculate what the potential span under this chunk will be
+		span := f.getPotentialSpan(level)
+
+		// calculate the actual data under this span
+		// if data is fully written, the current chunk may be shorter than the span
+		var dataUnderSpan int
+		if f.isWriteFinished() {
+			dataUnderSpan = (f.totalBytes-1)%span + 1
+		} else {
+			dataUnderSpan = span
+		}
+
+		// calculate the length of the actual data in this chunk (the data to be hashed)
+		var hashDataSize int
+		if level == 0 {
+			hashDataSize = dataUnderSpan
+		} else {
+			hashDataSize = ((dataUnderSpan-1)/(span/f.branches) + 1) * f.segmentSize
+		}
+
+		// hash the chunk and write it to the current cursor position on the next level
+		meta := make([]byte, 8)
+		binary.LittleEndian.PutUint64(meta, uint64(dataUnderSpan))
+		log.Debug("hash", "level", level, "size", hashDataSize, "meta", meta, "wc", wc)
+		hashResult := f.hashers[level].Sum(nil, hashDataSize, meta)
+		f.hashers[level].Reset()
+		go func(level int, wc int, finished bool) {
+			f.lwg[level+1].Wait()
+			chunkWriteCount := wc % f.branches
+			parentOffset := (chunkWriteCount - 1) / f.branches
+			if (level == 0 && finished) || f.targetCount[level] == wc {
+				log.Debug("done", "level", level)
+				f.lock.Lock()
+				f.wg.Done()
+				f.lock.Unlock()
+				f.doneC[level] <- struct{}{}
+			}
+			f.write(hashResult, parentOffset, level+1)
+			f.lock.Lock()
+			f.lwg[level].Done()
+			f.lock.Unlock()
+		}(level, wc, f.finished)
+	}
+}
+
+func (f *AltFileHasher) wgDoneFunc(level int, prune bool) func() {
+	log.Warn("done", "level", level, "prune", prune)
+	return func() {
+		f.lock.Lock()
+		f.wg.Done()
+		f.lock.Unlock()
+	}
+}
--- swarm/storage/filehasher_test.go
+++ swarm/storage/filehasher_test.go
@@ -48,8 +48,11 @@ func TestReferenceFileHasher(t *testing.T) {
 		"59de730bf6c67a941f3b2ffa2f920acfaa1713695ad5deea12b4a121e5f23fa1",
 		"522194562123473dcfd7a457b18ee7dee8b7db70ed3cfa2b73f348a992fdfd3b",
 	}
-	dataLengths := []int{31, 32, 33, 63, 64, 65, chunkSize, chunkSize + 31, chunkSize + 32, chunkSize + 63, chunkSize + 64, chunkSize * 2, chunkSize*2 + 32, chunkSize * 128, chunkSize*128 + 31, chunkSize*128 + 32, chunkSize*128 + 64, chunkSize * 129, chunkSize * 130, chunkSize * 128 * 128}
+	//dataLengths := []int{31, 32, 33, 63, 64, 65, chunkSize, chunkSize + 31, chunkSize + 32, chunkSize + 63, chunkSize + 64, chunkSize * 2, chunkSize*2 + 32, chunkSize * 128, chunkSize*128 + 31, chunkSize*128 + 32, chunkSize*128 + 64, chunkSize * 129, chunkSize * 130, chunkSize * 128 * 128}
+	dataLengths := []int{31, 32, 33, 63, 64, 65, chunkSize, chunkSize + 31, chunkSize + 32, chunkSize + 63, chunkSize + 64, chunkSize * 2, chunkSize*2 + 32}
+	//dataLengths := []int{chunkSize + 32}
 	for i, dataLength := range dataLengths {
+		log.Info("start", "len", dataLength)
 		fh := NewReferenceFileHasher(h, 128)
 		_, data := generateSerialData(dataLength, 255, 0)
 		refHash := fh.Hash(bytes.NewReader(data), len(data)).Bytes()
@@ -58,7 +61,62 @@ func TestReferenceFileHasher(t *testing.T) {
 			mismatch++
 			eq = false
 		}
-		t.Logf("[%7d+%4d]\t%v\tref: %s\texpect: %x", dataLength/chunkSize, dataLength%chunkSize, eq, expected[i], refHash)
+		t.Logf("[%7d+%4d]\t%v\tref: %x\texpect: %s", dataLength/chunkSize, dataLength%chunkSize, eq, refHash, expected[i])
+	}
+	if mismatch > 0 {
+		t.Fatalf("mismatches: %d/%d", mismatch, len(dataLengths))
+	}
+}
+
+func TestAltFileHasher(t *testing.T) {
+	var mismatch int
+	chunkSize := 128 * 32
+	expected := []string{
+		"ece86edb20669cc60d142789d464d57bdf5e33cb789d443f608cbd81cfa5697d",
+		"0be77f0bb7abc9cd0abed640ee29849a3072ccfd1020019fe03658c38f087e02",
+		"3463b46d4f9d5bfcbf9a23224d635e51896c1daef7d225b86679db17c5fd868e",
+		"95510c2ff18276ed94be2160aed4e69c9116573b6f69faaeed1b426fea6a3db8",
+		"490072cc55b8ad381335ff882ac51303cc069cbcb8d8d3f7aa152d9c617829fe",
+		"541552bae05e9a63a6cb561f69edf36ffe073e441667dbf7a0e9a3864bb744ea",
+		"c10090961e7682a10890c334d759a28426647141213abda93b096b892824d2ef",
+		"91699c83ed93a1f87e326a29ccd8cc775323f9e7260035a5f014c975c5f3cd28",
+		"73759673a52c1f1707cbb61337645f4fcbd209cdc53d7e2cedaaa9f44df61285",
+		"db1313a727ffc184ae52a70012fbbf7235f551b9f2d2da04bf476abe42a3cb42",
+		"ade7af36ac0c7297dc1c11fd7b46981b629c6077bce75300f85b02a6153f161b",
+		"29a5fb121ce96194ba8b7b823a1f9c6af87e1791f824940a53b5a7efe3f790d9",
+		"61416726988f77b874435bdd89a419edc3861111884fd60e8adf54e2f299efd6",
+		"3047d841077898c26bbe6be652a2ec590a5d9bd7cd45d290ea42511b48753c09",
+		"e5c76afa931e33ac94bce2e754b1bb6407d07f738f67856783d93934ca8fc576",
+		"485a526fc74c8a344c43a4545a5987d17af9ab401c0ef1ef63aefcc5c2c086df",
+		"624b2abb7aefc0978f891b2a56b665513480e5dc195b4a66cd8def074a6d2e94",
+		"b8e1804e37a064d28d161ab5f256cc482b1423d5cd0a6b30fde7b0f51ece9199",
+		"59de730bf6c67a941f3b2ffa2f920acfaa1713695ad5deea12b4a121e5f23fa1",
+		"522194562123473dcfd7a457b18ee7dee8b7db70ed3cfa2b73f348a992fdfd3b",
+	}
+	//dataLengths := []int{31, 32, 33, 63, 64, 65, chunkSize, chunkSize + 31, chunkSize + 32, chunkSize + 63, chunkSize + 64, chunkSize * 2, chunkSize*2 + 32, chunkSize * 128, chunkSize*128 + 31, chunkSize*128 + 32, chunkSize*128 + 64, chunkSize * 129, chunkSize * 130, chunkSize * 128 * 128}
+	dataLengths := []int{31, 32, 33, 63, 64, 65, chunkSize, chunkSize + 31, chunkSize + 32, chunkSize + 63, chunkSize + 64, chunkSize * 2, chunkSize*2 + 32}
+	//dataLengths := []int{chunkSize + 32}
+	for i, dataLength := range dataLengths {
+		log.Info("start", "len", dataLength)
+		fh := NewAltFileHasher(newAsyncHasher, 32, 128)
+		_, data := generateSerialData(dataLength, 255, 0)
+		l := 32
+		offset := 0
+		for i := 0; i < dataLength; i += 32 {
+			remain := dataLength - offset
+			if remain < l {
+				l = remain
+			}
+			fh.Write(data[offset : offset+l])
+			offset += 32
+		}
+		refHash := fh.Finish(nil)
+		eq := true
+		if expected[i] != fmt.Sprintf("%x", refHash) {
+			mismatch++
+			eq = false
+		}
+		t.Logf("[%7d+%4d]\t%v\tref: %x\texpect: %s", dataLength/chunkSize, dataLength%chunkSize, eq, refHash, expected[i])
 	}
 	if mismatch > 0 {
 		t.Fatalf("mismatches: %d/%d", mismatch, len(dataLengths))
