999f79aa33056444d746d949a5e80e46bafa1517
Ferenc Szabo <ferenc.szabo@ethereum.org>
2018-12-26 12:11:46 +0100

    p2p/simulations: drop unused Connect* methods from connect.go
    The deleted methods were all unused. So I don't want to maintain them
    during refactor.

--- p2p/simulations/connect.go
+++ p2p/simulations/connect.go
@@ -27,50 +27,6 @@ var (
 	ErrNodeNotFound = errors.New("node not found")
 )
 
-// ConnectToLastNode connects the node with provided NodeID
-// to the last node that is up, and avoiding connection to self.
-// It is useful when constructing a chain network topology
-// when Network adds and removes nodes dynamically.
-func (net *Network) ConnectToLastNode(id enode.ID) (err error) {
-	ids := net.getUpNodeIDs()
-	l := len(ids)
-	if l < 2 {
-		return nil
-	}
-	last := ids[l-1]
-	if last == id {
-		last = ids[l-2]
-	}
-	return net.connect(last, id)
-}
-
-// ConnectToRandomNode connects the node with provided NodeID
-// to a random node that is up.
-func (net *Network) ConnectToRandomNode(id enode.ID) (err error) {
-	selected := net.GetRandomUpNode(id)
-	if selected == nil {
-		return ErrNodeNotFound
-	}
-	return net.connect(selected.ID(), id)
-}
-
-// ConnectNodesFull connects all nodes one to another.
-// It provides a complete connectivity in the network
-// which should be rarely needed.
-func (net *Network) ConnectNodesFull(ids []enode.ID) (err error) {
-	if ids == nil {
-		ids = net.getUpNodeIDs()
-	}
-	for i, lid := range ids {
-		for _, rid := range ids[i+1:] {
-			if err = net.connect(lid, rid); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
 // ConnectNodesChain connects all nodes in a chain topology.
 // If ids argument is nil, all nodes that are up will be connected.
 func (net *Network) ConnectNodesChain(ids []enode.ID) (err error) {
@@ -100,23 +56,6 @@ func (net *Network) ConnectNodesRing(ids []enode.ID) (err error) {
 		return err
 	}
 	return net.connect(ids[l-1], ids[0])
-}
-
-// ConnectNodesStar connects all nodes into a star topology
-// If ids argument is nil, all nodes that are up will be connected.
-func (net *Network) ConnectNodesStar(ids []enode.ID, center enode.ID) (err error) {
-	if ids == nil {
-		ids = net.getUpNodeIDs()
-	}
-	for _, id := range ids {
-		if center == id {
-			continue
-		}
-		if err := net.connect(center, id); err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 // connect connects two nodes but ignores already connected error.
--- p2p/simulations/connect_test.go
+++ p2p/simulations/connect_test.go
@@ -57,99 +57,6 @@ func newTestNetwork(t *testing.T, nodeCount int) (*Network, []enode.ID) {
 	return network, ids
 }
 
-func TestConnectToLastNode(t *testing.T) {
-	net, ids := newTestNetwork(t, 10)
-	defer net.Shutdown()
-
-	first := ids[0]
-	if err := net.ConnectToLastNode(first); err != nil {
-		t.Fatal(err)
-	}
-
-	last := ids[len(ids)-1]
-	for i, id := range ids {
-		if id == first || id == last {
-			continue
-		}
-
-		if net.GetConn(first, id) != nil {
-			t.Errorf("connection must not exist with node(ind: %v, id: %v)", i, id)
-		}
-	}
-
-	if net.GetConn(first, last) == nil {
-		t.Error("first and last node must be connected")
-	}
-}
-
-func TestConnectToRandomNode(t *testing.T) {
-	net, ids := newTestNetwork(t, 10)
-	defer net.Shutdown()
-
-	err := net.ConnectToRandomNode(ids[0])
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	var cc int
-	for i, a := range ids {
-		for _, b := range ids[i:] {
-			if net.GetConn(a, b) != nil {
-				cc++
-			}
-		}
-	}
-
-	if cc != 1 {
-		t.Errorf("expected one connection, got %v", cc)
-	}
-}
-
-func TestConnectNodesFull(t *testing.T) {
-	tests := []struct {
-		name      string
-		nodeCount int
-	}{
-		{name: "no node", nodeCount: 0},
-		{name: "single node", nodeCount: 1},
-		{name: "2 nodes", nodeCount: 2},
-		{name: "3 nodes", nodeCount: 2},
-		{name: "even number of nodes", nodeCount: 12},
-		{name: "odd number of nodes", nodeCount: 13},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			net, ids := newTestNetwork(t, test.nodeCount)
-			defer net.Shutdown()
-
-			err := net.ConnectNodesFull(ids)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			verifyFull(t, net, ids)
-		})
-	}
-}
-
-func verifyFull(t *testing.T, net *Network, ids []enode.ID) {
-	t.Helper()
-	n := len(ids)
-	var connections int
-	for i, lid := range ids {
-		for _, rid := range ids[i+1:] {
-			if net.GetConn(lid, rid) != nil {
-				connections++
-			}
-		}
-	}
-
-	want := n * (n - 1) / 2
-	if connections != want {
-		t.Errorf("wrong number of connections, got: %v, want: %v", connections, want)
-	}
-}
-
 func TestConnectNodesChain(t *testing.T) {
 	net, ids := newTestNetwork(t, 10)
 	defer net.Shutdown()
@@ -200,39 +107,6 @@ func verifyRing(t *testing.T, net *Network, ids []enode.ID) {
 		for j := i + 1; j < n; j++ {
 			c := net.GetConn(ids[i], ids[j])
 			if i == j-1 || (i == 0 && j == n-1) {
-				if c == nil {
-					t.Errorf("nodes %v and %v are not connected, but they should be", i, j)
-				}
-			} else {
-				if c != nil {
-					t.Errorf("nodes %v and %v are connected, but they should not be", i, j)
-				}
-			}
-		}
-	}
-}
-
-func TestConnectNodesStar(t *testing.T) {
-	net, ids := newTestNetwork(t, 10)
-	defer net.Shutdown()
-
-	pivotIndex := 2
-
-	err := net.ConnectNodesStar(ids, ids[pivotIndex])
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	verifyStar(t, net, ids, pivotIndex)
-}
-
-func verifyStar(t *testing.T, net *Network, ids []enode.ID, centerIndex int) {
-	t.Helper()
-	n := len(ids)
-	for i := 0; i < n; i++ {
-		for j := i + 1; j < n; j++ {
-			c := net.GetConn(ids[i], ids[j])
-			if i == centerIndex || j == centerIndex {
 				if c == nil {
 					t.Errorf("nodes %v and %v are not connected, but they should be", i, j)
 				}
