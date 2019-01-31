f103870616d1871c43e773fd4c825973007be618
Anton Evangelatov <anton.evangelatov@gmail.com>
2019-01-31 18:18:32 +0100

    cmd/swarm: extract network topology: get private keys and json marshal snapshot

--- cmd/swarm/extract_network_topology/main.go
+++ cmd/swarm/extract_network_topology/main.go
@@ -1,12 +1,69 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os/exec"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/p2p/simulations"
+	"github.com/ethereum/go-ethereum/p2p/simulations/adapters"
 )
 
+// TODO: set correct node ids
+// TODO: set correct node services
+// echo '{"jsonrpc":"2.0","method":"admin_nodeInfo","id":1}' | websocat ws://localhost:8001/api/v1/namespaces/<<namespace>>/pods/http:<<deploymentName>>-<<index>>:8546/proxy/ --origin localhost
+
+// TODO: init correctly Snap.Conns
+// echo '{"jsonrpc":"2.0","method":"admin_peers","id":1}' | websocat ws://localhost:8001/api/v1/namespaces/gluk256/pods/http:swarm-3:8546/proxy/ --origin localhost | jq ".[]" | tail -n+3 | jq ".[] | .enode"
+// echo '{"jsonrpc":"2.0","method":"admin_peers","id":1}' | websocat ws://localhost:8001/api/v1/namespaces/<<namespace>>/pods/http:<<deploymentName>>-<<index>>:8546/proxy/ --origin localhost
+// echo '{"jsonrpc":"2.0","method":"admin_peers","id":1}' | websocat ws://localhost:8001/api/v1/namespaces/gluk256/pods/http:swarm-3:8546/proxy/ --origin localhost | jq ".[]" | tail -n+3 | jq ".[] | .id"
+
 func main() {
-	cmd := exec.Command("kubectl", "exec", "-n", "staging", "-ti", "swarm-public-0", "--", "cat", "/root/.ethereum/swarm/nodekey")
-	res, err := cmd.Output()
-	fmt.Println(string(res))
+	// TODO: get from command-line arguments / golang flags
+	namespace := "staging"
+	nodes := 3
+	deploymentName := "swarm-private"
+
+	privateKeys := []string{}
+	names := []string{}
+
+	// get private keys
+	for i := 0; i < nodes; i++ {
+		cmd := exec.Command("kubectl", "exec", "-n", namespace, "-ti", fmt.Sprintf("%s-%d", deploymentName, i), "--", "cat", "/root/.ethereum/swarm/nodekey")
+		res, err := cmd.Output()
+		if err != nil {
+			panic(err)
+		}
+
+		names = append(names, fmt.Sprintf("%s-%d", deploymentName, i))
+		privateKeys = append(privateKeys, string(res))
+	}
+
+	snap := simulations.Snapshot{}
+	// generate snapshot
+	for i := 0; i < nodes; i++ {
+		prvkey, err := crypto.HexToECDSA(privateKeys[i])
+		if err != nil {
+			panic(err)
+		}
+
+		n := simulations.Node{
+			Config: &adapters.NodeConfig{
+				Name:            names[i],
+				PrivateKey:      prvkey,
+				EnableMsgEvents: true,
+			},
+			Up: true,
+		}
+
+		snap.Nodes = append(snap.Nodes, simulations.NodeSnapshot{Node: n})
+	}
+
+	js, err := json.MarshalIndent(snap, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(js))
 }
