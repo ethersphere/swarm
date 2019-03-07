466ed06e88bc7f35762eb858863e73a305eed4ea
lash <dev@holbrook.no>
2019-03-07 15:41:44 +0100

    swarm/storage: Resolve hang in AltFileHasher hashing

--- swarm/storage/filehasher_alt.go
+++ swarm/storage/filehasher_alt.go
@@ -18,12 +18,12 @@ type AltFileHasher struct {
 	chunkSize     int
 	batchSegments int
 	hashers       [altFileHasherMaxLevels]bmt.SectionWriter
-	buffers       [altFileHasherMaxLevels][]byte         // holds chunk data on each level (todo; push data to channel on complete)
+	buffers       [altFileHasherMaxLevels][]byte         // holds chunk data on each level (todo; push data to channel on complete). Buffers can hold one batch of data
 	levelCount    int                                    // number of levels in this job (only determined when Finish() is called
 	finished      bool                                   // finished writing data
 	totalBytes    int                                    // total data bytes written
-	targetCount   [altFileHasherMaxLevels - 1]int        // expected section writes per level
-	writeCount    [altFileHasherMaxLevels]int            // number of section writes per level
+	targetCount   [altFileHasherMaxLevels - 1]int        // expected segment writes per level
+	writeCount    [altFileHasherMaxLevels]int            // number of segment writes per level
 	doneC         [altFileHasherMaxLevels]chan struct{}  // used to tell parent that child is done writing on right edge
 	wg            sync.WaitGroup                         // used to tell caller hashing is done (maybe be replced by channel, and doneC only internally)
 	lwg           [altFileHasherMaxLevels]sync.WaitGroup // used when busy hashing
@@ -68,27 +68,28 @@ func (f *AltFileHasher) isWriteFinished() bool {
 
 func (f *AltFileHasher) Finish(b []byte) []byte {
 	f.lock.Lock()
+
+	// if we call finish with additional data
+	// include this data in the total length
 	if b != nil {
 		f.totalBytes += len(b)
 	}
 	f.finished = true
 
-	// find our level height and release the unused levels
+	// find our level height and decrease the waitgroup count to used levels only
 	f.levelCount = getLevelsFromLength(f.totalBytes, f.segmentSize, f.branches)
-
 	log.Debug("finish set", "levelcount", f.levelCount, "b", len(b))
 	for i := altFileHasherMaxLevels; i > f.levelCount; i-- {
 		log.Debug("purging unused level wg", "l", i)
 		f.wg.Done()
 	}
 
-	// calculate the amount of writes expected on each level
-	target := f.writeCount[0]
-	if b != nil {
-		target++
-	}
+	// calculate the amount of write() calls expected in total
+	// start with the amount of data writes (level 0)
+	// add number of writes divided by 128 for every additional level
+	// we don't use targetCount for level 0, since f.finished annotates that it is reached
+	target := (f.totalBytes-1)/f.segmentSize + 1
 	log.Debug("setting targetcount", "l", 0, "t", target)
-	target = (f.totalBytes-1)/f.segmentSize + 1
 	for i := 1; i < f.levelCount; i++ {
 		target = (target-1)/f.branches + 1
 		f.targetCount[i] = target
@@ -98,18 +99,22 @@ func (f *AltFileHasher) Finish(b []byte) []byte {
 	f.lock.Unlock()
 
 	// write and return result when we get it back
-	//f.write(b, f.writeCount[0], 0)
 	f.write(b, f.writeCount[0], 0, f.totalBytes)
 	f.wg.Wait()
 	return f.buffers[f.levelCount-1][:f.segmentSize]
 }
 
+// Write writes data provided from the buffer to the hasher
+// \TODO currently not safe to write intermediate data of length not multiple of 32 bytes
 func (f *AltFileHasher) Write(b []byte) {
 	f.totalBytes += len(b)
-	//f.write(b, f.writeCount[0], 0)
-	f.write(b, f.writeCount[0], 0, f.totalBytes)
+	for i := 0; i < len(b); i += 32 {
+		f.write(b[i:], f.writeCount[0], 0, f.totalBytes)
+	}
 }
 
+// getPotentialSpan returns the total amount of data that can represented under the given level
+// \TODO use a table instead
 func (f *AltFileHasher) getPotentialSpan(level int) int {
 	span := f.chunkSize
 	for i := 0; i < level; i++ {
@@ -118,12 +123,17 @@ func (f *AltFileHasher) getPotentialSpan(level int) int {
 	return span
 }
 
+// write writes the provided data directly to the underlying hasher
+// and performs recursive hashing on complete batches or data end
+// b is the data to write
+// offset is the level's segment we are writing to
+// level is the tree level we are writing to
+// currentTotal is the current total of data bytes written so far
 // TODO: ensure local copies of all thread unsafe vars
-// performs recursive hashing on complete batches or data end
 //func (f *AltFileHasher) write(b []byte, offset int, level int) {
 func (f *AltFileHasher) write(b []byte, offset int, level int, currentTotal int) {
 
-	// thread safe state vars
+	// copy state vars so we don't have to keep lock across the call
 	f.lock.Lock()
 	wc := f.writeCount[level]
 	//currentTotal := f.totalBytes
@@ -132,40 +142,50 @@ func (f *AltFileHasher) write(b []byte, offset int, level int, currentTotal int)
 
 	// only for log, delete on prod
 	if b == nil {
-		log.Debug("write", "level", level, "offset", offset, "length", "nil", "wc", f.writeCount[level], "total", currentTotal)
+		log.Debug("write", "level", level, "offset", offset, "length", "nil", "wc", wc, "total", currentTotal)
 	} else {
 		l := 32
 		if len(b) < l {
 			l = len(b)
 		}
-		log.Debug("write", "level", level, "offset", offset, "length", len(b), "wc", f.writeCount[level], "data", b[:l], "total", currentTotal)
+		log.Debug("write", "level", level, "offset", offset, "length", len(b), "wc", wc, "data", b[:l], "total", currentTotal)
 	}
 
+	// if top level then b is the root hash which means we are finished
+	// write it to the topmost buffer and release the waitgroup blocking  and then return
 	f.lock.Lock()
-	// if top level then return
 	if level == f.levelCount-1 {
 		copy(f.buffers[level], b)
-		f.wg.Done()
 		f.lock.Unlock()
+		f.wg.Done()
 		log.Debug("top done", "level", level)
 		return
 	}
 	f.lock.Unlock()
 
 	// only write if we have data
-	// b will never be nil except bottom level where it can be nil upon finish (which will have already been hashed if on chunk boundary)
-	if b != nil {
+	// b will never be nil except data level where it can be nil if no additional data is written upon the call to Finish()
+	// (else) if b is nil, and if the data is on a chunk boundary, the data will already have been hashed, which means we're done with that level
+	if len(b) > 0 {
+
+		// get the segment within the batch we are in
 		netOffset := (offset % f.batchSegments)
+
+		// write to the current level's hasher
 		f.hashers[level].Write(netOffset%f.branches, b)
+
+		// copy the data into the buffer
+		// TODO do we need this on the data level? should this be pipe write to something else?
 		copy(f.buffers[level][netOffset*f.segmentSize:], b)
+
+		// increment the write count
 		f.lock.Lock()
 		f.writeCount[level]++
 		wc = f.writeCount[level]
 		f.lock.Unlock()
+
 	} else if wc%f.branches == 0 {
-		f.lock.Lock()
 		f.wg.Done()
-		f.lock.Unlock()
 		f.doneC[level] <- struct{}{}
 		return
 	}
@@ -187,17 +207,15 @@ func (f *AltFileHasher) write(b []byte, offset int, level int, currentTotal int)
 		executeHasher = true
 	}
 
-	// if this was a nil data finish instruction and we are on boundary, we've already hashed what we need to hash
+	// if this was a nil data finish instruction and we are on boundary, we may be still hashing asynchronously. Wait for it to finish
+	// if we are on boundary, no need to hash further
 	if f.finished && len(b) == 0 && level == 0 {
 		f.lwg[0].Wait()
 		log.Debug("finished and 0", "wc", wc)
-		if wc%f.branches == 0 {
-			executeHasher = false
-		}
 	}
 
 	if executeHasher {
-
+		f.lwg[level].Wait()
 		// check for the dangling chunk
 		if level > 0 && f.finished {
 			f.lock.Lock()
--- swarm/storage/filehasher_test.go
+++ swarm/storage/filehasher_test.go
@@ -68,7 +68,7 @@ var (
 		"522194562123473dcfd7a457b18ee7dee8b7db70ed3cfa2b73f348a992fdfd3b",
 	}
 
-	start = 19
+	start = 0
 	end   = 20
 )
 
