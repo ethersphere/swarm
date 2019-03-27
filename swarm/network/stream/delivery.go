31f4b0d4c0971f29b4d7ef2b9bff7c95ea9aef71
Janos Guljas <janos@resenje.org>
2019-03-27 19:51:45 +0100

    swarm/network/stream: update handleChunkDeliveryMsg

--- swarm/network/stream/delivery.go
+++ swarm/network/stream/delivery.go
@@ -233,18 +233,13 @@ func (d *Delivery) handleChunkDeliveryMsg(ctx context.Context, sp *Peer, req int
 		peerPO := chunk.Proximity(sp.ID().Bytes(), msg.Addr)
 		po := chunk.Proximity(d.kad.BaseAddr(), msg.Addr)
 		depth := d.kad.NeighbourhoodDepth()
-		// is the chunk within our area of responsibility?
-		if po < depth {
-			// chunks within the area of responsibility should always sync
-			// https://github.com/ethersphere/go-ethereum/pull/1282#discussion_r269406125
+		// chunks within the area of responsibility should always sync
+		// https://github.com/ethersphere/go-ethereum/pull/1282#discussion_r269406125
+		if po >= depth || peerPO < po {
 			mode = chunk.ModePutSync
 		} else {
-			if peerPO > po {
-				// do not sync if peer that is sending us a chunk is closer to the chunk then we are
-				mode = chunk.ModePutRequest
-			} else {
-				mode = chunk.ModePutSync
-			}
+			// do not sync if peer that is sending us a chunk is closer to the chunk then we are
+			mode = chunk.ModePutRequest
 		}
 	case *ChunkDeliveryMsgSyncing:
 		msg = (*ChunkDeliveryMsg)(r)
