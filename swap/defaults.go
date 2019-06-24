4f73f275cb54506791502d3779986b71b6d7ed92
mortelli <ortellimarcelo@gmail.com>
2019-06-24 08:49:45 -0300

    Increase default placeholder thresholds

--- swap/defaults.go
+++ swap/defaults.go
@@ -2,6 +2,6 @@ package swap
 
 // Uknown for now, placeholder values
 const (
-	DefaultPaymentThreshold    = 1
-	DefaultDisconnectThreshold = 10
+	DefaultPaymentThreshold    = 100
+	DefaultDisconnectThreshold = 1000
 )
