6661800bb22061b448f7e91c63f06e1e44b7af1e
acud <theman@elad.im>
2019-07-02 20:28:38 +0300

    network/syncer: wip abstract StreamProvider

--- network/syncer/peer.go
+++ network/syncer/peer.go
@@ -21,8 +21,6 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
-	"strconv"
-	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -44,7 +42,6 @@ const (
 	HashSize  = 32
 	BatchSize = 50
 	//DeliveryFrameSize        = 128
-	HistoricalStreamPageSize = BatchSize
 )
 
 type Offer struct {
@@ -70,33 +67,39 @@ type Want struct {
 // Peer is the Peer extension for the streaming protocol
 type Peer struct {
 	*network.BzzPeer
-	mtx          sync.Mutex
-	streamsDirty bool // a request for StreamInfo is underway and awaiting reply
-	syncer       *SwarmSyncer
+	mtx       sync.Mutex
+	providers map[string]StreamProvider
 
-	streamCursors     map[uint]uint64           // key: bin, value: session cursor. when unset - we are not interested in that bin
-	historicalStreams map[uint]*syncStreamFetch //maintain state for each stream fetcher on the client side
-	openWants         map[uint]*Want            //maintain open wants on the client side
-	openOffers        map[uint]Offer            // maintain open offers on the server side
-	quit              chan struct{}             //peer is going offline
+	streamCursors map[string]uint64 // key: Stream ID string representation, value: session cursor. Keeps cursors for all streams. when unset - we are not interested in that bin
+	openWants     map[uint]*Want    // maintain open wants on the client side
+	openOffers    map[uint]Offer    // maintain open offers on the server side
+	quit          chan struct{}     // closed when peer is going offline
 }
 
 // NewPeer is the constructor for Peer
-func NewPeer(peer *network.BzzPeer, s *SwarmSyncer) *Peer {
+func NewPeer(peer *network.BzzPeer, providers map[string]StreamProvider) *Peer {
 	p := &Peer{
-		BzzPeer:           peer,
-		streamCursors:     make(map[uint]uint64),
-		historicalStreams: make(map[uint]*syncStreamFetch),
-		openWants:         make(map[uint]*Want),
-		openOffers:        make(map[uint]Offer),
-		syncer:            s,
-		quit:              make(chan struct{}),
+		BzzPeer:       peer,
+		providers:     providers,
+		streamCursors: make(map[uint]uint64),
+		openWants:     make(map[uint]*Want),
+		openOffers:    make(map[uint]Offer),
+		quit:          make(chan struct{}),
 	}
 	return p
 }
 
 func (p *Peer) Left() {
 	close(p.quit)
+}
+
+func (p *Peer) InitProviders() {
+	log.Debug("peer.InitProviders")
+
+	for _, sp := range p.providers {
+
+		go sp.RunUpdateStreams(p)
+	}
 }
 
 // HandleMsg is the message handler that delegates incoming messages
@@ -131,20 +134,32 @@ func (p *Peer) handleStreamInfoReq(ctx context.Context, msg *StreamInfoReq) {
 		panic("nil streams msg requested")
 	}
 	for _, v := range msg.Streams {
-		streamCursor, err := p.syncer.netStore.LastPullSubscriptionBinID(uint8(v))
-		if err != nil {
-			log.Error("error getting last bin id", "bin", v)
-			panic("shouldnt happen")
+		if provider, ok := p.providers[v.Name]; ok {
+			key, err := provider.ParseKey(v.Key)
+			if err != nil {
+				// error parsing the stream key,
+				log.Error("error parsing the stream key", "peer", p.ID(), "key", key)
+				p.Drop()
+			}
+			streamCursor, err := provider.Cursor(key)
+			if err != nil {
+				log.Error("error getting cursor for stream key", "peer", p.ID(), "name", v.Name, "key", key)
+				panic("shouldnt happen")
+			}
+			descriptor := StreamDescriptor{
+				Stream:  v,
+				Cursor:  streamCursor,
+				Bounded: provider.Boundedness(),
+			}
+			streamRes.Streams = append(streamRes.Streams, descriptor)
+
+		} else {
+
+			// tell the other peer we dont support this stream. this is non fatal
 		}
-		descriptor := StreamDescriptor{
-			Name:    fmt.Sprintf("SYNC|%d", v),
-			Cursor:  streamCursor,
-			Bounded: false,
-		}
-		streamRes.Streams = append(streamRes.Streams, descriptor)
 	}
 	if err := p.Send(ctx, streamRes); err != nil {
-		log.Error("failed to send StreamInfoRes to client", "requested bins", msg.Streams)
+		log.Error("failed to send StreamInfoRes to client", "requested keys", msg.Streams)
 	}
 }
 
@@ -157,88 +172,105 @@ func (p *Peer) handleStreamInfoRes(ctx context.Context, msg *StreamInfoRes) {
 
 	if len(msg.Streams) == 0 {
 		log.Error("StreamInfo response is empty")
-		p.Drop()
+		panic("panic for now - this shouldnt happen") //p.Drop()
 	}
 
 	for _, s := range msg.Streams {
-		bin, err := syncStreamToBin(s.Name) //ParseStream(s.Name)
-		if err != nil {
-			log.Error("error parsing stream", "stream", s.Name)
-			p.Drop()
-		}
-		log.Debug("setting bin cursor", "peer", p.ID(), "bin", uint(bin), "cursor", s.Cursor)
-		p.streamCursors[uint(bin)] = s.Cursor
+		if provider, ok := p.providers[s.Stream.Name]; ok {
+			// check the stream integrity
+			_, err := provider.ParseKey(s.Stream.Key)
+			if err != nil {
+				log.Error("error parsing stream", "stream", s.Stream)
+				panic("w00t")
+				p.Drop()
+			}
+			log.Debug("setting stream cursor", "peer", p.ID(), "stream", s.Stream.String(), "cursor", s.Cursor)
+			p.streamCursors[s.Stream.String()] = s.Cursor
 
-		if s.Cursor > 0 {
-			go func(name string, b uint, cursor uint64) {
-				err := p.requestStreamRange(ctx, name, b, cursor)
-				if err != nil {
-					log.Error("had an error sending initial GetRange for historical stream", "peer", p.ID(), "stream", name, "err", err)
-					p.Drop()
-				}
-			}(s.Name, uint(bin), s.Cursor)
+			if s.Cursor > 0 {
+				// fetch everything from beginning till  s.Cursor
+				go func(name string, k interface{}, cursor uint64) {
+					err := p.requestStreamRange(ctx, name, k, cursor)
+					if err != nil {
+						log.Error("had an error sending initial GetRange for historical stream", "peer", p.ID(), "stream", name, "err", err)
+						p.Drop()
+					}
+				}(s.Stream.Name, key, s.Cursor)
+			}
+
+			// handle stream unboundedness
+			if !s.Bounded {
+				// constantly fetch the head of the stream
+
+			}
+		} else {
+			log.Error("got a StreamInfoRes message for a provider which I dont support")
+			panic("shouldn't happen, replace with p.Drop()")
 		}
 	}
 }
 
-func (p *Peer) requestStreamRange(ctx context.Context, stream string, bin uint, cursor uint64) error {
-	log.Debug("peer.requestStreamRange", "peer", p.ID(), "stream", stream, "bin", bin, "cursor", cursor)
-	interval, err := p.getOrCreateInterval(bin)
-	if err != nil {
-		return err
-	}
-	from, to := interval.Next()
-	log.Debug("peer.requestStreamRange nextInterval", "peer", p.ID(), "stream", stream, "bin", bin, "cursor", cursor, "from", from, "to", to)
-	if from > cursor {
-		log.Debug("peer.requestStreamRange stream finished", "peer", p.ID(), "stream", stream, "bin", bin, "cursor", cursor)
-		// stream finished. quit
+func (p *Peer) requestStreamRange(ctx context.Context, streamName string, key interface{}, cursor uint64) error {
+	log.Debug("peer.requestStreamRange", "peer", p.ID(), "stream", streamName, "key", key, "cursor", cursor)
+	if provider, ok := p.providers[streamName]; ok {
+		interval, err := p.getOrCreateInterval(provider.EncodeKey(key))
+		if err != nil {
+			return err
+		}
+		from, to := interval.Next()
+		log.Debug("peer.requestStreamRange nextInterval", "peer", p.ID(), "stream", stream, "bin", bin, "cursor", cursor, "from", from, "to", to)
+		if from > cursor {
+			log.Debug("peer.requestStreamRange stream finished", "peer", p.ID(), "stream", stream, "bin", bin, "cursor", cursor)
+			// stream finished. quit
+			return nil
+		}
+		if to > cursor {
+			log.Debug("adjusting cursor")
+			to = cursor
+		}
+		if to == 0 {
+			// todo: Next() should take a ceiling argument. it returns 0 if there's no upper bound in the interval (i.e. HEAD)
+			to = cursor
+		}
+		if from == 0 {
+			panic("no")
+		}
+		if to-from > BatchSize-1 {
+			log.Debug("limiting TO to HistoricalStreamPageSize", "to", to, "new to", from+HistoricalStreamPageSize)
+			to = from + BatchSize - 1 //because the intervals are INCLUSIVE, it means we get also FROM and TO
+		}
+
+		g := GetRange{
+			Ruid:      uint(rand.Uint32()),
+			Stream:    streamName,
+			From:      from,
+			To:        to,
+			BatchSize: 128,
+			Roundtrip: true,
+		}
+		log.Debug("sending GetRange to peer", "peer", p.ID(), "bin", uint(bin), "cursor", cursor, "GetRange", g)
+
+		if err := p.Send(ctx, g); err != nil {
+			return err
+		}
+
+		w := &Want{
+			ruid:   g.Ruid,
+			stream: g.Stream,
+			from:   g.From,
+			to:     g.To,
+
+			hashes:    make(map[string]bool),
+			requested: time.Now(),
+			wg:        &sync.WaitGroup{},
+		}
+
+		p.openWants[w.ruid] = w
 		return nil
-	}
-	if to > cursor {
-		log.Debug("adjusting cursor")
-		to = cursor
-	}
-	if to == 0 {
-		// todo: Next() should take a ceiling argument. it returns 0 if there's no upper bound in the interval (i.e. HEAD)
-		to = cursor
-	}
-	if from == 0 {
-		panic("no")
-	}
-	if to-from > HistoricalStreamPageSize-1 {
-		log.Debug("limiting TO to HistoricalStreamPageSize", "to", to, "new to", from+HistoricalStreamPageSize)
-		to = from + HistoricalStreamPageSize - 1 //because the intervals are INCLUSIVE, it means we get also FROM, so we have to deduce one
-		// from the end cursor, because...
-	}
-	streamFetch := newSyncStreamFetch(uint(bin))
-	p.historicalStreams[uint(bin)] = streamFetch
-	g := GetRange{
-		Ruid:      uint(rand.Uint32()),
-		Stream:    stream,
-		From:      from,
-		To:        to,
-		BatchSize: 128,
-		Roundtrip: true,
-	}
-	log.Debug("sending GetRange to peer", "peer", p.ID(), "bin", uint(bin), "cursor", cursor, "GetRange", g)
 
-	if err := p.Send(ctx, g); err != nil {
-		return err
+	} else {
+		//got a message for an unsupported provider
 	}
-
-	w := &Want{
-		ruid:   g.Ruid,
-		stream: g.Stream,
-		from:   g.From,
-		to:     g.To,
-
-		hashes:    make(map[string]bool),
-		requested: time.Now(),
-		wg:        &sync.WaitGroup{},
-	}
-
-	p.openWants[w.ruid] = w
-	return nil
 }
 
 // handleGetRange is handled by the SERVER and sends in response an OfferedHashes message
@@ -645,211 +677,4 @@ func (p *Peer) collectBatch(ctx context.Context, bin uint, from, to uint64) (has
 	}
 	return batch, *batchStartID, batchEndID, nil
 
-}
-
-// syncStreamFetch is a struct that holds exposed state used by a separate goroutine that handles stream retrievals
-type syncStreamFetch struct {
-	bin       uint          //the bin we're working on
-	lastIndex uint64        //last chunk bin index that we handled
-	quit      chan struct{} //used to signal from other components to quit this stream (i.e. on depth change)
-	done      chan struct{} //signaled by the actor on stream fetch done
-	err       chan error    //signaled by the actor on error
-}
-
-func newSyncStreamFetch(bin uint) *syncStreamFetch {
-	return &syncStreamFetch{
-		bin:       bin,
-		lastIndex: 0,
-		quit:      make(chan struct{}),
-		done:      make(chan struct{}),
-		err:       make(chan error),
-	}
-}
-
-// syncSubscriptionsDiff calculates to which proximity order bins a peer
-// (with po peerPO) needs to be subscribed after kademlia neighbourhood depth
-// change from prevDepth to newDepth. Max argument limits the number of
-// proximity order bins. Returned values are slices of integers which represent
-// proximity order bins, the first one to which additional subscriptions need to
-// be requested and the second one which subscriptions need to be quit. Argument
-// prevDepth with value less then 0 represents no previous depth, used for
-// initial syncing subscriptions.
-func syncSubscriptionsDiff(peerPO, prevDepth, newDepth, max int, syncBinsWithinDepth bool) (subBins, quitBins []uint) {
-	newStart, newEnd := syncBins(peerPO, newDepth, max, syncBinsWithinDepth)
-	if prevDepth < 0 {
-		if newStart == -1 && newEnd == -1 {
-			return nil, nil
-		}
-		// no previous depth, return the complete range
-		// for subscriptions requests and nothing for quitting
-		return intRange(newStart, newEnd), nil
-	}
-
-	prevStart, prevEnd := syncBins(peerPO, prevDepth, max, syncBinsWithinDepth)
-	if newStart == -1 && newEnd == -1 {
-		// this means that we should not have any streams on any bins with this peer
-		// get rid of what was established on the previous depth
-		quitBins = append(quitBins, intRange(prevStart, prevEnd)...)
-		return
-	}
-
-	if newStart < prevStart {
-		subBins = append(subBins, intRange(newStart, prevStart)...)
-	}
-
-	if prevStart < newStart {
-		quitBins = append(quitBins, intRange(prevStart, newStart)...)
-	}
-
-	if newEnd < prevEnd {
-		quitBins = append(quitBins, intRange(newEnd, prevEnd)...)
-	}
-
-	if prevEnd < newEnd {
-		subBins = append(subBins, intRange(prevEnd, newEnd)...)
-	}
-
-	return subBins, quitBins
-}
-
-// CreateStreams creates and maintains the streams per peer.
-// Runs per peer, in a separate goroutine
-// when the depth changes on our node
-//  - peer moves from out-of-depth to depth -> determine new streams ; init new streams (delete old streams, stop sending get range queries ; graceful shutdown of existing streams)
-//  - peer moves from depth to out-of-depth -> determine new streams ; init new streams (delete old streams, stop sending get range queries ; graceful shutdown of existing streams)
-//  - depth changes, and peer stays in depth, but we need MORE (or LESS) streams (WHY???).. so again -> determine new streams ; init new streams (delete old streams, stop sending get range queries ; graceful shutdown of existing streams)
-// peer connects and disconnects quickly
-func (s *SwarmSyncer) CreateStreams(p *Peer) {
-	defer log.Debug("createStreams closed", "peer", p.ID())
-
-	peerPo := chunk.Proximity(s.kad.BaseAddr(), p.BzzAddr.Address())
-	depth := s.kad.NeighbourhoodDepth()
-	withinDepth := peerPo >= depth
-
-	log.Debug("create streams", "peer", p.BzzAddr, "base", s.kad.BaseAddr(), "withinDepth", withinDepth, "depth", depth, "po", peerPo)
-
-	if withinDepth {
-		sub, _ := syncSubscriptionsDiff(peerPo, -1, depth, s.kad.MaxProxDisplay, true)
-		log.Debug("sending initial subscriptions message", "peer", p.ID(), "bins", sub)
-		time.Sleep(createStreamsDelay)
-		doPeerSubUpdate(p, sub, nil)
-		if len(sub) == 0 {
-			panic("w00t")
-		}
-		//if err := p.Send(context.TODO(), streamsMsg); err != nil {
-		//log.Error("err establishing initial subscription", "err", err)
-		//}
-	}
-
-	subscription, unsubscribe := s.kad.SubscribeToNeighbourhoodDepthChange()
-	defer unsubscribe()
-	for {
-		select {
-		case <-subscription:
-			newDepth := s.kad.NeighbourhoodDepth()
-			log.Debug("got kademlia depth change sig", "peer", p.ID(), "peerPo", peerPo, "depth", depth, "newDepth", newDepth, "withinDepth", withinDepth)
-			switch {
-			case peerPo >= newDepth:
-				// peer is within depth
-				if !withinDepth {
-					log.Debug("peer moved into depth, requesting cursors", "peer", p.ID())
-					withinDepth = true // peerPo >= newDepth
-					// previous depth is -1 because we did not have any streams with the client beforehand
-					sub, _ := syncSubscriptionsDiff(peerPo, -1, newDepth, s.kad.MaxProxDisplay, true)
-					doPeerSubUpdate(p, sub, nil)
-					if len(sub) == 0 {
-						panic("w00t")
-					}
-					depth = newDepth
-				} else {
-					// peer was within depth, but depth has changed. we should request the cursors for the
-					// necessary bins and quit the unnecessary ones
-					sub, quits := syncSubscriptionsDiff(peerPo, depth, newDepth, s.kad.MaxProxDisplay, true)
-					log.Debug("peer was inside depth, checking if needs changes", "peer", p.ID(), "peerPo", peerPo, "depth", depth, "newDepth", newDepth, "subs", sub, "quits", quits)
-					doPeerSubUpdate(p, sub, quits)
-					depth = newDepth
-				}
-			case peerPo < newDepth:
-				if withinDepth {
-					sub, quits := syncSubscriptionsDiff(peerPo, depth, newDepth, s.kad.MaxProxDisplay, true)
-					log.Debug("peer transitioned out of depth", "peer", p.ID(), "subs", sub, "quits", quits)
-					doPeerSubUpdate(p, sub, quits)
-					withinDepth = false
-				}
-			}
-
-		case <-s.quit:
-			return
-		}
-	}
-}
-
-func doPeerSubUpdate(p *Peer, subs, quits []uint) {
-	if len(subs) > 0 {
-		log.Debug("getting cursors info from peer", "peer", p.ID(), "subs", subs)
-		streamsMsg := StreamInfoReq{Streams: subs}
-		if err := p.Send(context.TODO(), streamsMsg); err != nil {
-			log.Error("error establishing subsequent subscription", "err", err)
-			p.Drop()
-		}
-	}
-	for _, v := range quits {
-		log.Debug("removing cursor info for peer", "peer", p.ID(), "bin", v, "cursors", p.streamCursors, "quits", quits)
-		delete(p.streamCursors, uint(v))
-
-		if hs, ok := p.historicalStreams[uint(v)]; ok {
-			log.Debug("closing historical stream for peer", "peer", p.ID(), "bin", v, "historicalStream", hs)
-
-			close(hs.quit)
-			// todo: wait for the hs.done to close?
-			delete(p.historicalStreams, uint(v))
-		} else {
-			// this could happen when the cursor was 0 thus the historical stream was not created - do nothing
-		}
-	}
-}
-
-// syncBins returns the range to which proximity order bins syncing
-// subscriptions need to be requested, based on peer proximity and
-// kademlia neighbourhood depth. Returned range is [start,end), inclusive for
-// start and exclusive for end.
-func syncBins(peerPO, depth, max int, syncBinsWithinDepth bool) (start, end int) {
-	if syncBinsWithinDepth && peerPO < depth {
-		// we don't want to request anything from peers outside depth
-		return -1, -1
-	} else {
-		if peerPO < depth {
-			// subscribe only to peerPO bin if it is not
-			// in the nearest neighbourhood
-			return peerPO, peerPO + 1
-		}
-	}
-	// subscribe from depth to max bin if the peer
-	// is in the nearest neighbourhood
-	return depth, max + 1
-}
-
-// intRange returns the slice of integers [start,end). The start
-// is inclusive and the end is not.
-func intRange(start, end int) (r []uint) {
-	for i := start; i < end; i++ {
-		r = append(r, uint(i))
-	}
-	return r
-}
-
-func syncStreamToBin(stream string) (uint, error) {
-	vals := strings.Split(stream, "|")
-	if len(vals) != 2 {
-		return 0, fmt.Errorf("error getting bin id from stream string: %s", stream)
-	}
-	bin, err := strconv.Atoi(vals[1])
-	if err != nil {
-		return 0, err
-	}
-	return uint(bin), nil
-}
-
-func binToSyncStream(bin uint) string {
-	return fmt.Sprintf("SYNC|%d", bin)
 }
