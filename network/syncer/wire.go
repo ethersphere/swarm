fba0520be3ac47ac13026999f4b5ccc4090b3bd7
acud <theman@elad.im>
2019-07-02 20:28:38 +0300

    network/syncer: wip abstract StreamProvider

--- network/syncer/wire.go
+++ network/syncer/wire.go
@@ -17,10 +17,58 @@
 package syncer
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/ethersphere/swarm/chunk"
 	"github.com/ethersphere/swarm/storage"
 )
+
+// StreamProvider interface provides a lightweight abstraction that allows an easily-pluggable
+// stream provider as part of the Stream! protocol specification.
+// Since Stream! thoroughly defines the concepts of a stream, intervals, clients and servers, the
+// interface therefore needs only a pluggable provider.
+// The domain interpretable notions which are at the discretion of the implementing
+// provider therefore are - sourcing data (get, put, subscribe for constant new data, and need data
+// which is to decide whether to retrieve data or not), retrieving cursors from the data store, the
+// implementation of which streams to maintain with a certain peer and providing functionality
+// to expose, parse and encode values related to the string represntation of the stream
+type StreamProvider interface {
+
+	// NeedData informs the caller whether a certain chunk needs to be fetched from another peer or not.
+	// Typically this will involve checking whether a certain chunk exists locally.
+	// In case a chunk does not exist locally - a `wait` function returns upon chunk delivery
+	NeedData(ctx context.Context, ctx chunk.Address) (bool, wait func(context.Context) error)
+
+	// Get a particular chunk identified by addr from the local storage
+	Get(ctx context.Context, addr chunk.Address) ([]byte, error)
+
+	// Put a certain chunk into the local storage
+	Put(ctx context.Context, addr chunk.Address, data []byte) error
+
+	// Subscribe to a data stream from an arbitrary data source
+	Subscribe(key interface{}, from, to uint64) (<-chan chunk.Descriptor, func())
+
+	// Cursor returns the last known Cursor for a given Stream Key
+	Cursor(interface{}) (uint64, error)
+
+	// RunUpdateStreams is a provider specific implementation on how to maintain running streams with
+	// an arbitrary Peer. This method should always be run in a separate goroutine
+	RunUpdateStreams(p *Peer)
+
+	// StreamName returns the Name of the Stream (see ID)
+	StreamName() string
+
+	// ParseStream from a standard pipe-separated string and return the Stream Key
+	ParseKey(string) interface{}
+
+	// EncodeStream from a Stream Key to a Stream pipe-separated string representation
+	EncodeKey(interface{}) string
+
+	IntervalKey(ID) string
+
+	Boundedness() bool
+}
 
 type StreamInfoReq struct {
 	Streams []ID
@@ -78,11 +126,12 @@ type StreamState struct {
 	Message string
 }
 
-// Stream defines a unique stream identifier.
+// Stream defines a unique stream identifier in a textual representation
 type ID struct {
-	// Name is used for Client and Server functions identification.
+	// Name is used for the Stream provider identification
 	Name string
-	// Key is the name of specific stream data.
+	// Key is the name of specific data stream within the stream provider. The semantics of this value
+	// is at the discretion of the stream provider implementation
 	Key string
 }
 
