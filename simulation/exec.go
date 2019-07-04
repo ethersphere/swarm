04dd442ec5cd84b542179eecb13be3acbecee588
Rafael Matias <rafael@skyle.net>
2019-07-04 17:01:18 +0200

    simulation: exec adapter start/stop

--- simulation/exec.go
+++ simulation/exec.go
@@ -1,10 +1,17 @@
 package simulation
 
 import (
-	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"runtime"
+	"syscall"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/ethersphere/swarm"
 )
 
 type ExecAdapter struct {
@@ -13,11 +20,14 @@ type ExecAdapter struct {
 }
 
 type ExecAdapterConfig struct {
+	// Directory stores all the nodes' data directories
 	Directory string
 }
 
 type ExecNode struct {
-	config NodeConfig
+	adapter *ExecAdapter
+	config  NodeConfig
+	cmd     *exec.Cmd
 }
 
 func NewExecAdapter(config ExecAdapterConfig) (*ExecAdapter, error) {
@@ -36,18 +46,18 @@ func (a *ExecAdapter) NewNode(config NodeConfig) (Node, error) {
 		return nil, fmt.Errorf("node '%s' already exists", config.ID)
 	}
 
-	dir := filepath.Join(a.directory, string(config.ID))
-	if err := os.Mkdir(dir, 0755); err != nil {
-		return nil, fmt.Errorf("failed to create node directory: %s", err)
-	}
-
 	node := &ExecNode{
-		config: config,
+		config:  config,
+		adapter: a,
 	}
 
 	a.nodes[config.ID] = node
 
 	return node, nil
+}
+
+func nodeDataDir(adapterDir string, id NodeID) string {
+	return filepath.Join(adapterDir, string(id))
 }
 
 // Status returns the node status
@@ -61,10 +71,113 @@ func (n *ExecNode) Status() NodeStatus {
 
 // Start starts the node
 func (n *ExecNode) Start() error {
-	return errors.New("not implemented")
+	// Check if command already exists
+	if n.cmd != nil {
+		return fmt.Errorf("node %s is already running", n.config.ID)
+	}
+
+	// Create command line arguments
+	args := []string{"swarm"}
+	args = append(args, n.config.Args...)
+
+	// Create data directory for this node
+	dir := n.dataDir()
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		return fmt.Errorf("failed to create node directory: %s", err)
+	}
+
+	// Configure data directory
+	args = append(args, "--datadir", dir)
+
+	// Configure IPC path
+	args = append(args, "--ipcpath", n.ipcPath())
+
+	// Automatically allocate ports
+	args = append(args, "--pprofport", "0")
+	args = append(args, "--bzzport", "0")
+	args = append(args, "--wsport", "0")
+	args = append(args, "--port", "0")
+
+	// Start command
+	n.cmd = &exec.Cmd{
+		Path:   "/home/rafael/go/bin/swarm",
+		Args:   args,
+		Dir:    dir,
+		Env:    n.config.Env,
+		Stdout: n.config.Stdout,
+		Stderr: n.config.Stderr,
+	}
+
+	if err := n.cmd.Start(); err != nil {
+		n.cmd = nil
+		return fmt.Errorf("error starting node %s: %s", n.config.ID, err)
+	}
+
+	// Wait for the node to start
+	var client *rpc.Client
+	var err error
+	defer func() {
+		if err != nil {
+			n.Stop()
+		}
+	}()
+	for start := time.Now(); time.Since(start) < 10*time.Second; time.Sleep(50 * time.Millisecond) {
+		client, err = rpc.Dial(n.ipcPath())
+		if err == nil {
+			break
+		}
+	}
+	if client == nil {
+		return fmt.Errorf("could not establish rpc connection. node %s: %v", n.config.ID, err)
+	}
+	defer client.Close()
+	var info swarm.Info
+	if err := client.Call(&info, "bzz_info"); err != nil {
+		return fmt.Errorf("could not get info via rpc call. node %s: %v", n.config.ID, err)
+	}
+
+	spew.Dump(info)
+
+	return nil
 }
 
 // Stop stops the node
 func (n *ExecNode) Stop() error {
-	return errors.New("not implemented")
+	if n.cmd == nil {
+		return nil
+	}
+	defer func() {
+		n.cmd = nil
+	}()
+	// Try to gracefully terminate the process
+	if err := n.cmd.Process.Signal(syscall.SIGTERM); err != nil {
+		return n.cmd.Process.Kill()
+	}
+
+	waitErr := make(chan error)
+	go func() {
+		waitErr <- n.cmd.Wait()
+	}()
+	select {
+	case err := <-waitErr:
+		return err
+	case <-time.After(20 * time.Second):
+		return n.cmd.Process.Kill()
+	}
+
+}
+
+// ipcPath returns the path to the ipc socket
+func (n *ExecNode) ipcPath() string {
+	ipcfile := "bzzd.ipc"
+	// On windows we can have to use pipes
+	if runtime.GOOS == "windows" {
+		return `\\.\pipe\` + ipcfile
+	}
+	return fmt.Sprintf("%s/%s", n.dataDir(), ipcfile)
+}
+
+// dataDir returns the path to the data directory that the node should use
+func (n *ExecNode) dataDir() string {
+	return filepath.Join(n.adapter.directory, string(n.config.ID))
 }
--- simulation/exec_test.go
+++ simulation/exec_test.go
@@ -1,14 +1,17 @@
 package simulation
 
 import (
+	"encoding/hex"
 	"io/ioutil"
 	"os"
 	"testing"
+
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func TestExecAdapter(t *testing.T) {
 
-	tmpdir, err := ioutil.TempDir("", "test-exec")
+	tmpdir, err := ioutil.TempDir("", "test-adapter-exec")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -18,11 +21,25 @@ func TestExecAdapter(t *testing.T) {
 		Directory: tmpdir,
 	})
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("could not create exec adapter: %v", err)
 	}
 
+	key, err := crypto.GenerateKey()
+	if err != nil {
+		t.Fatalf("could not generate key: %v", err)
+	}
+	hexKey := hex.EncodeToString(crypto.FromECDSA(key))
+
+	args := []string{
+		"--bootnodes", "",
+		"--bzzkeyhex", hexKey,
+		"--bzznetworkid", "49",
+	}
 	nodeconfig := NodeConfig{
-		ID: "node1",
+		ID:     "node1",
+		Args:   args,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
 	}
 	node, err := adapter.NewNode(nodeconfig)
 	if err != nil {
@@ -30,12 +47,32 @@ func TestExecAdapter(t *testing.T) {
 	}
 	status := node.Status()
 	if status.ID != "node1" {
-		t.Error("node id is different")
+		t.Fatal("node id is different")
 	}
 
 	_, err = adapter.NewNode(nodeconfig)
 	if err == nil {
-		t.Error("a node with the same id was registered")
+		t.Fatal("a node with the same id was registered")
+	}
+
+	err = node.Start()
+	if err != nil {
+		t.Fatalf("node did not start: %v", err)
+	}
+
+	err = node.Stop()
+	if err != nil {
+		t.Fatalf("node didn't stop: %v", err)
+	}
+
+	err = node.Start()
+	if err != nil {
+		t.Fatalf("node didn't start again: %v", err)
+	}
+
+	err = node.Stop()
+	if err != nil {
+		t.Fatalf("node didn't stop: %v", err)
 	}
 
 }
