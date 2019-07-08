c7863dbbb581e600bff629b21c9a93e8ed1078f2
acud <theman@elad.im>
2019-07-08 11:55:04 +0300

    network/newstream: test case for historical AND live syncing

--- network/newstream/syncing_test.go
+++ network/newstream/syncing_test.go
@@ -291,6 +291,136 @@ func TestTwoNodesSyncWithGaps(t *testing.T) {
 	}
 }
 
+func TestTwoNodesFullSyncWithLive(t *testing.T) {
+	var (
+		chunkCount = 1000
+		syncTime   = 3 * time.Second
+	)
+	sim := simulation.NewInProc(map[string]simulation.ServiceFunc{
+		"bzz-sync": newBzzSyncWithLocalstoreDataInsertion(0, StreamAutostart),
+	})
+	defer sim.Close()
+
+	timeout := 30 * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	_, err := sim.AddNode()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	result := sim.Run(ctx, func(ctx context.Context, sim *simulation.Simulation) (err error) {
+		nodeIDs := sim.UpNodeIDs()
+
+		item := sim.NodeItem(sim.UpNodeIDs()[0], bucketKeyFileStore)
+
+		log.Debug("subscriptions on all bins exist between the two nodes, proceeding to check bin indexes")
+		log.Debug("uploader node", "enode", nodeIDs[0])
+		item = sim.NodeItem(nodeIDs[0], bucketKeyFileStore)
+		store := item.(chunk.Store)
+
+		//put some data into just the first node
+		filesize := chunkCount * 4096
+		cctx := context.Background()
+		_, wait, err := item.(*storage.FileStore).Store(cctx, testutil.RandomReader(0, filesize), int64(filesize), false)
+		if err != nil {
+			return err
+		}
+		if err := wait(cctx); err != nil {
+			return err
+		}
+
+		id, err := sim.AddNodes(1)
+		if err != nil {
+			return err
+		}
+		err = sim.Net.ConnectNodesStar(id, nodeIDs[0])
+		if err != nil {
+			return err
+		}
+		nodeIDs = sim.UpNodeIDs()
+		syncingNodeId := nodeIDs[1]
+
+		uploaderNodeBinIDs := make([]uint64, 17)
+
+		log.Debug("checking pull subscription bin ids")
+		for po := 0; po <= 16; po++ {
+			until, err := store.LastPullSubscriptionBinID(uint8(po))
+			if err != nil {
+				return err
+			}
+			log.Debug("uploader node got bin index", "bin", po, "binIndex", until)
+
+			uploaderNodeBinIDs[po] = until
+		}
+
+		// wait for syncing
+		<-time.After(syncTime)
+
+		// check that the sum of bin indexes is equal
+
+		log.Debug("compare to", "enode", syncingNodeId)
+		item = sim.NodeItem(syncingNodeId, bucketKeyFileStore)
+		db := item.(chunk.Store)
+
+		uploaderSum, otherNodeSum := 0, 0
+		for po, uploaderUntil := range uploaderNodeBinIDs {
+			shouldUntil, err := db.LastPullSubscriptionBinID(uint8(po))
+			if err != nil {
+				return err
+			}
+			otherNodeSum += int(shouldUntil)
+			uploaderSum += int(uploaderUntil)
+		}
+		if uploaderSum != otherNodeSum {
+			return fmt.Errorf("bin indice sum mismatch. got %d want %d", otherNodeSum, uploaderSum)
+		}
+
+		// now add stuff so that we fetch it with live syncing
+
+		_, wait, err = item.(*storage.FileStore).Store(cctx, testutil.RandomReader(101010, filesize), int64(filesize), false)
+		if err != nil {
+			return err
+		}
+		if err = wait(cctx); err != nil {
+			return err
+		}
+
+		// count the content in the bins again
+		for po := 0; po <= 16; po++ {
+			until, err := store.LastPullSubscriptionBinID(uint8(po))
+			if err != nil {
+				return err
+			}
+			log.Debug("uploader node got bin index", "bin", po, "binIndex", until)
+
+			uploaderNodeBinIDs[po] = until
+		}
+
+		// wait for live syncing
+		<-time.After(syncTime)
+
+		for po, uploaderUntil := range uploaderNodeBinIDs {
+			shouldUntil, err := db.LastPullSubscriptionBinID(uint8(po))
+			if err != nil {
+				return err
+			}
+			otherNodeSum += int(shouldUntil)
+			uploaderSum += int(uploaderUntil)
+		}
+		if uploaderSum != otherNodeSum {
+			return fmt.Errorf("live sync bin indice sum mismatch. got %d want %d", otherNodeSum, uploaderSum)
+		}
+
+		return nil
+	})
+
+	if result.Error != nil {
+		t.Fatal(result.Error)
+	}
+}
+
 type chunkProxData struct {
 	addr            chunk.Address
 	uploaderNodePO  int
