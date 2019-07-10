119c38a4e952989075df9eb9923547ca0f82702f
mortelli <ortellimarcelo@gmail.com>
2019-07-10 07:59:27 -0300

    Increase payment and disconnect threshold values

--- swap/defaults.go
+++ swap/defaults.go
@@ -2,6 +2,6 @@ package swap
 
 // Uknown for now, placeholder values
 const (
-	DefaultPaymentThreshold    = 100
-	DefaultDisconnectThreshold = 1000
+	DefaultPaymentThreshold    = 1000000
+	DefaultDisconnectThreshold = 1500000
 )
