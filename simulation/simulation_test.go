ea80683338e612ca90b7428b485b5bbf05b9dc5d
Rafael Matias <rafael@skyle.net>
2019-07-11 15:32:33 +0200

    simulation: discovery test

--- simulation/simulation_test.go
+++ simulation/simulation_test.go
@@ -2,19 +2,37 @@ package simulation
 
 import (
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"sync"
 	"testing"
+	"time"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/ethersphere/swarm"
+	"github.com/ethersphere/swarm/network"
+	"github.com/mattn/go-colorable"
 )
+
+var (
+	nodes    = flag.Int("nodes", 20, "number of nodes to create")
+	loglevel = flag.Int("loglevel", 3, "verbosity of logs")
+	rawlog   = flag.Bool("rawlog", false, "remove terminal formatting from logs")
+)
+
+func init() {
+	flag.Parse()
+	log.PrintOrigins(true)
+	log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(*loglevel), log.StreamHandler(colorable.NewColorableStderr(), log.TerminalFormat(!*rawlog))))
+}
 
 func TestAdapters(t *testing.T) {
 
-	nodeCount := 10
+	nodeCount := *nodes
 
 	// Test exec adapter
 	t.Run("exec", func(t *testing.T) {
@@ -37,11 +55,7 @@ func TestAdapters(t *testing.T) {
 	// Test docker adapter
 	t.Run("docker", func(t *testing.T) {
 		config := DefaultDockerAdapterConfig()
-		config.DockerImage = "sim-docker-test:latest"
-		/*config.BuildContext.Dockerfile = "Dockerfile"
-		config.BuildContext.Directory = "../"
-		config.BuildContext.Tag = "sim-docker-test:latest"*/
-
+		config.DockerImage = "ethersphere/swarm:edge"
 		adapter, err := NewDockerAdapter(config)
 		if err != nil {
 			t.Fatalf("could not create docker adapter: %v", err)
@@ -53,14 +67,7 @@ func TestAdapters(t *testing.T) {
 	t.Run("kubernetes", func(t *testing.T) {
 		config := DefaultKubernetesAdapterConfig()
 		config.Namespace = "simulation-test"
-		config.DockerImage = "skylenet/swarm-test:why"
-		/*config.BuildContext.Dockerfile = "Dockerfile"
-		config.BuildContext.Directory = "../"
-		config.BuildContext.Tag = "swarm-test:why"
-		config.BuildContext.Registry = "skylenet"
-		config.BuildContext.Username = "skylenet"
-		config.BuildContext.Password = "xxxxxxxx"*/
-
+		config.DockerImage = "ethersphere/swarm:edge"
 		adapter, err := NewKubernetesAdapter(config)
 		if err != nil {
 			t.Fatalf("could not create kubernetes adapter: %v", err)
@@ -101,79 +108,121 @@ func startSimulation(t *testing.T, adapter Adapter, count int) {
 		cfg := NodeConfig{
 			ID:     nodeIDs[i],
 			Args:   args,
-			Stdout: os.Stdout,
-			Stderr: os.Stderr,
+			Stdout: ioutil.Discard,
+			Stderr: ioutil.Discard,
 		}
 
-		if err := sim.Create(cfg); err != nil {
+		if err := sim.Init(cfg); err != nil {
 			t.Fatalf("failed to create node %s: %v", cfg.ID, err)
 		}
 
 	}
 
 	// Start nodes
-	for _, id := range nodeIDs {
-		err := sim.Start(id)
-		if err != nil {
-			t.Errorf("failed to start node %s: %v", id, err)
-		}
-	}
+	now := time.Now()
 
+	log.Info("Starting nodes...", "count", count)
+
+	err := sim.StartAll()
+
+	log.Info("Started nodes", "time", fmt.Sprintf("%fs", time.Since(now).Seconds()))
+
+	if err != nil {
+		sim.StopAll()
+		t.Fatalf("failed to start nodes: %v", err)
+	}
 	defer func() {
-		// Stop nodes
-		for _, id := range nodeIDs {
-			err := sim.Stop(id)
-			if err != nil {
-				t.Errorf("failed to stop node %s: %v", id, err)
-			}
+		err := sim.StopAll()
+		if err != nil {
+			t.Fatalf("could not stop all nodes: %v", err)
 		}
 	}()
 
-	// Test some RPC calls
-	nodes := make([]Node, count)
-	rpcClients := make([]*rpc.Client, count)
+	// Generate RPC clients
 
-	for idx, id := range nodeIDs {
-
-		node, err := sim.Get(id)
-		nodes[idx] = node
-		if err != nil {
-			t.Fatalf("could not get node %s: %v", id, err)
-		}
-		client, err := sim.RPCClient(id)
-		if err != nil {
-			t.Fatalf("failed to get an rpc client for node %s: %v", id, err)
-			continue
-		}
-		defer client.Close()
-		rpcClients[idx] = client
-
-		var swarminfo swarm.Info
-		err = client.Call(&swarminfo, "bzz_info")
-		if err != nil {
-			t.Errorf("failed getting bzz info: %v", err)
-		}
+	var clients struct {
+		RPC []*rpc.Client
+		mu  sync.Mutex
 	}
+	clients.RPC = make([]*rpc.Client, count)
 
-	// Add all nodes to each other
-	for i := 0; i < count; i++ {
-		for j := 0; j < count; j++ {
-			if i == j {
-				continue
-			}
-			err := rpcClients[i].Call(nil, "admin_addPeer", nodes[j].Status().Enode)
+	nodes := sim.GetAll()
+	var wg sync.WaitGroup
+	wg.Add(len(nodes))
+
+	for idx, node := range nodes {
+		go func(node Node, idx int) {
+			defer wg.Done()
+			id := node.Status().ID
+			log.Info("getting rpc client", "node", id)
+			client, err := sim.RPCClient(id)
 			if err != nil {
-				t.Errorf("could not add peer %s: %v", nodes[j].Status().ID, err)
+				t.Errorf("failed to get an rpc client for node %s: %v", id, err)
 			}
-		}
+			clients.mu.Lock()
+			clients.RPC[idx] = client
+			clients.mu.Unlock()
+		}(node, idx)
+	}
+	wg.Wait()
 
+	log.Info("Adding peers...")
+	for i := 0; i < count-1; i++ {
+		go func(idx int) {
+			err := clients.RPC[idx].Call(nil, "admin_addPeer", nodes[idx+1].Status().Enode)
+			if err != nil {
+				t.Errorf("could not add peer %s: %v", nodes[idx+1].Status().ID, err)
+			}
+		}(i)
 	}
 
+	// Wait for healthy kademlia on all peers
+	addrs := [][]byte{}
+
+	for _, node := range nodes {
+		byteaddr, err := hexutil.Decode(node.Status().BzzAddr)
+		if err != nil {
+			t.Fatalf("failed to decode hex")
+		}
+		addrs = append(addrs, byteaddr)
+	}
+
+	ppmap := network.NewPeerPotMap(network.NewKadParams().NeighbourhoodSize, addrs)
+
+	log.Info("Waiting for healthy kademlia...")
+
+outer:
+	for {
+		for i := 0; i < count; i++ {
+			healthy := &network.Health{}
+
+			if err := clients.RPC[i].Call(&healthy, "hive_getHealthInfo", ppmap[nodes[i].Status().BzzAddr[2:]]); err != nil {
+				t.Errorf("failed to call hive_getHealthInfo")
+			}
+			if !healthy.Healthy() {
+				log.Info("Node isn't healthy, checking again all nodes...", "node", nodes[i].Status().ID)
+				time.Sleep(500 * time.Millisecond)
+				continue outer
+			}
+		}
+		break
+	}
+
+	// Check hive output
 	var hive string
-	err := rpcClients[0].Call(&hive, "bzz_hive")
+	err = clients.RPC[0].Call(&hive, "bzz_hive")
 	if err != nil {
 		t.Errorf("could not get hive info: %v", err)
 	}
-
 	fmt.Println(hive)
+
+	var knownPeers []string
+	err = clients.RPC[0].Call(&knownPeers, "bzz_listKnown")
+	if err != nil {
+		t.Errorf("could not get known peers")
+	}
+
+	if len(knownPeers) != count-1 {
+		t.Errorf("wrong known peer count. Should be %d, was %d", count-1, len(knownPeers))
+	}
 }
