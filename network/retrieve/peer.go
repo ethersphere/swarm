377aa20ecb858d90b842e9738f79ed8c04be1421
acud <theman@elad.im>
2019-07-22 16:43:22 +0530

    network/retrieve: cleanup before PRing protocol

--- network/retrieve/peer.go
+++ network/retrieve/peer.go
@@ -17,27 +17,19 @@
 package retrieve
 
 import (
-	"errors"
-
 	"github.com/ethersphere/swarm/log"
 	"github.com/ethersphere/swarm/network"
 )
 
-// ErrMaxPeerServers will be returned if peer server limit is reached.
-// It will be sent in the SubscribeErrorMsg.
-var ErrMaxPeerServers = errors.New("max peer servers")
-
 // Peer is the Peer extension for the streaming protocol
 type Peer struct {
 	*network.BzzPeer
-	quit chan struct{}
 }
 
 // NewPeer is the constructor for Peer
 func NewPeer(peer *network.BzzPeer) *Peer {
 	p := &Peer{
 		BzzPeer: peer,
-		quit:    make(chan struct{}),
 	}
 	return p
 }
