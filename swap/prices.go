cc61dfb8ae63bc22d41084bd9c14ff55dfe81d76
holisticode <holistic.computing@gmail.com>
2019-07-24 14:06:03 -0500

    swap: Incentives prices (#1587)
    * swap: correct price handling and reset balances
    
    * swap: conversion to wei on cheque creation; cleanup
    
    * swap: bug fixes and TestEmitCheque
    
    * swap: addressed PR comments
    
    * swap: minor fixes due to PR comments

---
+++ swap/prices.go
@@ -0,0 +1,42 @@
+// Copyright 2019 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package swap
+
+/*
+This module contains the pricing for message types as constants.
+
+Pricing in Swarm is defined in an internal unit.
+The name of this internal unit is honey.
+The honey unit allows to set prices of messages relative to each other
+without any dependancy to any currency.
+
+The expectation is then that an external, probably on-chain, **oracle**
+would be queried with the total amount of honey for a message,
+for which the oracle would return the price in a given currency.
+
+Currently the expected currency from the oracle would be wei,
+but it could potentially be any currency the oracle and Swarm support,
+allowing for a multi-currency design.
+*/
+
+// Placeholder prices
+const (
+	RetrieveRequestMsgPrice        = uint64(1)
+	ChunkDeliveryMsgRetrievalPrice = uint64(1)
+	// default convertion of honey into output currency - currently ETH
+	defaultHoneyPrice = uint64(1)
+)
