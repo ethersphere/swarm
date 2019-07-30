b148e74bd3262a094a29f94e9261a7b8bc6ba383
mortelli <ortellimarcelo@gmail.com>
2019-07-30 14:28:36 -0300

    swap: structure logging calls in peer

--- swap/peer.go
+++ swap/peer.go
@@ -90,7 +90,7 @@ func (sp *Peer) handleEmitChequeMsg(ctx context.Context, msg *EmitChequeMsg) err
 
 	otherSwap, err := cswap.InstanceAt(cheque.Contract, sp.backend)
 	if err != nil {
-		log.Error("could not get an instance of simpleSwap", "error", err.Error())
+		log.Error("could not get an instance of simpleSwap", "error", err)
 		return err
 	}
 
@@ -99,7 +99,7 @@ func (sp *Peer) handleEmitChequeMsg(ctx context.Context, msg *EmitChequeMsg) err
 		// blocks here, as we are waiting for the transaction to be mined
 		receipt, err := otherSwap.SubmitChequeBeneficiary(opts, sp.backend, big.NewInt(int64(cheque.Serial)), big.NewInt(int64(cheque.Amount)), big.NewInt(int64(cheque.Timeout)), cheque.Sig)
 		if err != nil {
-			log.Error("Got error when calling submitChequeBeneficiary", "err", err.Error())
+			log.Error("Got error when calling submitChequeBeneficiary", "error", err)
 			//TODO: do something with the error
 			return
 		}
