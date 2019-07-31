fb92e77b7d34a8506c3d9a3975be19c2581beb33
Janos Guljas <janos@resenje.org>
2019-07-31 15:10:34 +0200

    chunk: avoid allocations in Address methods

--- chunk/chunk.go
+++ chunk/chunk.go
@@ -18,6 +18,7 @@ package chunk
 
 import (
 	"context"
+	"encoding/hex"
 	"errors"
 	"fmt"
 
@@ -69,18 +70,18 @@ type Address []byte
 var ZeroAddr = Address(common.Hash{}.Bytes())
 
 func (a Address) Hex() string {
-	return fmt.Sprintf("%064x", []byte(a[:]))
+	return hex.EncodeToString(a)
 }
 
 func (a Address) Log() string {
-	if len(a[:]) < 8 {
-		return fmt.Sprintf("%x", []byte(a[:]))
+	if len(a) < 8 {
+		return hex.EncodeToString(a)
 	}
-	return fmt.Sprintf("%016x", []byte(a[:8]))
+	return hex.EncodeToString(a[:8])
 }
 
 func (a Address) String() string {
-	return fmt.Sprintf("%064x", []byte(a))
+	return hex.EncodeToString(a)
 }
 
 func (a Address) MarshalJSON() (out []byte, err error) {
