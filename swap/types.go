ae43ed35e21765866c16d4cdd053e99aa07efab3
Rinke Hendriksen <33000441+Eknir@users.noreply.github.com>
2019-08-05 17:36:38 +0200

    removed TODOs which probably should not be fixed before merge (#1637)
    * removed TODOs which probably should not be fixed before merge
    
    * more TODOs removed
    
    * removed one more TODO
    
    * removed more TODOs
    
    * after review holisticode
    
    * referenced issue at put back TODO
    
    * swap: put in-code TODO back and changed position of NOTE
    
    * swap: restore 2 comments in handleEmitChequeMsg function

--- swap/types.go
+++ swap/types.go
@@ -20,9 +20,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-// TODO: add handshake protocol where we exchange last cheque (this is useful if one node disconnects)
-// FIXME: Check the contract bytecode of the counterparty
-
 // ChequeParams encapsulate all cheque parameters
 type ChequeParams struct {
 	Contract    common.Address // address of chequebook, needed to avoid cross-contract submission
@@ -34,7 +31,6 @@ type ChequeParams struct {
 }
 
 // Cheque encapsulates the parameters and the signature
-// TODO: There should be a request cheque struct that only gives the Serial
 type Cheque struct {
 	ChequeParams
 	Signature []byte // signature Sign(Keccak256(contract, beneficiary, amount), prvKey)
