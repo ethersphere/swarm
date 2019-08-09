df7cc485a06f0d0c2727fc428300926d6bed458f
mortelli <ortellimarcelo@gmail.com>
2019-08-09 08:55:19 -0300

    api, main, swarm: rename the config param BackendURL to SwapBackendURL (#1660)

--- cmd/swarm/flags.go
+++ cmd/swarm/flags.go
@@ -60,10 +60,10 @@ var (
 		Usage:  "Swarm SWAP enabled (default false)",
 		EnvVar: SwarmEnvSwapEnable,
 	}
-	SwarmBackendURLFlag = cli.StringFlag{
-		Name:   "backend-url",
+	SwarmSwapBackendURLFlag = cli.StringFlag{
+		Name:   "swap-backend-url",
 		Usage:  "URL of the Ethereum API provider to use to settle SWAP payments",
-		EnvVar: SwarmEnvBackendURL,
+		EnvVar: SwarmEnvSwapBackendURL,
 	}
 	SwarmSyncDisabledFlag = cli.BoolTFlag{
 		Name:   "nosync",
