854ce53c4152dd22cdfd004e4c415c4e2199a10b
mortelli <ortellimarcelo@gmail.com>
2019-08-13 16:29:30 -0300

    swap: return err in Swap Stop and Close functions

--- swap/protocol.go
+++ swap/protocol.go
@@ -83,8 +83,7 @@ func (s *Swap) Start(server *p2p.Server) error {
 // Stop is a node.Service interface method
 func (s *Swap) Stop() error {
 	log.Info("Swap service stopping")
-	s.Close()
-	return nil
+	return s.Close()
 }
 
 // verifyHandshake verifies the chequebook address transmitted in the swap handshake
