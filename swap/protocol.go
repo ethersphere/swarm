480c1d3bdd6dcf70fe3411c5881c6552dccb01be
holisticode <holistic.computing@gmail.com>
2019-08-15 15:01:42 -0500

    swap: refactored locks (#1671)
    * swap: refactored locks
    
    * swap: removed obsolete Println
    
    * swap: refactor access to swap cheques map through getters and setters
    
    * swap: rename lock field to accountingLock
    
    * swap: add mutex comments to updateBalance and loadBalance functions
    
    * swap: add mutex comments to loadLastSentCheque function
    
    * swap: refactor mapLock variable into 3 individual locks
    
    * swap: add mutex comments to createCheque and sendCheque functions

--- swap/protocol.go
+++ swap/protocol.go
@@ -129,18 +129,20 @@ func (s *Swap) run(p *p2p.Peer, rw p2p.MsgReadWriter) error {
 }
 
 func (s *Swap) removePeer(p *Peer) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
+	s.peersLock.Lock()
+	defer s.peersLock.Unlock()
 	delete(s.peers, p.ID())
 }
 
 func (s *Swap) addPeer(p *Peer) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
+	s.peersLock.Lock()
+	defer s.peersLock.Unlock()
 	s.peers[p.ID()] = p
 }
 
 func (s *Swap) getPeer(id enode.ID) (*Peer, bool) {
+	s.peersLock.RLock()
+	defer s.peersLock.RUnlock()
 	peer, ok := s.peers[id]
 	return peer, ok
 }
