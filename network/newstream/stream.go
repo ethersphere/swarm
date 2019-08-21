6bb336294e3e68cf7331c881522a7e13b5ac17de
acud <theman@elad.im>
2019-08-21 18:57:31 +0530

    network/newstream: experiment with adding cache

--- network/newstream/stream.go
+++ network/newstream/stream.go
@@ -802,7 +802,7 @@ func (r *Registry) serverHandleWantedHashes(ctx context.Context, p *Peer, msg *W
 	}
 
 	frameSize := 0
-	var maxFrame = 1 //BatchSize / 4 // should be BatchSize but testing to see if this makes a difference as its the major change from existing stream pkg
+	var maxFrame = BatchSize / 4 // should be BatchSize but testing to see if this makes a difference as its the major change from existing stream pkg
 	if maxFrame < 1 {
 		maxFrame = 1
 	}
