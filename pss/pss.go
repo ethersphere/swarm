a6036b6c88a51df5bd39795720659a963ee4198b
Alvaro <kortatu@gmail.com>
2019-08-23 10:29:18 +0200

    pss: Log crit and panic in an unexpected situation when outbox is full for a reenqued message

--- pss/pss.go
+++ pss/pss.go
@@ -580,10 +580,10 @@ func (p *Pss) enqueue(msg *PssMsg, pending bool) error {
 	}
 	metrics.GetOrRegisterCounter("pss.enqueue.outbox.full", nil).Inc(1)
 	if pending {
-		return errors.New("unexpected outbox full for pending message!")
-	} else {
-		return errors.New("outbox full")
+		log.Crit("unexpected outbox full for pending message!")
+		panic(errors.New("unexpected outbox full for pending message!"))
 	}
+	return errors.New("outbox full")
 }
 
 // Access to forwardPending is protected with RWMutex
