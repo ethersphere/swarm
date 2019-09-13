6e47cbe0e45c8ad01ff40e9e6aa1710cc59baadf
Fabio Barone <holistic.computing@gmail.com>
2019-09-12 19:50:38 -0500

    swap: ConnectFull not Chain

--- swap/simulations_test.go
+++ swap/simulations_test.go
@@ -243,7 +243,7 @@ func TestPingPongChequeSimulation(t *testing.T) {
 	ctx, cancelSimRun := context.WithTimeout(context.Background(), 1*time.Minute)
 	defer cancelSimRun()
 
-	_, err = sim.AddNodesAndConnectChain(nodeCount)
+	_, err = sim.AddNodesAndConnectFull(nodeCount)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -280,7 +280,7 @@ func TestPingPongChequeSimulation(t *testing.T) {
 			default:
 			}
 			// the node has all other peers in its peer list
-			if len(ts1.peers) == 1 && len(ts2.peers) == 1 {
+			if len(ts1.swap.peers) == 1 && len(ts2.swap.peers) == 1 {
 				break
 			}
 			// don't overheat the CPU...
@@ -367,7 +367,7 @@ func TestMultiChequeSimulation(t *testing.T) {
 	ctx, cancelSimRun := context.WithTimeout(context.Background(), 1*time.Minute)
 	defer cancelSimRun()
 
-	_, err = sim.AddNodesAndConnectChain(nodeCount)
+	_, err = sim.AddNodesAndConnectFull(nodeCount)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -408,7 +408,7 @@ func TestMultiChequeSimulation(t *testing.T) {
 			default:
 			}
 			// the node has all other peers in its peer list
-			if len(debitorSvc.peers) == 1 && len(creditorSvc.peers) == 1 {
+			if len(debitorSvc.swap.peers) == 1 && len(creditorSvc.swap.peers) == 1 {
 				break
 			}
 			// don't overheat the CPU...
