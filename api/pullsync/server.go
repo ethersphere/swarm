67b704bd1a3702df5b5036fd6c950e864fc7790c
zelig <viktor.tron@gmail.com>
2019-09-16 06:33:00 +0200

    removed accidentally added files

--- api/pullsync/server.go
+++
@@ -1,9 +0,0 @@
-package pullsync
-
-// pullSync.Server implements stream.Provider
-// uses localstore SubscribePull for the  bins
-// server is node-wide
-type Server struct {
-	// ...
-	*stream.LocalProvider
-}
