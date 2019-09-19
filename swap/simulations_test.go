26b56ea990a2cb5316b272df2c1bcbeeb5e09abf
Fabio Barone <holistic.computing@gmail.com>
2019-09-19 11:35:03 -0500

    swap: rebased to latest master; PR comments

--- swap/simulations_test.go
+++ swap/simulations_test.go
@@ -36,6 +36,7 @@ import (
 	"github.com/ethereum/go-ethereum/node"
 	"github.com/ethereum/go-ethereum/p2p"
 	"github.com/ethereum/go-ethereum/p2p/enode"
+	"github.com/ethereum/go-ethereum/p2p/simulations"
 	"github.com/ethereum/go-ethereum/p2p/simulations/adapters"
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/ethersphere/swarm/network"
@@ -89,6 +90,7 @@ func newTestSpec() *protocols.Spec {
 
 // testService encapsulates objects needed for the simulation
 type testService struct {
+	lock  sync.Mutex
 	swap  *Swap
 	spec  *protocols.Spec
 	peers map[enode.ID]*testPeer
@@ -496,24 +498,8 @@ func TestBasicSwapSimulation(t *testing.T) {
 	}
 
 	log.Debug("Wait for all connections to be established")
-	// first check: let's make sure that all nodes have been connected to the others
-	// this is the maximum number of possible connections
-	maxConns := nodeCount * (nodeCount - 1) / 2
-CONNS:
-	for {
-		select {
-		// let's be nice and make sure we catch a timeout
-		case <-ctx.Done():
-			t.Fatal("Timed out waiting for all connections to be established")
-		default:
-			// this should be true when all connections have been established
-			if len(sim.Net.Conns) == maxConns {
-				break CONNS
-			}
-		}
-		// don't overheat the CPU...
-		time.Sleep(5 * time.Millisecond)
-	}
+
+	simulations.VerifyFull(t, sim.Net, sim.NodeIDs())
 
 	log.Info("starting simulation...")
 
@@ -553,10 +539,6 @@ CONNS:
 
 		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 		defer cancel()
-		_, err = sim.WaitTillHealthy(ctx)
-		if err != nil {
-			return err
-		}
 
 		nodes := sim.UpNodeIDs()
 		msgCount := 0
@@ -605,7 +587,9 @@ CONNS:
 					}
 					if msgCount < maxMsgs {
 
+						ts.lock.Lock()
 						tp := ts.peers[p]
+						ts.lock.Unlock()
 						if tp == nil {
 							return errors.New("peer is nil")
 						}
@@ -729,7 +713,9 @@ func (ts *testService) APIs() []rpc.API {
 func (ts *testService) runProtocol(p *p2p.Peer, rw p2p.MsgReadWriter) error {
 	peer := protocols.NewPeer(p, rw, ts.spec)
 	tp := &testPeer{Peer: peer}
+	ts.lock.Lock()
 	ts.peers[tp.ID()] = tp
+	ts.lock.Unlock()
 	return peer.Run(tp.handleMsg)
 }
 
