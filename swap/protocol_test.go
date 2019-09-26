bbf2f9beee699fb5e1fecf1cde0298cea4ea3b65
eknir <rinkehendriksen@gmail.com>
2019-09-26 15:03:27 +0200

    swap: remove auditlog from protocol_test.go

--- swap/protocol_test.go
+++ swap/protocol_test.go
@@ -42,7 +42,6 @@ import (
 func init() {
 	log.PrintOrigins(true)
 	log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(*loglevel), log.StreamHandler(colorable.NewColorableStderr(), log.TerminalFormat(true))))
-	auditLog = newLogger("")
 }
 
 /*
