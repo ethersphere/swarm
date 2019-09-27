b0b7fc48fbd082600e2dd53d01c56be13cb29215
acud <theman@elad.im>
2019-09-27 18:23:46 +0530

    swarm: make pullsync disabled not expose stream protocol (#1830)
    * swarm: make pullsync disabled not expose stream protocol
    
    * network: remove spurious logline from hive
    
    * swarm: add comment

--- swarm.go
+++ swarm.go
@@ -559,7 +559,12 @@ func (s *Swarm) APIs() []rpc.API {
 	}
 
 	apis = append(apis, s.bzz.APIs()...)
-	apis = append(apis, s.streamer.APIs()...)
+
+	// this is a workaround disabling syncing altogether from a node but
+	// must be changed when multiple stream implementations are at hand
+	if s.config.SyncEnabled {
+		apis = append(apis, s.streamer.APIs()...)
+	}
 	apis = append(apis, s.bzzEth.APIs()...)
 
 	if s.ps != nil {
