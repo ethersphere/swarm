228c401a4abb52ad8db05053882c0cb001de2208
acud <theman@elad.im>
2019-10-08 10:59:06 +0530

    api/http: remove Gateway word from landing page

--- api/http/templates.go
+++ api/http/templates.go
@@ -295,7 +295,7 @@ const baseTemplate = `
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Swarm Gateway</title>
+	<title>Swarm</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
 	<meta http-equiv="X-UA-Compatible" ww="chrome=1"/>
