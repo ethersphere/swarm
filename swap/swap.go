5fde2a53cb7a9301051be54ce8b4ac0a83524270
mortelli <ortellimarcelo@gmail.com>
2019-11-12 13:22:21 -0300

    swap: extend comment in Add function

--- swap/swap.go
+++ swap/swap.go
@@ -300,7 +300,7 @@ func (s *Swap) Add(amount int64, peer *protocols.Peer) (err error) {
 	swapPeer.lock.Lock()
 	defer swapPeer.lock.Unlock()
 
-	// Check if balance with peer is over the disconnect threshold
+	// Check if balance with peer is over the disconnect threshold and if the message would increase the existing debt
 	balance := swapPeer.getBalance()
 	if balance >= s.params.DisconnectThreshold && amount > 0 {
 		return fmt.Errorf("balance for peer %s is over the disconnect threshold %d and cannot incur more debt, disconnecting", peer.ID().String(), s.params.DisconnectThreshold)
