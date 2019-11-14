70d4fc030bd363eaf136c92a93c56cc24e04e99b
acud <theman@elad.im>
2019-11-14 18:07:10 +0530

    localstore: fix index leak when NN chunks enter localstore (#1940)
    * storage/localstore: fixes an index leak that was manifesting when chunks that would enter the node's local storage would, under certain connectivity conditions not be added to the GC index.
    
    This is due to the fact that the uploader node would be the closest node to the chunk, the retrievalDataIndex eventually grow out of bound (dbCapacity), since no matter how many chunks in lower bins would be garbage collected, the higher bins would always make the store size eventually overflow.
    
    This fix introduces a pluggable function to localstore that is called when a chunk enters the local storage.

--- storage/localstore/mode_put_test.go
+++ storage/localstore/mode_put_test.go
@@ -25,6 +25,7 @@ import (
 	"time"
 
 	"github.com/ethersphere/swarm/chunk"
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
 // TestModePutRequest validates ModePutRequest index values on the provided DB.
@@ -315,6 +316,126 @@ func TestModePut_sameChunk(t *testing.T) {
 				})
 			}
 		})
+	}
+}
+
+// TestModePutSync_addToGc validates ModePut* with PutSetCheckFunc stub results
+// in the added chunk to show up in GC index
+func TestModePut_addToGc(t *testing.T) {
+	retVal := true
+	// PutSetCheckFunc's output is toggled from the test case
+	opts := &Options{PutToGCCheck: func(_ []byte) bool { return retVal }}
+
+	for _, m := range []struct {
+		mode    chunk.ModePut
+		putToGc bool
+	}{
+		{mode: chunk.ModePutSync, putToGc: true},
+		{mode: chunk.ModePutSync, putToGc: false},
+		{mode: chunk.ModePutUpload, putToGc: true},
+		{mode: chunk.ModePutUpload, putToGc: false},
+		{mode: chunk.ModePutRequest, putToGc: true}, // in ModePutRequest we always insert to GC, so putToGc=false not needed
+	} {
+		for _, tc := range multiChunkTestCases {
+			t.Run(tc.name, func(t *testing.T) {
+				retVal = m.putToGc
+
+				db, cleanupFunc := newTestDB(t, opts)
+				defer cleanupFunc()
+
+				wantTimestamp := time.Now().UTC().UnixNano()
+				defer setNow(func() (t int64) {
+					return wantTimestamp
+				})()
+
+				chunks := generateTestRandomChunks(tc.count)
+
+				_, err := db.Put(context.Background(), m.mode, chunks...)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				binIDs := make(map[uint8]uint64)
+
+				for _, ch := range chunks {
+					po := db.po(ch.Address())
+					binIDs[po]++
+					var wantErr error
+					if !m.putToGc {
+						wantErr = leveldb.ErrNotFound
+					}
+					newRetrieveIndexesTestWithAccess(db, ch, wantTimestamp, wantTimestamp)
+					newGCIndexTest(db, ch, wantTimestamp, wantTimestamp, binIDs[po], wantErr)(t)
+				}
+			})
+		}
+	}
+}
+
+// TestModePutSync_addToGcExisting validates ModePut* with PutSetCheckFunc stub results
+// in the added chunk to show up in GC index
+func TestModePut_addToGcExisting(t *testing.T) {
+	retVal := true
+	// PutSetCheckFunc's output is toggled from the test case
+	opts := &Options{PutToGCCheck: func(_ []byte) bool { return retVal }}
+
+	for _, m := range []struct {
+		mode    chunk.ModePut
+		putToGc bool
+	}{
+		{mode: chunk.ModePutSync, putToGc: true},
+		{mode: chunk.ModePutSync, putToGc: false},
+		{mode: chunk.ModePutUpload, putToGc: true},
+		{mode: chunk.ModePutUpload, putToGc: false},
+		{mode: chunk.ModePutRequest, putToGc: true}, // in ModePutRequest we always insert to GC, so putToGc=false not needed
+	} {
+		for _, tc := range multiChunkTestCases {
+			t.Run(tc.name, func(t *testing.T) {
+				retVal = m.putToGc
+
+				db, cleanupFunc := newTestDB(t, opts)
+				defer cleanupFunc()
+
+				wantStoreTimestamp := time.Now().UTC().UnixNano()
+				defer setNow(func() (t int64) {
+					return wantStoreTimestamp
+				})()
+
+				chunks := generateTestRandomChunks(tc.count)
+
+				_, err := db.Put(context.Background(), m.mode, chunks...)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				time.Sleep(1 * time.Millisecond)
+				// change the timestamp, put the chunks again and
+				// expect the access timestamp to change
+				wantAccessTimestamp := time.Now().UTC().UnixNano()
+				defer setNow(func() (t int64) {
+					return wantAccessTimestamp
+				})()
+
+				_, err = db.Put(context.Background(), m.mode, chunks...)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				binIDs := make(map[uint8]uint64)
+
+				for _, ch := range chunks {
+					po := db.po(ch.Address())
+					binIDs[po]++
+					var wantErr error
+					if !m.putToGc {
+						wantErr = leveldb.ErrNotFound
+					}
+
+					newRetrieveIndexesTestWithAccess(db, ch, wantStoreTimestamp, wantAccessTimestamp)
+					newGCIndexTest(db, ch, wantStoreTimestamp, wantAccessTimestamp, binIDs[po], wantErr)(t)
+				}
+			})
+		}
 	}
 }
 
