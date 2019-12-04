3953663d3e55579df3baaa1cf37b48093fae1380
nolash <dev@holbrook.no>
2019-12-04 13:20:59 +0100

    file: Add key derivation to filehasher encrypt

--- file/encrypt/encrypt.go
+++ file/encrypt/encrypt.go
@@ -2,6 +2,9 @@ package encrypt
 
 import (
 	"context"
+	crand "crypto/rand"
+	"fmt"
+	"hash"
 
 	"github.com/ethersphere/swarm/param"
 	"github.com/ethersphere/swarm/storage/encryption"
@@ -9,15 +12,32 @@ import (
 )
 
 type Encrypt struct {
+	key     []byte
 	e       encryption.Encryption
 	w       param.SectionWriter
+	keyHash hash.Hash
 	errFunc func(error)
 }
 
-func New(key []byte, initCtr uint32) *Encrypt {
-	return &Encrypt{
-		e: encryption.New(key, 0, initCtr, sha3.NewLegacyKeccak256),
+func New(key []byte, initCtr uint32) (*Encrypt, error) {
+	e := &Encrypt{
+		e:       encryption.New(key, 0, initCtr, sha3.NewLegacyKeccak256),
+		key:     make([]byte, encryption.KeyLength),
+		keyHash: param.HashFunc(),
 	}
+	if key == nil {
+		e.key = make([]byte, encryption.KeyLength)
+		c, err := crand.Read(e.key)
+		if err != nil {
+			return nil, err
+		}
+		if c < encryption.KeyLength {
+			return nil, fmt.Errorf("short read: %d", c)
+		}
+	} else {
+		copy(e.key, key)
+	}
+	return e, nil
 }
 
 func (e *Encrypt) Init(_ context.Context, errFunc func(error)) {
@@ -42,7 +62,13 @@ func (e *Encrypt) Reset(ctx context.Context) {
 }
 
 func (e *Encrypt) Sum(b []byte, length int, span []byte) []byte {
-	return e.w.Sum(b, length, span)
+	oldKey := make([]byte, 32)
+	copy(oldKey, e.key)
+	e.keyHash.Reset()
+	e.keyHash.Write(e.key)
+	copy(e.key, e.keyHash.Sum(nil))
+	s := e.w.Sum(b, length, span)
+	return append(oldKey, s...)
 }
 
 func (e *Encrypt) DigestSize() int {
--- file/encrypt/encrypt_test.go
+++ file/encrypt/encrypt_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethersphere/swarm/bmt"
 	"github.com/ethersphere/swarm/file/hasher"
 	"github.com/ethersphere/swarm/file/testutillocal"
@@ -25,6 +26,51 @@ const (
 
 func init() {
 	testutil.Init()
+}
+
+func TestKey(t *testing.T) {
+
+	e, err := New(nil, 42)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if e.key == nil {
+		t.Fatalf("new key nil; expected not nil")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+	defer cancel()
+	errFunc := func(error) {}
+	cache := testutillocal.NewCache()
+	cache.Init(ctx, errFunc)
+	cacheFunc := func() param.SectionWriter {
+		return cache
+	}
+	key := [32]byte{}
+	key[0] = 0x2a
+	e, err = New(key[:], 42)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(key[:], e.key) {
+		t.Fatalf("key seed; expected %x, got %x", key, e.key)
+	}
+
+	_, data := testutil.SerialData(chunkSize, 255, 0)
+	e.Link(cacheFunc)
+	e.Write(0, data)
+	span := bmt.LengthToSpan(chunkSize)
+	doubleRef := e.Sum(nil, chunkSize, span)
+	refKey := doubleRef[:encryption.KeyLength]
+	if !bytes.Equal(refKey, key[:]) {
+		t.Fatalf("returned ref key, expected %x, got %x", key, refKey)
+	}
+
+	correctNextKeyHex := "0xd83b8137defe4bdaf5e1243b3175dc49b0a19c9d1f68044b7bf261db9f006233"
+	nextKeyHex := hexutil.Encode(e.key)
+	if nextKeyHex != correctNextKeyHex {
+		t.Fatalf("key next; expected %s, got %s", correctNextKeyHex, nextKeyHex)
+	}
 }
 
 func TestEncryptOneChunk(t *testing.T) {
@@ -57,7 +103,10 @@ func TestEncryptOneChunk(t *testing.T) {
 		t.Fatalf("short read %d", c)
 	}
 	encryptFunc := func() param.SectionWriter {
-		eFunc := New(key, uint32(42))
+		eFunc, err := New(key, uint32(42))
+		if err != nil {
+			t.Fatal(err)
+		}
 		eFunc.Init(ctx, errFunc)
 		eFunc.Link(cacheFunc)
 		return eFunc
@@ -68,7 +117,7 @@ func TestEncryptOneChunk(t *testing.T) {
 	h.Init(ctx, func(error) {})
 	h.Link(refHashFunc)
 	h.Write(0, data)
-	ref := h.Sum(nil, 0, nil)
+	h.Sum(nil, 0, nil)
 
 	enc := encryption.New(key, 0, 42, sha3.NewLegacyKeccak256)
 	cipherText, err := enc.Encrypt(data)
@@ -81,3 +130,40 @@ func TestEncryptOneChunk(t *testing.T) {
 		t.Fatalf("encrypt onechunk; data mismatch")
 	}
 }
+
+//func TestEncryptIntermediateChunk(t *testing.T) {
+//	poolSync := bmt.NewTreePool(sha3.NewLegacyKeccak256, branches, bmt.PoolSize)
+//	poolAsync := bmt.NewTreePool(sha3.NewLegacyKeccak256, branches, bmt.PoolSize)
+//	refHashFunc := func() param.SectionWriter {
+//		return bmt.New(poolAsync).NewAsyncWriter(false)
+//	}
+//	dataHashFunc := func() param.SectionWriter {
+//		return hasher.NewBMTSyncSectionWriter(bmt.New(poolSync))
+//	}
+//
+//	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+//	defer cancel()
+//	errFunc := func(error) {}
+//
+//	cache := testutillocal.NewCache()
+//	cache.Init(ctx, errFunc)
+//	cache.Link(dataHashFunc)
+//	cacheFunc := func() param.SectionWriter {
+//		return cache
+//	}
+//
+//	key := make([]byte, encryption.KeyLength)
+//	c, err := crand.Read(key)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//	if c != encryption.KeyLength {
+//		t.Fatalf("short read %d", c)
+//	}
+//	encryptFunc := func() param.SectionWriter {
+//		eFunc := New(key, uint32(42))
+//		eFunc.Init(ctx, errFunc)
+//		eFunc.Link(cacheFunc)
+//		return eFunc
+//	}
+//}
