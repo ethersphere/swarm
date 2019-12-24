44aa724ff89af7707ad135dcd4f707dba94a05db
mortelli <ortellimarcelo@gmail.com>
2019-12-24 17:18:16 -0300

    swap: iterate Uint256 struct

--- swap/types.go
+++ swap/types.go
@@ -61,7 +61,14 @@ type Uint256 struct {
 var minUint256 = big.NewInt(0)
 var maxUint256, _ = new(big.Int).SetString("115792089237316195423570985008687907853269984665640564039457584007913129639935", 10) // 2^256 - 1 (base 10)
 
-// Value returns the underlying big int pointer for the Uint256 struct
+// NewUint256 returns a new Uint256 struct with a value of the given param
+func NewUint256(i uint64) *Uint256 {
+	var u *Uint256
+	u.value = new(big.Int).SetUint64(i) // any uint64 is good enough for a uint256
+	return u
+}
+
+// Value returns the underlying big.Int pointer for the Uint256 struct
 func (u *Uint256) Value() *big.Int {
 	return u.value
 }
