8a1464438717e1b4855f720c6c53f07aa306dfc2
Petar Radovic <petar.radovic@gmail.com>
2019-12-26 15:30:56 +0100

    network: fix stream protocol run

--- network/stream/stream.go
+++ network/stream/stream.go
@@ -144,6 +144,7 @@ func (r *Registry) Run(bp *network.BzzPeer) error {
 	// todo: log peer info appropriately
 	if err := sp.Peer.Run(r.HandleMsg(sp)); err != nil {
 		log.Error("Shutting down stream protocol.", "peer:", sp, "reason", err)
+		return err
 	}
 
 	log.Info("Shutting down stream protocol gracefully")
