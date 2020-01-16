040be9602b004701e9cf93e13f0d5350db1d42f6
Tomasz Zdybał <tomek@zdybal.lap.pl>
2020-01-16 13:46:16 +0100

    storage/localstore: more GC quantiles testing

--- storage/localstore/quantile_test.go
+++ storage/localstore/quantile_test.go
@@ -10,20 +10,12 @@ import (
 
 func TestSorting(t *testing.T) {
 	t.Parallel()
-	n := uint64(10)
-	m := 10 * n
-	data := make(quantiles, n)
 
-	rnd := rand.New(rand.NewSource(123)) // fixed seed for repeatable tests
-
-	for i := uint64(0); i < n; i++ {
-		data[i].numerator = rnd.Uint64() % m
-		data[i].denominator = rnd.Uint64() % m
-	}
-
+	n := 100
+	data := getRandomQuantiles(n, 123)
 	sort.Sort(data)
 
-	for i := uint64(1); i < n; i++ {
+	for i := 1; i < n; i++ {
 		// compare without trusting any methods - use common denominator
 		x, y := data[i], data[i-1]
 		if x.numerator*y.denominator < y.numerator*x.denominator {
@@ -52,8 +44,65 @@ func TestFraction(t *testing.T) {
 	for _, test := range tests {
 		t.Run(fmt.Sprintf("Fraction %d/%d", test.f.numerator, test.f.denominator), func(tt *testing.T) {
 			if math.Abs(test.f.Decimal()-test.r) > tolerance {
-				tt.Errorf("expected: %f, received: %f", test.r, test.f.Decimal())
+				tt.Errorf("expected: %f, got: %f", test.r, test.f.Decimal())
 			}
 		})
 	}
+}
+
+func TestClosest(t *testing.T) {
+	t.Parallel()
+
+	emptyQ := make(quantiles, 0)
+	if emptyQ.Closest(fraction{1, 2}) != nil {
+		t.Error("expected nil")
+	}
+
+	data := make(quantiles, 5)
+	data[0].numerator = 1
+	data[0].denominator = 2
+	data[1].numerator = 1
+	data[1].denominator = 3
+	data[2].numerator = 1
+	data[2].denominator = 4
+	data[3].numerator = 2
+	data[3].denominator = 3
+	data[4].numerator = 3
+	data[4].denominator = 4
+
+	sanityChecks := []fraction{
+		{0, 1}, {1, 1}, {1, 2}, {2, 2}, {2, 1},
+	}
+	for _, check := range sanityChecks {
+		if data.Closest(check) == nil {
+			t.Error("expected any value, got nil")
+		}
+	}
+
+	checks := []struct {
+		f        fraction
+		expected *quantile
+	}{
+		{fraction{1, 2}, &data[0]},
+	}
+
+	for _, check := range checks {
+		if c := data.Closest(check.f); c != check.expected {
+			t.Errorf("invalid quantile: expected fraction: %d/%d, got: %d/%d",
+				check.expected.numerator, check.expected.denominator, c.numerator, c.denominator)
+		}
+	}
+}
+
+func getRandomQuantiles(n int, seed int64) quantiles {
+	m := uint64(100 * n)
+	data := make(quantiles, n)
+
+	rnd := rand.New(rand.NewSource(seed)) // fixed seed for repeatable tests
+
+	for i := 0; i < n; i++ {
+		data[i].numerator = rnd.Uint64() % m
+		data[i].denominator = rnd.Uint64() % m
+	}
+	return data
 }
