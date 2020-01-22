1d885fc29af374e3033ee90ef304d4583a6eb69f
mortelli <ortellimarcelo@gmail.com>
2020-01-22 09:09:32 -0300

    swap: remove randomness in TestMultiChequeSimulation

--- swap/simulations_test.go
+++ swap/simulations_test.go
@@ -23,7 +23,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math/big"
-	"math/rand"
 	"os"
 	"strings"
 	"sync"
@@ -332,15 +331,9 @@ func TestMultiChequeSimulation(t *testing.T) {
 
 	paymentThreshold := debitorSvc.swap.params.PaymentThreshold
 
-	minCheques := 2
-	maxCheques := 5
-
+	chequesAmount := 4
 	msgsPerCheque := (uint64(paymentThreshold) / msgPrice) + 1 // +1 to round up without casting to float
-
-	minMsgs := msgsPerCheque * uint64(minCheques)
-	maxMsgs := msgsPerCheque * uint64(maxCheques)
-
-	msgAmount := rand.Intn(int(maxMsgs-minMsgs)) + int(minMsgs)
+	msgAmount := int(msgsPerCheque) * chequesAmount
 	log.Debug("sending %d messages", msgAmount)
 
 	// the peer object used for sending
