457b5693973c405afc5081854e980c13606fe2f3
nolash <dev@holbrook.no>
2020-02-11 14:51:02 +0100

    file: Remove premature code

--- file/hasher/util.go
+++ file/hasher/util.go
@@ -16,33 +16,6 @@ func generateSpanSizes(branches int, levels int) []int {
 	return spans
 }
 
-// calculates the section index of the given byte size
-func dataSizeToSectionIndex(length int, sectionSize int) int {
-	return (length - 1) / sectionSize
-}
-
-// calculates the section count of the given byte size
-func dataSizeToSectionCount(length int, sectionSize int) int {
-	return dataSizeToSectionIndex(length, sectionSize) + 1
-}
-
-// calculates the corresponding level section for a data section
-func dataSectionToLevelSection(p *treeParams, lvl int, sections int) int {
-	span := p.Spans[lvl]
-	return sections / span
-}
-
-// calculates the lower data section boundary of a level for which a data section is contained
-// the higher level use is to determine whether the final data section written falls within
-// a certain level's span
-func dataSectionToLevelBoundary(p *treeParams, lvl int, section int) int {
-	span := p.Spans[lvl+1]
-	spans := section / span
-	spanBytes := spans * span
-	//log.Trace("levelboundary", "spans", spans, "section", section, "span", span)
-	return spanBytes
-}
-
 // TODO: use params instead of sectionSize, branches
 // calculate the last level index which a particular data section count will result in.
 // the returned level will be the level of the root hash
