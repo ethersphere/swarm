48143c6e84bc4420e17216c3419fc9dde344e9ab
acud <12988138+acud@users.noreply.github.com>
2020-03-04 12:05:42 +0800

    still works

--- storage/fcds/fcds.go
+++ storage/fcds/fcds.go
@@ -51,7 +51,7 @@ type Storer interface {
 var _ Storer = new(Store)
 
 // Number of files that store chunk data.
-var ShardCount = uint8(4)
+var ShardCount = uint8(16)
 
 // ErrStoreClosed is returned if store is already closed.
 var (
