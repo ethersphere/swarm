a2b3d25b640e874ad543cfe843cb920dd058a4fd
Zahoor Mohamed <zahoor@zahoor.in>
2020-03-16 21:43:32 +0530

    storage/fcds: badger as fcds with benchmarks

--- go.mod
+++ go.mod
@@ -3,67 +3,44 @@ module github.com/ethersphere/swarm
 go 1.13
 
 require (
-	bazil.org/fuse v0.0.0-20160811212531-371fbbdaa898
-	github.com/Azure/azure-pipeline-go v0.0.0-20180607212504-7571e8eb0876 // indirect
-	github.com/Azure/azure-storage-blob-go v0.0.0-20180712005634-eaae161d9d5e
-	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
-	github.com/Microsoft/go-winio v0.4.13 // indirect
-	github.com/Microsoft/hcsshim v0.8.6 // indirect
+	bazil.org/fuse v0.0.0-20200117225306-7b5117fecadc
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/VividCortex/ewma v1.1.1 // indirect
-	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
-	github.com/containerd/containerd v1.2.7 // indirect
-	github.com/containerd/continuity v0.0.0-20190426062206-aaeac12a7ffc // indirect
+	github.com/containerd/continuity v0.0.0-20200228182428-0f16d7a0959c // indirect
+	github.com/dgraph-io/badger v1.6.0
+	github.com/dgraph-io/badger/v2 v2.0.2
 	github.com/docker/distribution v2.7.1+incompatible // indirect
-	github.com/docker/docker v0.7.3-0.20190806133308-ecdb0b22393b
+	github.com/docker/docker v1.4.2-0.20180625184442-8e610b2b55bf
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
-	github.com/ethereum/go-ethereum v1.9.2
-	github.com/ethersphere/go-sw3 v0.2.1
-	github.com/fatih/color v1.7.0 // indirect
+	github.com/ethereum/go-ethereum v1.9.11
+	github.com/ethersphere/go-sw3 v0.2.3
 	github.com/fjl/memsize v0.0.0-20180418122429-ca190fb6ffbc
-	github.com/go-kit/kit v0.9.0 // indirect
-	github.com/go-logfmt/logfmt v0.4.0 // indirect
-	github.com/gogo/protobuf v1.2.1 // indirect
-	github.com/golang/protobuf v1.3.2 // indirect
-	github.com/googleapis/gnostic v0.0.0-20190624222214-25d8b0b66985 // indirect
-	github.com/gorilla/mux v1.7.3 // indirect
 	github.com/hashicorp/golang-lru v0.5.3
-	github.com/json-iterator/go v1.1.7 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
-	github.com/kylelemons/godebug v1.1.0 // indirect
+	github.com/imdario/mergo v0.3.8 // indirect
 	github.com/mattn/go-colorable v0.1.2
 	github.com/mattn/go-isatty v0.0.8
-	github.com/morikuni/aec v0.0.0-20170113033406-39771216ff4c // indirect
-	github.com/naoina/go-stringutil v0.1.0 // indirect
-	github.com/naoina/toml v0.0.0-20170918210437-9fafd6967416
-	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/naoina/toml v0.1.2-0.20170918210437-9fafd6967416
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/opencontainers/runc v0.1.1 // indirect
 	github.com/opentracing/opentracing-go v1.1.0
 	github.com/pborman/uuid v0.0.0-20170112150404-1b00554d8222
-	github.com/peterh/liner v0.0.0-20190123174540-a2c9a5303de7 // indirect
-	github.com/rnsdomains/rns-go-lib v0.0.0-20191114120302-3505575b0b8f
-	github.com/robertkrimen/otto v0.0.0-20170205013659-6a77b7cbc37d // indirect
+	github.com/rnsdomains/rns-go-lib v0.0.0-20200116145323-7d17f149733e
 	github.com/rs/cors v0.0.0-20160617231935-a62a804a8a00
-	github.com/syndtr/goleveldb v0.0.0-20190318030020-c3a204f8e965
+	github.com/sirupsen/logrus v1.4.2 // indirect
+	github.com/syndtr/goleveldb v1.0.1-0.20190923125748-758128399b1d
 	github.com/tilinna/clock v1.0.2
-	github.com/uber-go/atomic v1.4.0 // indirect
-	github.com/uber/jaeger-client-go v0.0.0-20180607151842-f7e0d4744fa6
-	github.com/uber/jaeger-lib v0.0.0-20180615202729-a51202d6f4a7 // indirect
+	github.com/uber/jaeger-client-go v2.22.1+incompatible
+	github.com/uber/jaeger-lib v2.2.0+incompatible // indirect
 	github.com/vbauerster/mpb v3.4.0+incompatible
-	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/atomic v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
-	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80
-	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
+	golang.org/x/net v0.0.0-20191004110552-13f9640d40b9
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
-	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
-	google.golang.org/appengine v1.6.1 // indirect
-	google.golang.org/grpc v1.22.1 // indirect
-	gopkg.in/inf.v0 v0.9.1 // indirect
-	gopkg.in/sourcemap.v1 v1.0.5 // indirect
 	gopkg.in/urfave/cli.v1 v1.20.0
-	gotest.tools v2.2.0+incompatible // indirect
-	k8s.io/api v0.0.0-20190703205437-39734b2a72fe
-	k8s.io/apimachinery v0.0.0-20190703205208-4cfb76a8bf76
-	k8s.io/client-go v0.0.0-20190706005506-4ed54556a14a
+	k8s.io/api v0.17.4
+	k8s.io/apimachinery v0.17.4
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/utils v0.0.0-20200229041039-0a110f9eb7ab // indirect
 )
---
+++ storage/fcds/fcds_test.go
@@ -0,0 +1,262 @@
+// Copyright 2019 The Swarm Authors
+// This file is part of the Swarm library.
+//
+// The Swarm library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Swarm library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Swarm library. If not, see <http://www.gnu.org/licenses/>.
+
+package fcds
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/ethersphere/swarm/chunk"
+)
+
+const (
+	ConcurrentThreads = 128
+)
+
+func newBadger(b *testing.B) (db Storer, clean func()) {
+	b.Helper()
+
+	path, err := ioutil.TempDir("", "swarm-shed")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	db, err = NewBadgerStore(path)
+	if err != nil {
+		os.RemoveAll(path)
+		b.Fatal(err)
+	}
+	return db, func() {
+		db.Close()
+		os.RemoveAll(path)
+	}
+}
+
+func getChunks(count int, chunkCache []chunk.Chunk) []chunk.Chunk {
+	l := len(chunkCache)
+	if l == 0 {
+		chunkCache = make([]chunk.Chunk, count)
+		for i := 0; i < count; i++ {
+			chunkCache[i] = GenerateTestRandomChunk()
+		}
+		return chunkCache
+	}
+	if l < count {
+		for i := 0; i < count-l; i++ {
+			chunkCache = append(chunkCache, GenerateTestRandomChunk())
+		}
+		return chunkCache
+	}
+	return chunkCache[:count]
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func GenerateTestRandomChunk() chunk.Chunk {
+	data := make([]byte, chunk.DefaultSize)
+	rand.Read(data)
+	key := make([]byte, 32)
+	rand.Read(key)
+	return chunk.NewChunk(key, data)
+}
+
+// Benchmarkings
+
+func runBenchmark(b *testing.B, baseChunksCount int, writeChunksCount int, readChunksCount int, deleteChunksCount int, iterationCount int) {
+	b.Helper()
+
+	var writeElapsed time.Duration
+	var readElapsed time.Duration
+	var deleteElapsed time.Duration
+
+	db, clean := newBadger(b)
+	var basechunks []chunk.Chunk
+
+	if baseChunksCount > 0 {
+		basechunks = getChunks(baseChunksCount, basechunks)
+		start := time.Now()
+		sem := make(chan struct{}, ConcurrentThreads)
+		var wg sync.WaitGroup
+		wg.Add(baseChunksCount)
+		for i, ch := range basechunks {
+			sem <- struct{}{}
+			go func(i int, ch chunk.Chunk) {
+				defer func() {
+					<-sem
+					wg.Done()
+				}()
+				if err := db.Put(ch); err != nil {
+					panic(err)
+				}
+			}(i, ch)
+		}
+		wg.Wait()
+		elapsed := time.Since(start)
+		fmt.Println("-- adding base chunks took, ", elapsed)
+	}
+
+	rand.Shuffle(baseChunksCount, func(i, j int) {
+		basechunks[i], basechunks[j] = basechunks[j], basechunks[i]
+	})
+
+	for i := 0; i < iterationCount; i++ {
+
+		var jobWg sync.WaitGroup
+		if writeChunksCount > 0 {
+			jobWg.Add(1)
+			go func() {
+				var writeChunks []chunk.Chunk
+				writeChunks = getChunks(writeChunksCount, writeChunks)
+				start := time.Now()
+				sem := make(chan struct{}, ConcurrentThreads)
+				var wg sync.WaitGroup
+				wg.Add(writeChunksCount)
+				for i, ch := range writeChunks {
+					sem <- struct{}{}
+					go func(i int, ch chunk.Chunk) {
+						defer func() {
+							<-sem
+							wg.Done()
+						}()
+						if err := db.Put(ch); err != nil {
+							panic(err)
+						}
+					}(i, ch)
+				}
+				wg.Wait()
+				elapsed := time.Since(start)
+				fmt.Println("-- writing chunks took , ", elapsed)
+				writeElapsed += elapsed
+				jobWg.Done()
+			}()
+		}
+
+		if readChunksCount > 0 {
+			jobWg.Add(1)
+			go func() {
+				errCount := 0
+				start := time.Now()
+				sem := make(chan struct{}, ConcurrentThreads*4)
+				var wg sync.WaitGroup
+				wg.Add(readChunksCount)
+				for i, ch := range basechunks {
+					if i >= readChunksCount {
+						break
+					}
+					sem <- struct{}{}
+					go func(i int, ch chunk.Chunk) {
+						defer func() {
+							<-sem
+							wg.Done()
+						}()
+						_, err := db.Get(ch.Address())
+						if err != nil {
+							//panic(err)
+							errCount++
+						}
+					}(i, ch)
+				}
+				wg.Wait()
+				elapsed := time.Since(start)
+				fmt.Println("-- reading chunks took , ", elapsed)
+				readElapsed += elapsed
+				jobWg.Done()
+			}()
+		}
+
+		if deleteChunksCount > 0 {
+			jobWg.Add(1)
+			go func() {
+				start := time.Now()
+				sem := make(chan struct{}, ConcurrentThreads)
+				var wg sync.WaitGroup
+				wg.Add(deleteChunksCount)
+				for i, ch := range basechunks {
+					if i >= deleteChunksCount {
+						break
+					}
+					sem <- struct{}{}
+					go func(i int, ch chunk.Chunk) {
+						defer func() {
+							<-sem
+							wg.Done()
+						}()
+						if err := db.Delete(ch.Address()); err != nil {
+							panic(err)
+						}
+					}(i, ch)
+				}
+				wg.Wait()
+				elapsed := time.Since(start)
+				fmt.Println("-- deleting chunks took , ", elapsed)
+				deleteElapsed += elapsed
+				jobWg.Done()
+			}()
+		}
+
+		jobWg.Wait()
+	}
+	clean()
+
+
+	if writeElapsed > 0 {
+		fmt.Println("- Average write  time : ", writeElapsed.Nanoseconds()/int64(iterationCount), " ns/op")
+	}
+	if readElapsed > 0 {
+		fmt.Println("- Average read time : ", readElapsed.Nanoseconds()/int64(iterationCount), " ns/op")
+	}
+	if deleteElapsed > 0 {
+		fmt.Println("- Average delete time : ", deleteElapsed.Nanoseconds()/int64(iterationCount), " ns/op")
+	}
+}
+
+//func TestStorage (b *testing.T) {
+//	runBenchmark(b, 0, 1000000, 0, 0)
+//}
+
+func BenchmarkWriteOverClean_10000(t *testing.B) { runBenchmark(t, 0, 10000, 0, 0,8) }
+func BenchmarkWriteOverClean_100000(t *testing.B) { runBenchmark(t, 0, 100000, 0, 0, 6) }
+func BenchmarkWriteOverClean_1000000(t *testing.B) { runBenchmark(t, 0, 1000000, 0, 0, 4) }
+
+
+func BenchmarkWriteOver1Million_10000(t *testing.B) { runBenchmark(t, 1000000, 10000, 0, 0,8) }
+func BenchmarkWriteOver1Million_100000(t *testing.B) { runBenchmark(t, 1000000, 100000, 0, 0,6) }
+func BenchmarkWriteOver1Million_1000000(t *testing.B) { runBenchmark(t, 1000000, 1000000, 0, 0,4) }
+
+func BenchmarkReadOver1Million_10000(t *testing.B) { runBenchmark(t, 1000000, 0, 10000, 0,8) }
+func BenchmarkReadOver1Million_100000(t *testing.B) { runBenchmark(t, 1000000, 0, 100000, 0, 6) }
+func BenchmarkReadOver1Million_1000000(t *testing.B) { runBenchmark(t, 1000000, 0, 1000000, 0,4) }
+
+func BenchmarkDeleteOver1Million_10000(t *testing.B) { runBenchmark(t, 1000000, 0, 0, 10000,8) }
+func BenchmarkDeleteOver1Million_100000(t *testing.B) { runBenchmark(t, 1000000, 0, 0, 100000,6) }
+func BenchmarkDeleteOver1Million_1000000(t *testing.B) { runBenchmark(t, 1000000, 0, 0, 1000000,4) }
+
+func BenchmarkWriteReadOver1Million_10000(t *testing.B) { runBenchmark(t, 1000000, 10000, 10000, 0,8) }
+func BenchmarkWriteReadOver1Million_100000(t *testing.B) { runBenchmark(t, 1000000, 100000, 100000, 0,6) }
+func BenchmarkWriteReadOver1Million_1000000(t *testing.B) { runBenchmark(t, 1000000, 1000000, 1000000, 0,4) }
+
+func BenchmarkWriteReadDeleteOver1Million_10000(t *testing.B) { runBenchmark(t, 1000000, 10000, 10000, 10000,8) }
+func BenchmarkWriteReadDeleteOver1Million_100000(t *testing.B) { runBenchmark(t, 1000000, 100000, 100000, 100000,6) }
+func BenchmarkWriteReadDeleteOver1Million_1000000(t *testing.B) { runBenchmark(t, 1000000, 1000000, 1000000, 1000000,4) }
+
