40327f905fb80db4d839325c1a676138d693bc28
Ralph Pichler <pichler.ralph@gmail.com>
2020-03-18 12:32:19 +0100

    swap/chain: add line after copyright header

--- swap/chain/txqueue_test.go
+++ swap/chain/txqueue_test.go
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with the Swarm library. If not, see <http://www.gnu.org/licenses/>.
+
 package chain
 
 import (
