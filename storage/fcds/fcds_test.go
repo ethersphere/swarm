e665acb3c39b406bdaec523b1a8d4f1e0f5e46c8
Zahoor Mohamed <zahoor@zahoor.in>
2020-03-26 16:15:19 +0530

    benchmark changes

--- storage/fcds/fcds_test.go
+++ storage/fcds/fcds_test.go
@@ -90,18 +90,50 @@ func runBenchmark(b *testing.B, baseChunksCount int, writeChunksCount int, readC
 	var readElapsed time.Duration
 	var deleteElapsed time.Duration
 
-	for i := 0; i < iterationCount; i++ {
-		db, clean := newBadger(b)
-		var basechunks []chunk.Chunk
+	db, clean := newBadger(b)
+	var basechunks []chunk.Chunk
 
-		b.StopTimer()
-		if baseChunksCount > 0 {
-			basechunks = getChunks(baseChunksCount, basechunks)
+	b.StopTimer()
+	if baseChunksCount > 0 {
+		basechunks = getChunks(baseChunksCount, basechunks)
+		start := time.Now()
+		sem := make(chan struct{}, ConcurrentThreads)
+		var wg sync.WaitGroup
+		wg.Add(baseChunksCount)
+		for i, ch := range basechunks {
+			sem <- struct{}{}
+			go func(i int, ch chunk.Chunk) {
+				defer func() {
+					<-sem
+					wg.Done()
+				}()
+				if err := db.Put(ch); err != nil {
+					panic(err)
+				}
+			}(i, ch)
+		}
+		wg.Wait()
+		elapsed := time.Since(start)
+		fmt.Println("-- adding base chunks took, ", elapsed)
+	}
+
+	rand.Shuffle(baseChunksCount, func(i, j int) {
+		basechunks[i], basechunks[j] = basechunks[j], basechunks[i]
+	})
+
+	var writeChunks []chunk.Chunk
+	writeChunks = getChunks(writeChunksCount, writeChunks)
+	b.StartTimer()
+
+	var jobWg sync.WaitGroup
+	if writeChunksCount > 0 {
+		jobWg.Add(1)
+		go func() {
 			start := time.Now()
 			sem := make(chan struct{}, ConcurrentThreads)
 			var wg sync.WaitGroup
-			wg.Add(baseChunksCount)
-			for i, ch := range basechunks {
+			wg.Add(writeChunksCount)
+			for i, ch := range writeChunks {
 				sem <- struct{}{}
 				go func(i int, ch chunk.Chunk) {
 					defer func() {
@@ -115,121 +147,88 @@ func runBenchmark(b *testing.B, baseChunksCount int, writeChunksCount int, readC
 			}
 			wg.Wait()
 			elapsed := time.Since(start)
-			fmt.Println("-- adding base chunks took, ", elapsed)
-		}
+			fmt.Println("-- writing chunks took , ", elapsed)
+			writeElapsed += elapsed
+			jobWg.Done()
+		}()
+	}
 
-		rand.Shuffle(baseChunksCount, func(i, j int) {
-			basechunks[i], basechunks[j] = basechunks[j], basechunks[i]
-		})
-		b.StartTimer()
-
-		var jobWg sync.WaitGroup
-		if writeChunksCount > 0 {
-			jobWg.Add(1)
-			go func() {
-				var writeChunks []chunk.Chunk
-				writeChunks = getChunks(writeChunksCount, writeChunks)
-				start := time.Now()
-				sem := make(chan struct{}, ConcurrentThreads)
-				var wg sync.WaitGroup
-				wg.Add(writeChunksCount)
-				for i, ch := range writeChunks {
-					sem <- struct{}{}
-					go func(i int, ch chunk.Chunk) {
-						defer func() {
-							<-sem
-							wg.Done()
-						}()
-						if err := db.Put(ch); err != nil {
-							panic(err)
-						}
-					}(i, ch)
+	if readChunksCount > 0 {
+		jobWg.Add(1)
+		go func() {
+			errCount := 0
+			start := time.Now()
+			sem := make(chan struct{}, ConcurrentThreads*4)
+			var wg sync.WaitGroup
+			wg.Add(readChunksCount)
+			for i, ch := range basechunks {
+				if i >= readChunksCount {
+					break
 				}
-				wg.Wait()
-				elapsed := time.Since(start)
-				fmt.Println("-- writing chunks took , ", elapsed)
-				writeElapsed += elapsed
-				jobWg.Done()
-			}()
-		}
-
-		if readChunksCount > 0 {
-			jobWg.Add(1)
-			go func() {
-				errCount := 0
-				start := time.Now()
-				sem := make(chan struct{}, ConcurrentThreads*4)
-				var wg sync.WaitGroup
-				wg.Add(readChunksCount)
-				for i, ch := range basechunks {
-					if i >= readChunksCount {
-						break
+				sem <- struct{}{}
+				go func(i int, ch chunk.Chunk) {
+					defer func() {
+						<-sem
+						wg.Done()
+					}()
+					_, err := db.Get(ch.Address())
+					if err != nil {
+						//panic(err)
+						errCount++
 					}
-					sem <- struct{}{}
-					go func(i int, ch chunk.Chunk) {
-						defer func() {
-							<-sem
-							wg.Done()
-						}()
-						_, err := db.Get(ch.Address())
-						if err != nil {
-							//panic(err)
-							errCount++
-						}
-					}(i, ch)
-				}
-				wg.Wait()
-				elapsed := time.Since(start)
-				fmt.Println("-- reading chunks took , ", elapsed)
-				readElapsed += elapsed
-				jobWg.Done()
-			}()
-		}
+				}(i, ch)
+			}
+			wg.Wait()
+			elapsed := time.Since(start)
+			//fmt.Println("-- reading chunks took , ", elapsed)
+			readElapsed += elapsed
+			jobWg.Done()
+		}()
+	}
 
-		if deleteChunksCount > 0 {
-			jobWg.Add(1)
-			go func() {
-				start := time.Now()
-				sem := make(chan struct{}, ConcurrentThreads)
-				var wg sync.WaitGroup
-				wg.Add(deleteChunksCount)
-				for i, ch := range basechunks {
-					if i >= deleteChunksCount {
-						break
+	if deleteChunksCount > 0 {
+		jobWg.Add(1)
+		go func() {
+			start := time.Now()
+			sem := make(chan struct{}, ConcurrentThreads)
+			var wg sync.WaitGroup
+			wg.Add(deleteChunksCount)
+			for i, ch := range basechunks {
+				if i >= deleteChunksCount {
+					break
+				}
+				sem <- struct{}{}
+				go func(i int, ch chunk.Chunk) {
+					defer func() {
+						<-sem
+						wg.Done()
+					}()
+					if err := db.Delete(ch.Address()); err != nil {
+						panic(err)
 					}
-					sem <- struct{}{}
-					go func(i int, ch chunk.Chunk) {
-						defer func() {
-							<-sem
-							wg.Done()
-						}()
-						if err := db.Delete(ch.Address()); err != nil {
-							panic(err)
-						}
-					}(i, ch)
-				}
-				wg.Wait()
-				elapsed := time.Since(start)
-				fmt.Println("-- deleting chunks took , ", elapsed)
-				deleteElapsed += elapsed
-				jobWg.Done()
-			}()
-		}
-
-		jobWg.Wait()
-
-		clean()
+				}(i, ch)
+			}
+			wg.Wait()
+			elapsed := time.Since(start)
+			//fmt.Println("-- deleting chunks took , ", elapsed)
+			deleteElapsed += elapsed
+			jobWg.Done()
+		}()
 	}
 
-	if writeElapsed > 0 {
-		fmt.Println("- Average write  time : ", writeElapsed.Nanoseconds()/int64(iterationCount), " ns/op")
-	}
-	if readElapsed > 0 {
-		fmt.Println("- Average read time : ", readElapsed.Nanoseconds()/int64(iterationCount), " ns/op")
-	}
-	if deleteElapsed > 0 {
-		fmt.Println("- Average delete time : ", deleteElapsed.Nanoseconds()/int64(iterationCount), " ns/op")
-	}
+	jobWg.Wait()
+
+	clean()
+
+	//if writeElapsed > 0 {
+	//	fmt.Println("- Average write  time : ", writeElapsed.Nanoseconds()/int64(iterationCount), " ns/op")
+	//}
+	//if readElapsed > 0 {
+	//	//fmt.Println("- Average read time : ", readElapsed.Nanoseconds()/int64(iterationCount), " ns/op")
+	//}
+	//if deleteElapsed > 0 {
+	//	//fmt.Println("- Average delete time : ", deleteElapsed.Nanoseconds()/int64(iterationCount), " ns/op")
+	//}
 }
 
 //func TestStorage (b *testing.T) {
@@ -238,28 +237,32 @@ func runBenchmark(b *testing.B, baseChunksCount int, writeChunksCount int, readC
 
 //func BenchmarkWriteOverClean_10000(t *testing.B) { runBenchmark(t, 0, 10000, 0, 0,8) }
 //func BenchmarkWriteOverClean_100000(t *testing.B) { runBenchmark(t, 0, 100000, 0, 0, 6) }
-func BenchmarkWriteOverClean_1000000(t *testing.B) { runBenchmark(t, 0, 1000000, 0, 0, 4) }
+//func BenchmarkWriteOverClean_1000000(t *testing.B) { runBenchmark(t, 0, 1000000, 0, 0, 4) }
 
-//func BenchmarkWriteOver1Million_10000(t *testing.B) { runBenchmark(t, 1000000, 10000, 0, 0,8) }
+func BenchmarkWriteOver1Million_10000(t *testing.B) {
+	for i := 0; i < t.N; i++ {
+		runBenchmark(t, 1000000, 10000, 0, 0, 8)
+	}
+
+}
+
 //func BenchmarkWriteOver1Million_100000(t *testing.B) { runBenchmark(t, 1000000, 100000, 0, 0,6) }
-func BenchmarkWriteOver1Million_1000000(t *testing.B) { runBenchmark(t, 1000000, 1000000, 0, 0, 4) }
+//func BenchmarkWriteOver1Million_1000000(t *testing.B) { runBenchmark(t, 1000000, 1000000, 0, 0, 4) }
+//func BenchmarkWriteOver1Million_5000000(t *testing.B) { runBenchmark(t, 5000000, 1000000, 0, 0, 4) }
+//func BenchmarkWriteOver1Million_10000000(t *testing.B) { runBenchmark(t, 10000000, 1000000, 0, 0, 4) }
 
 //func BenchmarkReadOver1Million_10000(t *testing.B) { runBenchmark(t, 1000000, 0, 10000, 0,8) }
 //func BenchmarkReadOver1Million_100000(t *testing.B) { runBenchmark(t, 1000000, 0, 100000, 0, 6) }
-func BenchmarkReadOver1Million_1000000(t *testing.B) { runBenchmark(t, 1000000, 0, 1000000, 0, 4) }
+//func BenchmarkReadOver1Million_1000000(t *testing.B) { runBenchmark(t, 1000000, 0, 1000000, 0, 4) }
 
 //func BenchmarkDeleteOver1Million_10000(t *testing.B) { runBenchmark(t, 1000000, 0, 0, 10000,8) }
 //func BenchmarkDeleteOver1Million_100000(t *testing.B) { runBenchmark(t, 1000000, 0, 0, 100000,6) }
-func BenchmarkDeleteOver1Million_1000000(t *testing.B) { runBenchmark(t, 1000000, 0, 0, 1000000, 4) }
+//func BenchmarkDeleteOver1Million_1000000(t *testing.B) { runBenchmark(t, 1000000, 0, 0, 1000000, 4) }
 
 //func BenchmarkWriteReadOver1Million_10000(t *testing.B) { runBenchmark(t, 1000000, 10000, 10000, 0,8) }
 //func BenchmarkWriteReadOver1Million_100000(t *testing.B) { runBenchmark(t, 1000000, 100000, 100000, 0,6) }
-func BenchmarkWriteReadOver1Million_1000000(t *testing.B) {
-	runBenchmark(t, 1000000, 1000000, 1000000, 0, 4)
-}
+//func BenchmarkWriteReadOver1Million_1000000(t *testing.B) {runBenchmark(t, 1000000, 1000000, 1000000, 0, 4)}
 
 //func BenchmarkWriteReadDeleteOver1Million_10000(t *testing.B) { runBenchmark(t, 1000000, 10000, 10000, 10000,8) }
 //func BenchmarkWriteReadDeleteOver1Million_100000(t *testing.B) { runBenchmark(t, 1000000, 100000, 100000, 100000,6) }
-func BenchmarkWriteReadDeleteOver1Million_1000000(t *testing.B) {
-	runBenchmark(t, 1000000, 1000000, 1000000, 1000000, 4)
-}
+//func BenchmarkWriteReadDeleteOver1Million_1000000(t *testing.B) {runBenchmark(t, 1000000, 1000000, 1000000, 1000000, 4)}
