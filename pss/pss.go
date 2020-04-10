bdd72cb13274807d6b7c917324591edf728e8c3f
santicomp2014 <santicomp2@gmail.com>
2020-04-10 16:14:50 -0300

    pss: add new pss (#2150)
    * pss: commented partial  pss code for minimum implementation
    
    * pss: only barebones for global pinning
    
    * pss,pss\message: int test trojan chunk and retrieval
    
    * pss, pss/internal/ticker, pss/internal/ttlset, pss/trojan: test in progress Send method under construction, this will be iterated in the next commits
    
    * pss, pss/trojan: added test for send functionality
    
    * pss: removed unused consts
    
    * pss: removed unused consts, fixed linter issues
    
    * pss: fixed lint issues
    
    * pss: changed comment on test
    
    * pss: cleanup of variables and error handling
    
    * pss: reinit pss
    
    * pss: added pss again, another iteration is needed
    
    * pss, pss/message: refactored into Pss struct, add mock for Api still need another iteration
    
    * pss, pss/crypto, pss/internal/ticker, pss/internal/ttlset: barebone pss
    
    * pss: fixed comments spacing, Send signature changed
    
    * pss, pss/trojan: changed trojan.wrap to m.Wrap for trojanChunk, fixed newline
    
    * pss: refactored error handling in newMockLocalStore
    
    * pss: updated comments and authors
    
    * pss: fixed comments
    
    * pss: added TestTrojanChunkRetrieval comment

---
+++ pss/pss.go
@@ -0,0 +1,62 @@
+// Copyright 2020 The Swarm Authors
+// This file is part of the Swarm library.
+//
+// The Swarm library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Swarm library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Swarm library. If not, see <http://www.gnu.org/licenses/>.
+
+package pss
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/metrics"
+	"github.com/ethersphere/swarm/chunk"
+	trojan "github.com/ethersphere/swarm/pss/trojan"
+)
+
+// Pss is the top-level struct, which takes care of message sending
+type Pss struct {
+	localStore chunk.Store
+}
+
+// NewPss inits the Pss struct with the localstore
+func NewPss(localStore chunk.Store) *Pss {
+	return &Pss{
+		localStore: localStore,
+	}
+}
+
+// Send constructs a padded message with topic and payload,
+// wraps it in a trojan chunk such that one of the targets is a prefix of the chunk address
+// stores this in localstore for push-sync to pick up and deliver
+func (p *Pss) Send(ctx context.Context, targets [][]byte, topic trojan.Topic, payload []byte) (chunk.Chunk, error) {
+	metrics.GetOrRegisterCounter("trojanchunk/send", nil).Inc(1)
+	//construct Trojan Chunk
+	m, err := trojan.NewMessage(topic, payload)
+	if err != nil {
+		return nil, err
+	}
+	var tc chunk.Chunk
+	tc, err = m.Wrap(targets)
+	if err != nil {
+		return nil, err
+	}
+
+	// SAVE trojanChunk to localstore, if it exists do nothing as it's already peristed
+	// TODO: for second phase, use tags --> listen for response of recipient, recipient offline
+	if _, err = p.localStore.Put(ctx, chunk.ModePutUpload, tc); err != nil {
+		return nil, err
+	}
+
+	return tc, nil
+}
---
+++ pss/pss_test.go
@@ -0,0 +1,93 @@
+// Copyright 2020 The Swarm Authors
+// This file is part of the Swarm library.
+//
+// The Swarm library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Swarm library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Swarm library. If not, see <http://www.gnu.org/licenses/>.
+
+package pss
+
+import (
+	"context"
+	"crypto/rand"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/ethersphere/swarm/chunk"
+	trojan "github.com/ethersphere/swarm/pss/trojan"
+	"github.com/ethersphere/swarm/storage/localstore"
+)
+
+// TestTrojanChunkRetrieval creates a trojan chunk
+// mocks the localstore
+// calls pss.Send method and verifies it's properly stored
+func TestTrojanChunkRetrieval(t *testing.T) {
+	var err error
+	ctx := context.TODO()
+
+	localStore := newMockLocalStore(t)
+
+	testTargets := [][]byte{
+		{57, 120},
+		{209, 156},
+		{156, 38},
+		{89, 19},
+		{22, 129}}
+	payload := []byte("RECOVERY CHUNK")
+	topic := trojan.NewTopic("RECOVERY")
+
+	var ch chunk.Chunk
+
+	pss := NewPss(localStore)
+
+	// call Send to store trojan chunk in localstore
+	if ch, err = pss.Send(ctx, testTargets, topic, payload); err != nil {
+		t.Fatal(err)
+	}
+
+	// verify store, that trojan chunk has been stored correctly
+	var storedChunk chunk.Chunk
+	if storedChunk, err = localStore.Get(ctx, chunk.ModeGetRequest, ch.Address()); err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(ch, storedChunk) {
+		t.Fatalf("store chunk does not match sent chunk")
+	}
+
+	// check if pinning makes a difference
+
+}
+
+func newMockLocalStore(t *testing.T) *localstore.DB {
+	dir, err := ioutil.TempDir("", "swarm-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	baseKey := make([]byte, 32)
+	if _, err = rand.Read(baseKey); err != nil {
+		t.Fatal(err)
+	}
+
+	localStore, err := localstore.New(dir, baseKey, &localstore.Options{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return localStore
+}
+
+// TODO: later test could be a simulation test for 2 nodes, localstore + netstore
