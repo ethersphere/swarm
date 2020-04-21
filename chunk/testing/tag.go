247914cb06a3d89bef0cccca20329950becda58f
santicomp2014 <santicomp2@gmail.com>
2020-04-21 12:04:21 -0300

    chunk/testing: fixed Sent conditional in CheckTag

--- chunk/testing/tag.go
+++ chunk/testing/tag.go
@@ -44,7 +44,7 @@ func CheckTag(t *testing.T, tag *chunk.Tag, split, stored, seen, sent, synced, t
 	}
 
 	tSent := tag.Get(chunk.StateSent)
-	if tSent != seen {
+	if tSent != sent {
 		t.Fatalf("mismatch sent chunks, got %d want %d", tSent, sent)
 	}
 
