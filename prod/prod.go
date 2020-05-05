713216688e8a5bab88176c5630be6993afd70eeb
mortelli <ortellimarcelo@gmail.com>
2020-05-05 15:36:42 -0300

    prod: iterate getPinners func

--- prod/prod.go
+++ prod/prod.go
@@ -18,10 +18,15 @@ package prod
 
 import (
 	"context"
+	"encoding/hex"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethersphere/swarm/api"
 	"github.com/ethersphere/swarm/chunk"
 	"github.com/ethersphere/swarm/pss"
 	"github.com/ethersphere/swarm/pss/trojan"
+	"github.com/ethersphere/swarm/storage/feed"
 )
 
 // RecoveryHook defines code to be executed upon trigger of failed to be retrieved chunks
@@ -33,7 +38,7 @@ type sender func(ctx context.Context, targets [][]byte, topic trojan.Topic, payl
 // NewRecoveryHook returns a new RecoveryHook with the sender function defined
 func NewRecoveryHook(send sender) RecoveryHook {
 	return func(ctx context.Context, chunkAddress chunk.Address) error {
-		targets, err := getPinners(chunkAddress)
+		targets, err := getPinners(publisher)
 		if err != nil {
 			return err
 		}
@@ -48,14 +53,41 @@ func NewRecoveryHook(send sender) RecoveryHook {
 	}
 }
 
-// TODO: refactor this method to implement feed of target pinners
 // getPinners returns the specific target pinners for a corresponding chunk address
-func getPinners(chunkAddress chunk.Address) ([][]byte, error) {
-	//this should get the feed and return correct target of pinners
-	return [][]byte{
-		{57, 120},
-		{209, 156},
-		{156, 38},
-		{89, 19},
-		{22, 129}}, nil
+func getPinners(publisher string) ([][]byte, error) {
+	// get feed user from publisher
+	publisherBytes, err := hex.DecodeString(publisher)
+	if err != nil {
+		return nil, api.ErrDecrypt
+	}
+	pubKey, err := crypto.DecompressPubkey(publisherBytes)
+	addr := crypto.PubkeyToAddress(*pubKey)
+
+	// TODO: use existing swarm handler
+	fhParams := &feed.HandlerParams{}
+	fh := feed.NewHandler(fhParams)
+
+	// read feed
+	// TODO: resolve sinful type conversions
+	fd := feed.Feed{
+		Topic: feed.Topic(trojan.NewTopic("RECOVERY")),
+		User:  common.Address(addr),
+	}
+
+	// TODO: do we need query lookup before content fetch?
+	// query := feed.NewQuery(&fd, uint64(time.Now().Unix()), lookup.NoClue)
+	// TODO: context.WithCancel?
+	// _, err = fh.Lookup(context.Background(), query)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// TODO: time-outs?
+	_, content, err := fh.GetContent(&fd)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: transform content into actual list of targets
+	return [][]byte{content}, nil
 }
