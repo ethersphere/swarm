644933f1a6c35b55cab5a52257f7cd5313906f35
mortelli <ortellimarcelo@gmail.com>
2020-05-20 12:36:47 -0300

    prod: improve global pinning recovery hook tests (#2188)
    prod: improve global pinning recovery hook tests

--- prod/prod_test.go
+++ prod/prod_test.go
@@ -27,7 +27,6 @@ import (
 	ctest "github.com/ethersphere/swarm/chunk/testing"
 	"github.com/ethersphere/swarm/network"
 	"github.com/ethersphere/swarm/network/retrieval"
-	"github.com/ethersphere/swarm/network/timeouts"
 	"github.com/ethersphere/swarm/pss"
 	psstest "github.com/ethersphere/swarm/pss/testing"
 	"github.com/ethersphere/swarm/pss/trojan"
@@ -35,93 +34,143 @@ import (
 	"github.com/ethersphere/swarm/storage/feed"
 )
 
-// TestRecoveryHook tests that NewRecoveryHook has been properly invoked
+// TestRecoveryHook tests that a recovery hook can be created and called
 func TestRecoveryHook(t *testing.T) {
-	ctx := context.WithValue(context.TODO(), "publisher", "0226f213613e843a413ad35b40f193910d26eb35f00154afcde9ded57479a6224a")
+	// test variables needed to be correctly set for any recovery hook to reach the sender func
+	chunkAddr := ctest.GenerateTestRandomChunk().Address()
+	ctx := context.WithValue(context.Background(), "publisher", "0226f213613e843a413ad35b40f193910d26eb35f00154afcde9ded57479a6224a")
+	handler := newTestRecoveryFeedsHandler(t)
 
-	hookWasCalled := false // test variable to check hook func are correctly retrieved
-
-	// setup the hook
-	testHook := func(ctx context.Context, targets trojan.Targets, topic trojan.Topic, payload []byte) (*pss.Monitor, error) {
+	// setup the sender
+	hookWasCalled := false // test variable to check if hook is called
+	testSender := func(ctx context.Context, targets trojan.Targets, topic trojan.Topic, payload []byte) (*pss.Monitor, error) {
 		hookWasCalled = true
 		return nil, nil
 	}
-	testHandler := newTestRecoveryFeedHandler(t)
 
-	// setup recovery hook with testHook
-	recoverFunc := NewRecoveryHook(testHook, testHandler)
-
-	testChunk := "aacca8d446af47ebcab582ca2188fa73dfa871eb0a35eda798f47d4f91a575e9"
-	if err := recoverFunc(ctx, chunk.Address([]byte(testChunk))); err != nil {
+	// create recovery hook and call it
+	recoveryHook := NewRecoveryHook(testSender, handler)
+	if err := recoveryHook(ctx, chunkAddr); err != nil {
 		t.Fatal(err)
 	}
 
-	// verify the hook has been called correctly
 	if hookWasCalled != true {
-		t.Fatalf("unexpected result for prod Recover func, expected test variable to have a value of %t but is %t instead", true, hookWasCalled)
+		t.Fatalf("recovery hook was not called")
 	}
-
 }
 
-// TestSenderCall verifies that a hook is being called correctly within the netstore
-func TestSenderCall(t *testing.T) {
-	ctx := context.WithValue(context.TODO(), "publisher", "0226f213613e843a413ad35b40f193910d26eb35f00154afcde9ded57479a6224a")
-	tags := chunk.NewTags()
-	localStore := psstest.NewMockLocalStore(t, tags)
+// RecoveryHookTestCase is a struct used as test cases for the TestRecoveryHookCalls func
+type RecoveryHookTestCase struct {
+	name           string
+	ctx            context.Context
+	feedsHandler   feed.GenericHandler
+	expectsFailure bool
+}
 
-	lstore := chunk.NewValidatorStore(
-		localStore,
-		storage.NewContentAddressValidator(storage.MakeHashFunc(storage.DefaultHash)),
-	)
+// TestRecoveryHookCalls verifies that recovery hooks are being called as expected when net store attempts to get a chunk
+func TestRecoveryHookCalls(t *testing.T) {
+	// generate test chunk and store
+	netStore := newTestNetStore(t)
+	c := ctest.GenerateTestRandomChunk()
+	ref := c.Address()
 
+	// test cases variables
+	dummyContext := context.Background() // has no publisher
+	publisherContext := context.WithValue(context.Background(), "publisher", "0226f213613e843a413ad35b40f193910d26eb35f00154afcde9ded57479a6224a")
+	dummyHandler := feed.NewDummyHandler() // returns empty content for feed
+	feedsHandler := newTestRecoveryFeedsHandler(t)
+
+	for _, tc := range []RecoveryHookTestCase{
+		{
+			name:           "no publisher, no feed content",
+			ctx:            dummyContext,
+			feedsHandler:   dummyHandler,
+			expectsFailure: true,
+		},
+		{
+			name:           "publisher set, no feed content",
+			ctx:            publisherContext,
+			feedsHandler:   dummyHandler,
+			expectsFailure: true,
+		},
+		{
+			name:           "feed content set, no publisher",
+			ctx:            dummyContext,
+			feedsHandler:   feedsHandler,
+			expectsFailure: true,
+		},
+		{
+			name:           "publisher and feed content set",
+			ctx:            publisherContext,
+			feedsHandler:   feedsHandler,
+			expectsFailure: false,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			hookWasCalled := make(chan bool, 1) // channel to check if hook is called
+
+			// setup recovery hook
+			testHook := func(ctx context.Context, targets trojan.Targets, topic trojan.Topic, payload []byte) (*pss.Monitor, error) {
+				hookWasCalled <- true
+				return nil, nil
+			}
+			recoverFunc := NewRecoveryHook(testHook, tc.feedsHandler)
+
+			// set hook in net store
+			netStore.WithRecoveryCallback(recoverFunc)
+
+			// fetch test chunk
+			netStore.Get(tc.ctx, chunk.ModeGetRequest, storage.NewRequest(ref))
+
+			// checks whether the callback is invoked or the test case times out
+			select {
+			case <-hookWasCalled:
+				if !tc.expectsFailure {
+					return
+				} else {
+					t.Fatal("recovery hook was unexpectedly called")
+				}
+			case <-time.After(100 * time.Millisecond):
+				if tc.expectsFailure {
+					return
+				} else {
+					t.Fatal("recovery hook was not called when expected")
+				}
+			}
+		})
+	}
+}
+
+// newTestNetStore creates a test store with a set RemoteGet func
+func newTestNetStore(t *testing.T) *storage.NetStore {
+	// generate address
 	baseKey := make([]byte, 32)
 	_, err := rand.Read(baseKey)
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	baseAddress := network.NewBzzAddr(baseKey, baseKey)
-	// setup netstore
+
+	// generate net store
+	tags := chunk.NewTags()
+	localStore := psstest.NewMockLocalStore(t, tags)
+	lstore := chunk.NewValidatorStore(
+		localStore,
+		storage.NewContentAddressValidator(storage.MakeHashFunc(storage.DefaultHash)),
+	)
 	netStore := storage.NewNetStore(lstore, baseAddress)
 
-	hookWasCalled := false // test variable to check hook func are correctly retrieved
-
-	// setup recovery hook
-	testHook := func(ctx context.Context, targets trojan.Targets, topic trojan.Topic, payload []byte) (*pss.Monitor, error) {
-		hookWasCalled = true
-		return nil, nil
-	}
-	testHandler := newTestRecoveryFeedHandler(t)
-
-	recoverFunc := NewRecoveryHook(testHook, testHandler)
-	netStore.WithRecoveryCallback(recoverFunc)
-
-	c := ctest.GenerateTestRandomChunk()
-	ref := c.Address()
-
+	// generate retrieval
 	kad := network.NewKademlia(baseAddress.Over(), network.NewKadParams())
 	ret := retrieval.New(kad, netStore, baseAddress, nil)
+
+	// set retrieval on netstore and return
 	netStore.RemoteGet = ret.RequestFromPeers
-
-	netStore.Get(ctx, chunk.ModeGetRequest, storage.NewRequest(ref))
-
-	for {
-		// waits until the callback is called or timeout
-		select {
-		default:
-			if hookWasCalled {
-				return
-			}
-		// TODO: change the timeout
-		case <-time.After(timeouts.FetcherGlobalTimeout):
-			t.Fatalf("no hook was called")
-		}
-	}
-
+	return netStore
 }
 
-// newTestRecoveryFeedHandler returns a DummyHandler with binary content which can be correctly unmarshalled
-func newTestRecoveryFeedHandler(t *testing.T) *feed.DummyHandler {
+// newTestRecoveryFeedsHandler returns a DummyHandler with binary content which can be correctly unmarshalled
+func newTestRecoveryFeedsHandler(t *testing.T) *feed.DummyHandler {
 	h := feed.NewDummyHandler()
 
 	// test targets
