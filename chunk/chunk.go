a3ff06ad57d3397fee8a7dd3ad44157d803b2e73
mortelli <ortellimarcelo@gmail.com>
2020-05-26 11:03:00 -0300

    chunk: iterate indentation for Put func

--- chunk/chunk.go
+++ chunk/chunk.go
@@ -325,14 +325,17 @@ func (s *ValidatorStore) Put(ctx context.Context, mode ModePut, chs ...Chunk) (e
 	}
 	exist, err = s.Store.Put(ctx, mode, chs...)
 	// if callback is defined, call it for every new, valid, content-addressed chunk
-	if err == nil && s.deliverCallback != nil {
+	if err != nil {
+		return nil, err
+	}
+	if s.deliverCallback != nil {
 		for i, exists := range exist {
 			if !exists && chunkTypes[i] == ContentAddressed {
 				go s.deliverCallback(chs[i])
 			}
 		}
 	}
-	return exist, err
+	return exist, nil
 }
 
 // validate returns true if one of the validators
